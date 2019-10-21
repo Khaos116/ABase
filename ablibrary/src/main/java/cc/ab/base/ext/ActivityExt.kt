50ff86eaa3fa5dc0f5698cf11e7b9fd35f358c2f
case <442415631@qq.com>
2019-10-21 16:41:18 +0700

    新增键盘高度监听

--- ablibrary/src/main/java/cc/ab/base/ext/ActivityExt.kt
+++ ablibrary/src/main/java/cc/ab/base/ext/ActivityExt.kt
@@ -1,8 +1,7 @@
 package cc.ab.base.ext
 
 import android.app.Activity
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import android.widget.FrameLayout
 
 /**
@@ -31,9 +30,41 @@ val Activity.mContentView: FrameLayout
     return this.findViewById(android.R.id.content)
   }
 
-//ContentView
+//Context
 val Activity.mContext: Activity
   get() {
     return this
   }
+//Activity
+val Activity.mActivity: Activity
+  get() {
+    return this
+  }
 
+//监听键盘高度
+fun Activity.extKeyBoard(keyCall: (statusHeight: Int, navigationHeight: Int, keyBoardHeight: Int) -> Unit) {
+  mContentView.post { mContentView.layoutParams.height = mContentView.height }//防止键盘弹出导致整个布局高度变小
+  this.window.decorView.setOnApplyWindowInsetsListener(object : View.OnApplyWindowInsetsListener {
+    var preKeyOffset: Int = 0//键盘高度改变才回调
+    override fun onApplyWindowInsets(
+      v: View?,
+      insets: WindowInsets?
+    ): WindowInsets {
+      insets?.let { ins ->
+        val navHeight = ins.systemWindowInsetBottom//下面弹窗到屏幕底部的高度，比如键盘弹出后的键盘+虚拟导航键高度
+        val offset = if (navHeight < ins.stableInsetBottom) navHeight
+        else navHeight - ins.stableInsetBottom
+        if (offset != preKeyOffset || offset == 0) {//高度变化
+          val decorHeight = mActivity.window.decorView.height//整个布局高度，包含虚拟导航键
+          if (decorHeight > 0) {//为了防止手机去设置页修改虚拟导航键高度，导致整个内容显示有问题，所以需要重新设置高度(与上面设置固定高度对应)
+            mContentView.layoutParams.height =
+              decorHeight - navHeight.coerceAtMost(ins.stableInsetBottom)//取小值
+          }
+          preKeyOffset = offset
+          keyCall.invoke(ins.stableInsetTop, ins.stableInsetBottom, offset)
+        }
+      }
+      return mActivity.window.decorView.onApplyWindowInsets(insets)
+    }
+  })
+}
