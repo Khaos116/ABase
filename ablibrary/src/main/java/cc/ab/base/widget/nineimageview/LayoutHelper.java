9a62761b1773a26e9e320d6c91625c37e093c3d8
case <442415631@qq.com>
2019-10-25 19:57:19 +0700

    提取GlideImageView的NineImageView，但是目前存在复用闪动的情况

---
+++ ablibrary/src/main/java/cc/ab/base/widget/nineimageview/LayoutHelper.java
@@ -0,0 +1,12 @@
+package cc.ab.base.widget.nineimageview;
+
+import android.graphics.Point;
+
+/**
+ * @author sunfusheng on 2018/6/19.
+ */
+public interface LayoutHelper {
+    Point getCoordinate(int position);
+
+    Point getSize(int position);
+}
