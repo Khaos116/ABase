e9389b3cb1c340a260ad6326793417d31542c229
case <442415631@qq.com>
2019-10-26 19:50:23 +0700

    增加统一数据解析

---
+++ app/src/main/java/cc/abase/demo/repository/parse/BaseParameterizedTypeImpl.java
@@ -0,0 +1,53 @@
+package cc.abase.demo.repository.parse;
+
+import cc.ab.base.net.http.response.BaseResponse;
+import com.google.gson.Gson;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Description:https://www.jianshu.com/p/936a76198b57
+ *
+ * @author: caiyoufei
+ * @date: 2019/10/26 19:44
+ */
+public class BaseParameterizedTypeImpl implements ParameterizedType {
+  private final Class raw;
+  private final Type[] args;
+
+  public BaseParameterizedTypeImpl(Class raw, Type[] args) {
+    this.raw = raw;
+    this.args = args != null ? args : new Type[0];
+  }
+
+  public static <T> BaseResponse<T> fromJsonObject(String reader, Class<T> clazz) throws Exception {
+    Type type = new BaseParameterizedTypeImpl(BaseResponse.class, new Class[] { clazz });
+    return new Gson().fromJson(reader, type);
+  }
+
+  public static <T> BaseResponse<List<T>> fromJsonArray(String reader, Class<T> clazz)
+      throws Exception {
+    // 生成List<T> 中的 List<T>
+    Type listType = new BaseParameterizedTypeImpl(List.class, new Class[] { clazz });
+    // 根据List<T>生成完整的Response<List<T>>
+    Type type = new BaseParameterizedTypeImpl(BaseResponse.class, new Type[] { listType });
+    return new Gson().fromJson(reader, type);
+  }
+
+  @NotNull @Override
+  public Type[] getActualTypeArguments() {
+    return args;
+  }
+
+  @NotNull @Override
+  public Type getRawType() {
+    return raw;
+  }
+
+  @Override
+  public Type getOwnerType() {
+    return null;
+  }
+}
