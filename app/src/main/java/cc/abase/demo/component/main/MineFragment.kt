ee3ffafa17167912a881ccf29b1338960ef09052
case <442415631@qq.com>
2019-10-28 19:31:32 +0700

    增加功能扩展列表

--- app/src/main/java/cc/abase/demo/component/main/MineFragment.kt
+++ app/src/main/java/cc/abase/demo/component/main/MineFragment.kt
@@ -1,16 +1,23 @@
 package cc.abase.demo.component.main
 
+import android.content.Intent
 import android.util.Log
+import android.view.Gravity
 import android.view.View
-import cc.ab.base.ext.click
 import cc.ab.base.ext.toast
-import cc.ab.base.utils.PressEffectHelper
 import cc.abase.demo.R
 import cc.abase.demo.component.chat.ChatActivity
+import cc.abase.demo.component.comm.CommActivity
 import cc.abase.demo.component.comm.CommFragment
 import cc.abase.demo.component.ffmpeg.RxFFmpegActivity
+import cc.abase.demo.epoxy.base.dividerItem
+import cc.abase.demo.epoxy.item.simpleTextItem
+import cc.abase.demo.mvrx.MvRxEpoxyController
 import cc.abase.demo.repository.UserRepository
-import kotlinx.android.synthetic.main.fragment_mine.*
+import com.blankj.utilcode.util.ColorUtils
+import com.blankj.utilcode.util.StringUtils
+import kotlinx.android.synthetic.main.fragment_mine.mineIntegral
+import kotlinx.android.synthetic.main.fragment_mine.mineRecycler
 
 /**
  * Description:
@@ -18,6 +25,14 @@ import kotlinx.android.synthetic.main.fragment_mine.*
  * @date: 2019/9/30 18:13
  */
 class MineFragment : CommFragment() {
+  //菜单列表
+  val menuList = mutableListOf(
+      Pair(StringUtils.getString(R.string.chat_title), ChatActivity::class.java),
+      Pair(StringUtils.getString(R.string.ffmpeg_title), RxFFmpegActivity::class.java)
+  )
+  //item文字颜色
+  private var typeColor = ColorUtils.getColor(R.color.style_Primary)
+
   companion object {
     fun newInstance(): MineFragment {
       return MineFragment()
@@ -27,9 +42,7 @@ class MineFragment : CommFragment() {
   override val contentLayout = R.layout.fragment_mine
 
   override fun initView(root: View?) {
-    PressEffectHelper.bgColorEffect(myChatEmoji)
-    myChatEmoji.click { ChatActivity.startActivity(mContext) }
-    myFFmpeg.click { RxFFmpegActivity.startActivity(mContext) }
+    mineRecycler.setController(epoxyController)
   }
 
   override fun initData() {
@@ -38,12 +51,34 @@ class MineFragment : CommFragment() {
         .subscribe { t1, t2 ->
           dismissLoadingView()
           if (t1 != null) {
-            myIntegral.text = String.format(mActivity.getString(R.string.my_integral), t1.coinCount)
+            mineIntegral.text =
+              String.format(mActivity.getString(R.string.my_integral), t1.coinCount)
           } else if (t2 != null) {
+            mineIntegral.text =
+              String.format(mActivity.getString(R.string.my_integral), 0)
             mContext.toast(t2.message)
           }
+          epoxyController.data = menuList
         }
     Log.e("CASE", "MineFragment-开始加载数据")
   }
 
+  //epoxy
+  private val epoxyController =
+    MvRxEpoxyController<List<Pair<String, Class<out CommActivity>>>> { list ->
+      list.forEachIndexed { index, pair ->
+        //内容
+        simpleTextItem {
+          id("type_$index")
+          msg(pair.first)
+          textColor(typeColor)
+          gravity(Gravity.CENTER_VERTICAL)
+          onItemClick { mActivity.startActivity(Intent(mContext, pair.second)) }
+        }
+        //分割线
+        dividerItem {
+          id("line_$index")
+        }
+      }
+    }
 }
