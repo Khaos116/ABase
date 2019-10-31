8fac5adde405ebc9f87ceff3aeaa986193b3b650
case <442415631@qq.com>
2019-10-31 11:15:38 +0700

    增加DKPlayer的扩展功能View

---
+++ app/src/main/java/cc/abase/demo/widget/video/view/ExoVideoView.java
@@ -0,0 +1,89 @@
+package cc.abase.demo.widget.video.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.dueeeke.videoplayer.player.PlayerFactory;
+import com.dueeeke.videoplayer.player.VideoView;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+
+import cc.abase.demo.widget.video.player.CustomExoMediaPlayer;
+
+public class ExoVideoView extends VideoView<CustomExoMediaPlayer> {
+
+    private MediaSource mMediaSource;
+
+    private boolean mIsCacheEnabled;
+
+    private LoadControl mLoadControl;
+    private RenderersFactory mRenderersFactory;
+    private TrackSelector mTrackSelector;
+
+    public ExoVideoView(Context context) {
+        this(context, null);
+    }
+
+    public ExoVideoView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ExoVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //由于传递了泛型，必须将CustomExoMediaPlayer设置进来，否者报错
+        setPlayerFactory(new PlayerFactory<CustomExoMediaPlayer>() {
+            @Override
+            public CustomExoMediaPlayer createPlayer() {
+                return new CustomExoMediaPlayer();
+            }
+        });
+    }
+
+    @Override
+    protected void setInitOptions() {
+        super.setInitOptions();
+        mMediaPlayer.setLoadControl(mLoadControl);
+        mMediaPlayer.setRenderersFactory(mRenderersFactory);
+        mMediaPlayer.setTrackSelector(mTrackSelector);
+    }
+
+    @Override
+    protected boolean prepareDataSource() {
+        if (mIsCacheEnabled) {
+            mMediaPlayer.setDataSource(mUrl, mHeaders, true);
+            return true;
+        } else if (mMediaSource != null) {
+            mMediaPlayer.setDataSource(mMediaSource);
+            return true;
+        }
+        return super.prepareDataSource();
+    }
+
+    /**
+     * 设置ExoPlayer的MediaSource
+     */
+    public void setMediaSource(MediaSource mediaSource) {
+        this.mMediaSource = mediaSource;
+    }
+
+    /**
+     * 是否打开缓存
+     */
+    public void setCacheEnabled(boolean isEnabled) {
+        mIsCacheEnabled = isEnabled;
+    }
+
+    public void setLoadControl(LoadControl loadControl) {
+        mLoadControl = loadControl;
+    }
+
+    public void setRenderersFactory(RenderersFactory renderersFactory) {
+        mRenderersFactory = renderersFactory;
+    }
+
+    public void setTrackSelector(TrackSelector trackSelector) {
+        mTrackSelector = trackSelector;
+    }
+}
