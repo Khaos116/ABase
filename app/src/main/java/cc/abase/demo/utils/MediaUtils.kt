84be5e9470ae03fc59590c3a7640323e5860a995
case <442415631@qq.com>
2019-11-02 18:09:56 +0700

    增加图片和视频的文件判断工具

---
+++ app/src/main/java/cc/abase/demo/utils/MediaUtils.kt
@@ -0,0 +1,43 @@
+package cc.abase.demo.utils
+
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+import java.io.File
+
+/**
+ * Description:
+ * @author: caiyoufei
+ * @date: 2019/11/2 18:09
+ */
+class MediaUtils private constructor() {
+  private object SingletonHolder {
+    val holder = MediaUtils()
+  }
+
+  companion object {
+    val instance = SingletonHolder.holder
+  }
+
+  //判断文件是否是图片
+  fun isImageFile(filePath: String): Boolean {
+    if (!File(filePath).exists()) return false
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+    BitmapFactory.decodeFile(filePath, options)
+    return options.outWidth != -1
+  }
+
+  //判断文件是否是视频
+  fun isVideoFile(filePath: String): Boolean {
+    if (!File(filePath).exists()) return false
+    return try {
+      val mmr = MediaMetadataRetriever()
+      mmr.setDataSource(filePath)
+      val mimeType = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE)
+      mimeType?.contains("video", true) == true
+    } catch (e: Exception) {
+      e.printStackTrace()
+      false
+    }
+  }
+}
