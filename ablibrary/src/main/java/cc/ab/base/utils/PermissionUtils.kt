14bcc0e7212493e9267ffee8c81af596d46b123d
case <442415631@qq.com>
2019-11-12 14:32:42 +0700

    修改权限判断

--- ablibrary/src/main/java/cc/ab/base/utils/PermissionUtils.kt
+++ ablibrary/src/main/java/cc/ab/base/utils/PermissionUtils.kt
@@ -1,10 +1,17 @@
 package cc.ab.base.utils
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.hardware.Camera
 import android.location.LocationManager
 import android.media.*
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.blankj.utilcode.util.*
 import java.io.File
 
@@ -59,16 +66,16 @@ class PermissionUtils private constructor() {
   @SuppressLint("MissingPermission")
   fun hasRecordPermission(): Boolean {
     val minBufferSize = AudioRecord.getMinBufferSize(
-        44100,
-        AudioFormat.CHANNEL_IN_STEREO,
-        AudioFormat.ENCODING_PCM_16BIT
+      44100,
+      AudioFormat.CHANNEL_IN_STEREO,
+      AudioFormat.ENCODING_PCM_16BIT
     )
     var audioRecord: AudioRecord? = null
     try {
       audioRecord = AudioRecord(
-          MediaRecorder.AudioSource.MIC, 44100,
-          AudioFormat.CHANNEL_IN_STEREO,
-          AudioFormat.ENCODING_PCM_16BIT, minBufferSize
+        MediaRecorder.AudioSource.MIC, 44100,
+        AudioFormat.CHANNEL_IN_STEREO,
+        AudioFormat.ENCODING_PCM_16BIT, minBufferSize
       )
     } catch (e: Exception) {
       e.printStackTrace()
@@ -117,16 +124,65 @@ class PermissionUtils private constructor() {
   @SuppressLint("MissingPermission")
   fun hasLocationPermission(): Boolean {
     val c = Utils.getApp()
+    val permission = ActivityCompat.checkSelfPermission(c, Manifest.permission_group.LOCATION)
+    if (permission < 0) {
+      if (permission == -1) Log.e("CASE", "定位权限被拒绝")
+      if (permission == -2) Log.e("CASE", "定位权限被永久拒绝")
+      return false
+    }
+    return true
+  }
+
+  //定位是否可用
+  fun locationEnable(): Boolean {
+    val c = Utils.getApp()
+    val permission = ActivityCompat.checkSelfPermission(c, Manifest.permission_group.LOCATION)
+    if (permission < 0) {
+      if (permission == -1) Log.e("CASE", "定位权限被拒绝")
+      if (permission == -2) Log.e("CASE", "定位权限被永久拒绝")
+      return false
+    }
     val mLocationManager = c.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      return mLocationManager.isLocationEnabled
+    }
+    val provider = if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+      LocationManager.GPS_PROVIDER
+    } else if (mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+      LocationManager.NETWORK_PROVIDER
+    } else {
+      null
+    }
+    if (provider.isNullOrBlank()) return false
     return try {
-      mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-          ?.let {
-            LogUtils.e("getLastKnownLocation=$it")
-          }
+      mLocationManager.getLastKnownLocation(provider)
+        ?.let {
+          LogUtils.e("getLastKnownLocation=$it")
+        }
       true
     } catch (e: Exception) {
       LogUtils.e("LocationUtil:$e")
       false
+    }
+  }
+
+  //打开定位设置
+  fun startLocationSetting(context: Context) {
+    val intent = Intent()
+    intent.action = Settings.ACTION_LOCATION_SOURCE_SETTINGS
+    context.startActivity(intent)
+  }
+
+  //打开权限设置
+  fun startPermissionSetting(context: Context) {
+    try {
+      val intent = Intent()
+      intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+      val uri = Uri.fromParts("package", AppUtils.getAppPackageName(), null)
+      intent.data = uri
+      context.startActivity(intent)
+    } catch (e: Exception) {
+      e.printStackTrace()
     }
   }
 }
