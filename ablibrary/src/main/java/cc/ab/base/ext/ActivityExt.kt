28213be4edaff82321fb080e52f9dc21b92781e7
case <442415631@qq.com>
2019-11-19 17:05:21 +0700

    扩展恢复全屏方法，侧滑改为1.0.8

--- ablibrary/src/main/java/cc/ab/base/ext/ActivityExt.kt
+++ ablibrary/src/main/java/cc/ab/base/ext/ActivityExt.kt
@@ -11,6 +11,16 @@ import android.widget.FrameLayout
  * @date: 2019/9/20 16:36
  */
 
+//全屏
+fun Activity.extFullScreen() {
+  window?.let { win ->
+    win.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    win.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+  }
+}
+
 //常亮
 fun Activity.extKeepScreenOn() {
   window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
