f6ed5dec6fdf72bbeede5a66f1e7a31376eb347f
case <442415631@qq.com>
2019-11-20 17:27:06 +0700

    美化log打印

--- ablibrary/src/main/java/cc/ab/base/net/http/FuelHelper.kt
+++ ablibrary/src/main/java/cc/ab/base/net/http/FuelHelper.kt
@@ -17,56 +17,62 @@ object FuelHelper {
     requestInterceptor: FoldableRequestInterceptor? = null,
     responseInterceptor: FoldableResponseInterceptor? = null
   ) {
-    //服务器接口地址
-    FuelManager.instance.basePath = baseUrl
-    //超时时间
-    FuelManager.instance.timeoutInMillisecond = timeOut * 1000
-    FuelManager.instance.timeoutReadInMillisecond = timeOut * 1000
-    //添加拦截器
-    requestInterceptor?.let { FuelManager.instance.addRequestInterceptor(it) }
-    responseInterceptor?.let { FuelManager.instance.addResponseInterceptor(it) }
-    //添加请求日志拦截器
-    FuelManager.instance.addResponseInterceptor(CCResponseInterceptor)
-    FuelManager.instance.addRequestInterceptor(CCRequestInterceptor)
-    val ssl = CharlesUtils.getInstance()
-        .getFuelCharlesSSL(
-      CharlesUtils.getInstance().getCharlesInputStream("charles.pem")
-        )
-    if (ssl != null) {
-      FuelManager.instance.socketFactory = ssl
-    } else {
-      SSLManager.createSSLSocketFactory()
-          ?.let { FuelManager.instance.socketFactory = it }
+    FuelManager.instance.apply {
+      //服务器接口地址
+      basePath = baseUrl
+      //超时时间
+      timeoutInMillisecond = timeOut * 1000
+      timeoutReadInMillisecond = timeOut * 1000
+      //添加拦截器
+      requestInterceptor?.let { addRequestInterceptor(it) }
+      responseInterceptor?.let { addResponseInterceptor(it) }
+      //添加请求日志拦截器
+      addResponseInterceptor(CCResponseInterceptor)
+      addRequestInterceptor(CCRequestInterceptor)
+      val ssl = CharlesUtils.getInstance()
+        .getFuelCharlesSSL(CharlesUtils.getInstance().getCharlesInputStream("charles.pem"))
+      if (ssl != null) {
+        socketFactory = ssl
+      } else {
+        SSLManager.createSSLSocketFactory()?.let { socketFactory = it }
+      }
     }
   }
+
+  //换行符
+  private var line = "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t"
+  //空格符
+  private var space = "\t"
+  //每行最大数量
+  private var lineCount = 150
 
   private object CCRequestInterceptor : FoldableRequestInterceptor {
     override fun invoke(next: RequestTransformer): RequestTransformer {
       return { request ->
         val logging = StringBuffer()
-        logging.append("\n ")
-        logging.append("\n┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━请求参数拦截开始━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>>")
-        logging.append("\n┃")
-        logging.append("\n┣━━━   Method = ${request.method}")
-        logging.append("\n┃")
-        logging.append("\n┣━━━   headers = ${request.headers}")
-        logging.append("\n┃")
-        logging.append("\n┣━━━   url = ${request.url}")
+        logging.append("↓↓↓\n ")
+        logging.append("$line┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━请求参数拦截开始━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>>")
+        logging.append("$line┃")
+        logging.append("$line┣━━━   Method = ${request.method}")
+        logging.append("$line┃")
+        logging.append("$line┣━━━   headers = ${request.headers}")
+        logging.append("$line┃")
+        logging.append("$line┣━━━   url = ${request.url}")
         when (request.method) {
           Method.POST -> {
             if (!request.parameters.isNullOrEmpty()) {
-              logging.append("\n┃")
-              logging.append("\n┣━━━   request parameters:")
+              logging.append("$line┃")
+              logging.append("$line┣━━━   request parameters:")
             }
             request.parameters.forEach {
-              logging.append("\n┣━━━      [${it.first}]=${it.second}")
+              logging.append("$line┣━━━      [${it.first}]=${it.second}")
             }
           }
           else -> {
           }
         }
-        logging.append("\n┃")
-        logging.append("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━请求参数拦截结束━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━<<<\n")
+        logging.append("$line┃")
+        logging.append("$line┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━请求参数拦截结束━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━<<<\n")
         Log.i("request", logging.toString())
         next(request)
       }
@@ -77,35 +83,51 @@ object FuelHelper {
     override fun invoke(next: ResponseTransformer): ResponseTransformer {
       return { request, response ->
         val logging = StringBuffer()
-        logging.append("\n ")
-        logging.append("\n┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━响应结果拦截开始━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>>")
-        logging.append("\n┃")
-        logging.append("\n┣━━━   Method = ${request.method}")
-        logging.append("\n┃")
-        logging.append("\n┣━━━   headers = ${request.headers}")
+        logging.append("↓↓↓\n ")
+        logging.append("$line┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━响应结果拦截开始━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━>>>")
+        logging.append("$line┃")
+        logging.append("$line┣━━━   Method = ${request.method}")
+        logging.append("$line┃")
+        logging.append("$line┣━━━   headers = ${request.headers}")
         when (request.method) {
           Method.POST -> {
             if (!request.parameters.isNullOrEmpty()) {
-              logging.append("\n┃")
-              logging.append("\n┣━━━   request parameters:")
+              logging.append("$line┃")
+              logging.append("$line┣━━━   request parameters:")
             }
             request.parameters.forEach {
-              logging.append("\n┣━━━      [${it.first}]=${it.second}")
+              logging.append("$line┣━━━      [${it.first}]=${it.second}")
             }
           }
           else -> {
           }
         }
-        logging.append("\n┃")
+        logging.append("$line┃")
         logging.append(
-          "\n┣━━━   response = ${response.toString().trim().replace(
-            "\n",
-            "\n┃                     "
-          )}"
+          "$line┣━━━   response = ${response.toString().trim()
+            .replace(
+              "\n",
+              "$line┃                     "
+            )
+          }"
         )
-        logging.append("\n┃")
-        logging.append("\n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━响应结果拦截结束━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━<<<\n")
-        Log.i("response", logging.toString())
+        logging.append("$line┃")
+        logging.append("$line┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━响应结果拦截结束━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━<<<\n")
+        val size =
+          logging.toString().length / lineCount + if (logging.toString().length % lineCount > 0) 1 else 0
+        for (i in 0 until size) {
+          val msg = logging.toString().substring(
+            i * lineCount,
+            Math.min((i + 1) * lineCount, logging.toString().length)
+          ).trim()
+          Log.i(
+            "response",
+            (if (i > 0 && !msg.startsWith("┃") && !msg.startsWith("┣") && !msg.startsWith("┗")) "$space┃                     "
+            else if (i > 0 && (msg.startsWith("┃") || msg.startsWith("┣") || msg.startsWith("┗"))) space
+            else "") + msg
+
+          )
+        }
         next(request, response)
       }
     }
