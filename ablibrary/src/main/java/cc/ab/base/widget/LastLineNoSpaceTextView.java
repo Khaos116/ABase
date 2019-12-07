c399f8777be223ec9d2877e70ab82efc8d98d08d
case <442415631@qq.com>
2019-12-07 16:38:48 +0800

    去除TextView设置lineSpacingExtra后，最后一行多出的空白

---
+++ ablibrary/src/main/java/cc/ab/base/widget/LastLineNoSpaceTextView.java
@@ -0,0 +1,58 @@
+package cc.ab.base.widget;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.text.Layout;
+import android.util.AttributeSet;
+import androidx.appcompat.widget.AppCompatTextView;
+
+/**
+ * Description:去除TextView设置lineSpacingExtra后，最后一行多出的空白
+ * https://www.cnblogs.com/tangZH/p/11985745.html
+ *
+ * @author: caiyoufei
+ * @date: 2019/12/7 16:37
+ */
+public class LastLineNoSpaceTextView extends AppCompatTextView {
+  private Rect mRect;
+
+  public LastLineNoSpaceTextView(Context context) {
+    super(context);
+    init();
+  }
+
+  private void init() {
+    mRect = new Rect();
+  }
+
+  public LastLineNoSpaceTextView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public LastLineNoSpaceTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  protected void onMeasure(int i, int i2) {
+    super.onMeasure(i, i2);
+    int measuredHeight = getMeasuredHeight() - getLastLineSpace();
+    setMeasuredDimension(getMeasuredWidth(), measuredHeight);
+  }
+
+  public int getLastLineSpace() {
+    int lastLineIndex = getLineCount() - 1;
+    if (lastLineIndex < 0) {
+      return 0;
+    }
+    Layout layout = getLayout();
+    int baseline = getLineBounds(lastLineIndex, mRect);
+    if (getMeasuredHeight() - getPaddingTop() - getPaddingBottom() != layout.getHeight()) {
+      return 0;
+    }
+    int fontHeight = baseline + layout.getPaint().getFontMetricsInt().descent;
+    int lineHeight = mRect.bottom;
+    return lineHeight - fontHeight;
+  }
+}
