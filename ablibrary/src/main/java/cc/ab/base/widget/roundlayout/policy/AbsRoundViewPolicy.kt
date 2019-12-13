85f503c9786d0a37049c96017988e8c730fb1f60
case <442415631@qq.com>
2019-12-13 17:05:14 +0800

    修改为默认没有圆角

--- ablibrary/src/main/java/cc/ab/base/widget/roundlayout/policy/AbsRoundViewPolicy.kt
+++ ablibrary/src/main/java/cc/ab/base/widget/roundlayout/policy/AbsRoundViewPolicy.kt
@@ -13,36 +13,39 @@ import cc.ab.base.widget.roundlayout.dip2px
  *
  */
 abstract class AbsRoundViewPolicy(
-    view: View,
+  view: View,
+  context: Context,
+  attributeSet: AttributeSet?,
+  attrs: IntArray,
+  attrIndex: Int
+) : IRoundViewPolicy {
+  var mCornerRadius: Float = 0f
+  val mContainer: View = view
+
+  init {
+    initialize(context, attributeSet, attrs, attrIndex)
+  }
+
+  private fun initialize(
     context: Context,
     attributeSet: AttributeSet?,
     attrs: IntArray,
     attrIndex: Int
-) :
-    IRoundViewPolicy {
-    var mCornerRadius: Float = 0f
-    val mContainer: View = view
+  ) {
+    val typedArray = context.obtainStyledAttributes(attributeSet, attrs)
+    //get corner radius from xml file, default value is DEFAULT_CORNER_DP_SIZE
+    mCornerRadius = typedArray.getDimension(
+        attrIndex,
+        dip2px(context, DEFAULT_CORNER_DP_SIZE)
+    )
+    typedArray.recycle()
+  }
 
-    init {
-        initialize(context, attributeSet, attrs, attrIndex)
-    }
+  override fun setCornerRadius(cornerRadius: Float) {
+    this.mCornerRadius = cornerRadius
+  }
 
-    private fun initialize(context: Context, attributeSet: AttributeSet?, attrs: IntArray, attrIndex: Int) {
-        val typedArray = context.obtainStyledAttributes(attributeSet, attrs)
-        //get corner radius from xml file, default value is DEFAULT_CORNER_DP_SIZE
-        mCornerRadius = typedArray.getDimension(
-            attrIndex,
-            dip2px(context, DEFAULT_CORNER_DP_SIZE)
-        )
-        typedArray.recycle()
-    }
-
-    override fun setCornerRadius(cornerRadius: Float) {
-        this.mCornerRadius = cornerRadius
-    }
-
-    companion object {
-        const val DEFAULT_CORNER_DP_SIZE = 4f
-    }
-
+  companion object {
+    const val DEFAULT_CORNER_DP_SIZE = 0f
+  }
 }
