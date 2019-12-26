c1096608955c8c098a9264bb543386215817e6c6
case <442415631@qq.com>
2019-12-26 12:41:26 +0700

    监听列表滑动高度

---
+++ app/src/main/java/cc/abase/demo/widget/OffsetLinearLayoutManager.kt
@@ -0,0 +1,45 @@
+package cc.abase.demo.widget
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Description:监听列表滑动高度
+ * @author: caiyoufei
+ * @date: 2019/12/26 12:28
+ */
+class OffsetLinearLayoutManager(
+  context: Context,
+  call: ((offset: Int) -> Unit)? = null
+) :
+    LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false) {
+  private var callBack: ((offset: Int) -> Unit)? = call
+  private val heightMap: MutableMap<Int, Int> = HashMap()
+  override fun onLayoutCompleted(state: RecyclerView.State) {
+    super.onLayoutCompleted(state)
+    val count: Int = childCount
+    for (i in 0 until count) {
+      val view: View? = getChildAt(i)
+      heightMap[i] = view?.height ?: 0
+    }
+  }
+
+  override fun computeVerticalScrollOffset(state: RecyclerView.State): Int {
+    return if (childCount == 0) {
+      0
+    } else try {
+      val firstVisiablePosition: Int = findFirstVisibleItemPosition()
+      val firstVisiableView: View? = findViewByPosition(firstVisiablePosition)
+      var offsetY = -(firstVisiableView?.y ?: 0f).toInt()
+      for (i in 0 until firstVisiablePosition) {
+        offsetY += heightMap[i] ?: 0
+      }
+      callBack?.invoke(offsetY)
+      offsetY
+    } catch (e: Exception) {
+      0
+    }
+  }
+}
