d5b3b8e6853c462440f7ebeb81baac5cd44eba2a
case <442415631@qq.com>
2020-01-02 16:28:10 +0700

    更新lib

--- ablibrary/src/main/java/cc/ab/base/widget/engine/PicSelEngine.kt
+++ ablibrary/src/main/java/cc/ab/base/widget/engine/PicSelEngine.kt
@@ -1,15 +1,23 @@
 package cc.ab.base.widget.engine
 
 import android.content.Context
+import android.graphics.PointF
 import android.widget.ImageView
 import cc.ab.base.R
+import cc.ab.base.ext.gone
 import cc.ab.base.ext.loadSquare
+import cc.ab.base.ext.visible
+import cc.ab.base.ext.visibleGone
 import cc.ab.base.utils.MediaUtils
 import cc.ab.base.widget.sketch.VideoThumbnailUriModel
 import com.luck.picture.lib.engine.ImageEngine
+import com.luck.picture.lib.widget.longimage.ImageSource
+import com.luck.picture.lib.widget.longimage.ImageViewState
+import com.luck.picture.lib.widget.longimage.SubsamplingScaleImageView
 import me.panpf.sketch.Sketch
 import me.panpf.sketch.SketchImageView
 import me.panpf.sketch.request.*
+
 
 /**
  * Description:
@@ -77,6 +85,61 @@ class PicSelEngine : ImageEngine {
           }
         })
         .commit()
+  }
+
+  //加载网络长图适配
+  override fun loadImage(
+    context: Context,
+    url: String,
+    imageView: ImageView,
+    longImageView: SubsamplingScaleImageView
+  ) {
+    imageView.visible()
+    longImageView.gone()
+    //如果是相同的图片则不再进行加载
+    if (imageView.getTag(R.id.id_tag_sketch_suc) == url) return
+    imageView.setTag(R.id.id_tag_sketch_suc, null)
+    Sketch.with(context)
+      .load(url, object : LoadListener {
+        override fun onStarted() {
+          imageView.setImageResource(R.drawable.place_holder_square_loading2)
+        }
+
+        override fun onCanceled(cause: CancelCause) {
+          imageView.setImageResource(R.drawable.place_holder_square_fail2)
+        }
+
+        override fun onError(cause: ErrorCause) {
+          imageView.setImageResource(R.drawable.place_holder_square_fail2)
+        }
+
+        override fun onCompleted(result: LoadResult) {
+          result.bitmap?.let { resource ->
+            imageView.setTag(R.id.id_tag_sketch_suc, url)
+            val eqLongImage: Boolean = com.luck.picture.lib.tools.MediaUtils.isLongImg(
+              resource.width,
+              resource.height
+            )
+            longImageView.visibleGone(eqLongImage)
+            imageView.visibleGone(!eqLongImage)
+            if (eqLongImage) { // 加载长图
+              longImageView.isQuickScaleEnabled = true
+              longImageView.isZoomEnabled = true
+              longImageView.isPanEnabled = true
+              longImageView.setDoubleTapZoomDuration(100)
+              longImageView.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_CENTER_CROP)
+              longImageView.setDoubleTapZoomDpi(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER)
+              longImageView.setImage(
+                ImageSource.bitmap(resource),
+                ImageViewState(0f, PointF(0f, 0f), 0)
+              )
+            } else { // 普通图片
+              imageView.setImageBitmap(resource)
+            }
+          }
+        }
+      })
+      .commit()
   }
 
   override fun loadAsGifImage(
