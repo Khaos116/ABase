102554a2b303739b0c22e78739e58923bda14036
case <442415631@qq.com>
2020-02-19 15:24:04 +0700

    增加分割线展示

---
+++ app/src/main/java/cc/abase/demo/component/decoration/DecorationActivity.kt
@@ -0,0 +1,67 @@
+package cc.abase.demo.component.decoration
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.Gravity
+import androidx.recyclerview.widget.GridLayoutManager
+import cc.abase.demo.R
+import cc.abase.demo.component.comm.CommTitleActivity
+import cc.abase.demo.epoxy.item.simpleTextItem
+import cc.abase.demo.mvrx.MvRxEpoxyController
+import cc.abase.demo.widget.ItemDecorationPowerful
+import com.airbnb.epoxy.EpoxyItemSpacingDecorator
+import com.blankj.utilcode.util.StringUtils
+import kotlinx.android.synthetic.main.activity_decoration.decorRecycler
+
+/**
+ * Description:分割线展示
+ * @author: caiyoufei
+ * @date: 2020/2/19 14:20
+ */
+class DecorationActivity : CommTitleActivity() {
+
+  companion object {
+    fun startActivity(context: Context) {
+      val intent = Intent(context, DecorationActivity::class.java)
+      context.startActivity(intent)
+    }
+  }
+
+  override fun layoutResContentId() = R.layout.activity_decoration
+
+  override fun initContentView() {
+    setTitleText(StringUtils.getString(R.string.title_decoration))
+    val spanCount = 3
+    val layoutManager = GridLayoutManager(this, spanCount)
+    epoxyController.spanCount = spanCount
+    layoutManager.spanSizeLookup = epoxyController.spanSizeLookup
+    decorRecycler.layoutManager = layoutManager
+    decorRecycler.adapter = epoxyController.adapter
+    val hasEdge = System.currentTimeMillis() % 2 == 0L
+    val decorator = if (!hasEdge) EpoxyItemSpacingDecorator(20)
+    else ItemDecorationPowerful(ItemDecorationPowerful.GRID_DIV, Color.CYAN, 20)
+    decorRecycler.addItemDecoration(decorator)
+  }
+
+  override fun initData() {
+    val datas = mutableListOf<String>()
+    for (i in 1..30) {
+      datas.add("我是第${i}个元素")
+    }
+    epoxyController.data = datas
+  }
+
+  private val epoxyController = MvRxEpoxyController<MutableList<String>> { list ->
+    list.forEachIndexed { index, s ->
+      simpleTextItem {
+        id(index)
+        gravity(Gravity.CENTER)
+        bgColor(Color.parseColor("#550fff00"))
+        heightDp(70f)
+        msg(s)
+        spanCount = 1
+      }
+    }
+  }
+}
--- app/src/main/java/cc/abase/demo/component/main/MineFragment.kt
+++ app/src/main/java/cc/abase/demo/component/main/MineFragment.kt
@@ -8,13 +8,12 @@ import android.view.Gravity
 import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
-import cc.ab.base.ext.gone
-import cc.ab.base.ext.toast
-import cc.ab.base.ext.visible
+import cc.ab.base.ext.*
 import cc.abase.demo.R
 import cc.abase.demo.component.chat.ChatActivity
 import cc.abase.demo.component.comm.CommFragment
 import cc.abase.demo.component.coordinator.CoordinatorActivity
+import cc.abase.demo.component.decoration.DecorationActivity
 import cc.abase.demo.component.drag.DragActivity
 import cc.abase.demo.component.expand.EpoxyExpandActivity
 import cc.abase.demo.component.ffmpeg.RxFFmpegActivity
@@ -29,12 +28,10 @@ import cc.abase.demo.epoxy.base.dividerItem
 import cc.abase.demo.epoxy.item.simpleTextItem
 import cc.abase.demo.mvrx.MvRxEpoxyController
 import cc.abase.demo.repository.UserRepository
-import com.blankj.utilcode.util.ColorUtils
-import com.blankj.utilcode.util.SizeUtils
-import com.blankj.utilcode.util.StringUtils
+import com.blankj.utilcode.util.*
 import com.jeremyliao.liveeventbus.LiveEventBus
 import kotlinx.android.synthetic.main.fragment_mine.*
-import java.util.*
+import java.util.Locale
 
 /**
  * Description:
@@ -46,16 +43,17 @@ class MineFragment : CommFragment() {
   private val apkUrk = "https://down8.xiazaidb.com/app/yingyongbianliang.apk"
   //菜单列表
   private val menuList = mutableListOf(
-    Pair(StringUtils.getString(R.string.chat_title), ChatActivity::class.java),
-    Pair(StringUtils.getString(R.string.ffmpeg_title), RxFFmpegActivity::class.java),
-    Pair(StringUtils.getString(R.string.update_app), CcUpdateService::class.java),
-    Pair(StringUtils.getString(R.string.title_sticky), StickyActivity::class.java),
-    Pair(StringUtils.getString(R.string.title_drag), DragActivity::class.java),
-    Pair(StringUtils.getString(R.string.title_spedit), SpeditActivity::class.java),
-    Pair(StringUtils.getString(R.string.coordinator_refresh), CoordinatorActivity::class.java),
-    Pair(StringUtils.getString(R.string.epoxy_expandable), EpoxyExpandActivity::class.java),
-    Pair(StringUtils.getString(R.string.title_play_list), PlayListActivity::class.java),
-    Pair(StringUtils.getString(R.string.title_play_pager), PlayPagerActivity::class.java)
+      Pair(StringUtils.getString(R.string.chat_title), ChatActivity::class.java),
+      Pair(StringUtils.getString(R.string.ffmpeg_title), RxFFmpegActivity::class.java),
+      Pair(StringUtils.getString(R.string.update_app), CcUpdateService::class.java),
+      Pair(StringUtils.getString(R.string.title_sticky), StickyActivity::class.java),
+      Pair(StringUtils.getString(R.string.title_drag), DragActivity::class.java),
+      Pair(StringUtils.getString(R.string.title_spedit), SpeditActivity::class.java),
+      Pair(StringUtils.getString(R.string.coordinator_refresh), CoordinatorActivity::class.java),
+      Pair(StringUtils.getString(R.string.epoxy_expandable), EpoxyExpandActivity::class.java),
+      Pair(StringUtils.getString(R.string.title_play_list), PlayListActivity::class.java),
+      Pair(StringUtils.getString(R.string.title_play_pager), PlayPagerActivity::class.java),
+      Pair(StringUtils.getString(R.string.title_decoration), DecorationActivity::class.java)
   )
   //item文字颜色
   private var typeColor = ColorUtils.getColor(R.color.style_Primary)
@@ -77,40 +75,40 @@ class MineFragment : CommFragment() {
     showLoadingView()
     mineRoot.gone()
     UserRepository.instance.myIntegral()
-      .compose(lifecycleProvider.bindUntilEvent(Lifecycle.Event.ON_DESTROY))
-      .subscribe { t1, t2 ->
-        dismissLoadingView()
-        mineRoot?.visible()
-        if (t1 != null) {
-          mineIntegral.text =
-            String.format(StringUtils.getString(R.string.my_integral), t1.coinCount)
-        } else if (t2 != null) {
-          mineIntegral.text =
-            String.format(StringUtils.getString(R.string.my_integral), 0)
-          mContext.toast(t2.message)
+        .compose(lifecycleProvider.bindUntilEvent(Lifecycle.Event.ON_DESTROY))
+        .subscribe { t1, t2 ->
+          dismissLoadingView()
+          mineRoot?.visible()
+          if (t1 != null) {
+            mineIntegral.text =
+              String.format(StringUtils.getString(R.string.my_integral), t1.coinCount)
+          } else if (t2 != null) {
+            mineIntegral.text =
+              String.format(StringUtils.getString(R.string.my_integral), 0)
+            mContext.toast(t2.message)
+          }
+          epoxyController.data = menuList
         }
-        epoxyController.data = menuList
-      }
     val cls = Pair(UpdateEnum.START, 0f).javaClass
     LiveEventBus.get(EventKeys.UPDATE_PROGRESS, cls)
-      .observe(this, Observer {
-        when {
-          it.first == UpdateEnum.START -> {
-            Log.e("CASE", "APK开始下载")
+        .observe(this, Observer {
+          when {
+            it.first == UpdateEnum.START -> {
+              Log.e("CASE", "APK开始下载")
+            }
+            it.first == UpdateEnum.DOWNLOADING -> {
+              Log.e(
+                  "CASE", "APK下载进度：${String.format(Locale.getDefault(), "%.1f", it.second) + "%"}"
+              )
+            }
+            it.first == UpdateEnum.SUCCESS -> {
+              Log.e("CASE", "APK下载成功")
+            }
+            it.first == UpdateEnum.FAIL -> {
+              Log.e("CASE", "APK下载失败")
+            }
           }
-          it.first == UpdateEnum.DOWNLOADING -> {
-            Log.e(
-              "CASE", "APK下载进度：${String.format(Locale.getDefault(), "%.1f", it.second) + "%"}"
-            )
-          }
-          it.first == UpdateEnum.SUCCESS -> {
-            Log.e("CASE", "APK下载成功")
-          }
-          it.first == UpdateEnum.FAIL -> {
-            Log.e("CASE", "APK下载失败")
-          }
-        }
-      })
+        })
     mineScrollView.post {
       mineRecyclerParent.layoutParams.height = mineScrollView.height + SizeUtils.dp2px(70f)
     }
