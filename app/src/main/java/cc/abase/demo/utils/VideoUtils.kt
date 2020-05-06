9c51aa93f5abcf4a80278e5a88fd91354380f321
case <442415631@qq.com>
2020-05-06 16:07:23 +0700

    直接使用系统的视频压缩，不使用RxFFmpeg，以减少APK体积

--- app/src/main/java/cc/abase/demo/utils/VideoUtils.kt
+++ app/src/main/java/cc/abase/demo/utils/VideoUtils.kt
@@ -7,10 +7,12 @@ import android.util.Log
 import cc.ab.base.utils.RxUtils
 import cc.abase.demo.R
 import com.blankj.utilcode.util.*
-import io.microshow.rxffmpeg.RxFFmpegInvoke
-import io.microshow.rxffmpeg.RxFFmpegSubscriber
+import com.iceteck.silicompressorr.CompressCall
+import com.iceteck.silicompressorr.SiliCompressor
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import wseemann.media.FFmpegMediaMetadataRetriever
 import java.io.File
-import kotlin.math.min
 
 /**
  * Description:
@@ -18,10 +20,17 @@ import kotlin.math.min
  * @date: 2019/10/28 11:24
  */
 class VideoUtils private constructor() {
+  //<editor-fold defaultstate="collapsed" desc="变量区">
+
   //输出文件目录
   private val outParentVideo = PathUtils.getExternalAppDataPath() + File.separator + "video"
+
   //产生的封面保存地址
   private val outParentImgs = PathUtils.getExternalAppDataPath() + File.separator + "temp"
+
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化">
 
   private object SingletonHolder {
     val holder = VideoUtils()
@@ -29,11 +38,10 @@ class VideoUtils private constructor() {
 
   //创建文件夹
   init {
-    RxFFmpegInvoke.getInstance().setDebug(true)
     if (PermissionUtils.isGranted(
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE
-        )
+                                 )
     ) {
       if (!File(outParentVideo).exists())
         LogUtils.e("CASE:创建Video文件夹:${File(outParentVideo).mkdirs()}")
@@ -46,93 +54,245 @@ class VideoUtils private constructor() {
     val instance = SingletonHolder.holder
   }
 
-  //视频压缩进度
-  private var compressPro = -1
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="外部调用">
   //开始压缩
-  fun startCompressed(
-    originFile: File,
-    result: ((suc: Boolean, info: String) -> Unit)? = null,
-    pro: ((progress: Int) -> Unit)? = null
-  ) {
-    val outFile = File(outParentVideo, EncryptUtils.encryptMD5File2String(originFile) + ".mp4")
-    if (outFile.exists() && outFile.length() > 1024 * 1024) {//大于1M
-      //视频已压缩过，不再压缩，直接返回
-      result?.invoke(true, outFile.path)
+  fun startCompressed(originFile: File,
+      result: ((suc: Boolean, info: String) -> Unit)? = null,
+      pro: ((progress: Float) -> Unit)? = null) {
+    if (CompressCall.instance.progressCall != null) {
+      LogUtils.e("CASE:正在压缩中")
       return
     }
-    LogUtils.e("CASE:视频压缩前大小:${FileUtils.getSize(originFile)}")
-    val command = getCommandCompress(originFile.path, outFile.path)
-    //码率太低不进行压缩，直接拷贝原文件并返回
-    if (command == null) {
-      FileUtils.copy(originFile, outFile,null)
-      LogUtils.e("CASE:视频码率太小，不用压缩，直接拷贝上传")
-      result?.invoke(true, outFile.path)
-      return
-    }
-    LogUtils.e("CASE:执行的压缩命令:$command")
-    RxFFmpegInvoke.getInstance()
-        .runCommandRxJava(command)
-        .subscribe(object : RxFFmpegSubscriber() {
-          override fun onFinish() {
-            LogUtils.e("CASE:视频压缩成功后大小:${FileUtils.getSize(outFile)}")
-            result?.invoke(outFile.length() > 1024 * 1024, outFile.path)
-          }
-
-          override fun onCancel() {
-            result?.invoke(false, StringUtils.getString(R.string.compress_video_fail))
-          }
-
-          override fun onProgress(
-            progress: Int,
-            progressTime: Long
-          ) {
-            if (progress >= 0 && progress != compressPro) {
-              compressPro = progress
-              LogUtils.e("CASE:视频压缩进度:$progress")
-              pro?.invoke(progress)
-            }
-          }
-
-          override fun onError(message: String?) {
-            LogUtils.e("CASE:视频压缩失败")
-            result?.invoke(false, StringUtils.getString(R.string.compress_video_fail))
-          }
-        })
+    disposableCompress?.dispose()
+    CompressCall.instance.progressCall = { path, progress -> if (originFile.path == path) pro?.invoke(progress) }
+    disposableCompress = Observable.just(originFile.path)
+      .flatMap {
+        val resultPath = SiliCompressor.with(Utils.getApp()).compressVideo(it, outParentVideo)
+        if (File(resultPath).exists()) {
+          Observable.just(resultPath)
+        } else {
+          Observable.error(Throwable("压缩失败"))
+        }
+      }
+      .compose(RxUtils.instance.rx2SchedulerHelperO())
+      .subscribe({
+        CompressCall.instance.release()
+        result?.invoke(true, it)
+        LogUtils.e("\nCASE:视频压缩前大小:${FileUtils.getSize(originFile)}")
+        LogUtils.e("\nCASE:视频压缩后大小:${FileUtils.getSize(it)}\n")
+      }, {
+        CompressCall.instance.release()
+        result?.invoke(false, "压缩失败")
+      })
   }
 
   //获取视频封面第一帧
-  fun getFirstFrame(
-    originFile: File,
-    call: ((suc: Boolean, info: String) -> Unit)?
-  ) {
+  fun getFirstFrame(originFile: File, maxW: Int = 0, maxH: Int = 0, call: ((suc: Boolean, info: String) -> Unit)?) {
     val destImg = File(outParentImgs, EncryptUtils.encryptMD5File2String(originFile) + ".jpg")
-    val command = getCommandFirstFrame(originFile.path, destImg.path)
-    RxFFmpegInvoke.getInstance()
-        .runCommandRxJava(command)
-        .subscribe(object : RxFFmpegSubscriber() {
-          override fun onFinish() {
-            if (destImg.exists() && destImg.length() > 0) {
-              call?.invoke(true, destImg.path)
-            } else {
-              call?.invoke(false, StringUtils.getString(R.string.pic_first_frame_fail))
-            }
-          }
+    //已存在则直接返回
+    if (destImg.exists()) {
+      call?.invoke(true, destImg.path)
+      Log.e("CASE", "已存在封面，直接返回:${destImg.path}")
+      return
+    }
+    //获取封面
+    val mmr = FFmpegMediaMetadataRetriever()
+    mmr.setDataSource(originFile.path)
+    disposableCover?.dispose()
+    disposableCover = Observable.just(mmr)
+      .flatMap {
+        val rotation = mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION).toInt()
+        val originWidth = mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH).toInt()
+        val originHeight = mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT).toInt()
+        //限制图片的最大宽高
+        val maxWidth = if (maxW > 0) maxW else (ScreenUtils.getScreenWidth() * 0.8f).toInt()
+        val maxHeight = if (maxH > 0) maxH else (ScreenUtils.getScreenHeight() * 0.8f).toInt()
+        var width = originWidth
+        var height = originHeight
+        if (originWidth > maxWidth) {
+          width = maxWidth
+          height = (maxWidth * 1f / originWidth * originHeight).toInt()
+        } else if (originHeight > maxHeight) {
+          width = (maxHeight * 1f / originHeight * originWidth).toInt()
+          height = maxHeight
+        }
+        var bit = it.getScaledFrameAtTime(-1, width, height)
+        //纠正旋转角度
+        if (rotation != 0) bit = ImageUtils.rotate(bit, rotation, bit.width / 2f, bit.height / 2f)
+        //保存到指定位置
+        ImageUtils.save(bit, destImg, Bitmap.CompressFormat.JPEG)
+        Log.e("CASE", "\n修改前封面尺寸:width=${originWidth},height=${originHeight}")
+        Log.e("CASE", "\n修改后封面尺寸:width=${width},height=${height}\n")
+        bit.recycle()
+        Observable.just(destImg.path)
+      }
+      .compose(RxUtils.instance.rx2SchedulerHelperO())
+      .subscribe({
+        mmr.release()
+        call?.invoke(true, it)
+      }, {
+        mmr.release()
+        call?.invoke(true, StringUtils.getString(R.string.pic_first_frame_fail))
+      })
+  }
 
-          override fun onCancel() {
-            call?.invoke(false, StringUtils.getString(R.string.pic_first_frame_fail))
-          }
+  //网络视频封面获取
+  fun getNetVideoFistFrame(videoUrl: String, call: (bit: Bitmap?) -> Unit) {
+    disposableNet?.dispose()
+    disposableNet = Observable.just(videoUrl)
+      .flatMap {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(it, HashMap())
+        val bitmap = retriever.frameAtTime
+        retriever.release()
+        //限制图片的最大宽高
+        val maxWidth = (ScreenUtils.getScreenWidth() * 0.8f).toInt()
+        val maxHeight = (ScreenUtils.getScreenHeight() * 0.8f).toInt()
+        Observable.just(
+            when {
+              bitmap.width > maxWidth -> {
+                val width = maxWidth
+                val height = maxWidth * 1f / bitmap.width * bitmap.height
+                ImageUtils.compressBySampleSize(bitmap, width, height.toInt())
+              }
+              bitmap.height > maxHeight -> {
+                val width = maxHeight * 1f / bitmap.height * bitmap.width
+                val height = maxHeight
+                ImageUtils.compressBySampleSize(bitmap, width.toInt(), height)
+              }
+              else -> {
+                bitmap
+              }
+            })
+      }
+      .compose(RxUtils.instance.rx2SchedulerHelperO())
+      .subscribe({ bit ->
+        call.invoke(bit)
+      }, { call.invoke(null) })
+  }
+  //</editor-fold>
 
-          override fun onProgress(
-            progress: Int,
-            progressTime: Long
-          ) {
-          }
+  //<editor-fold defaultstate="collapsed" desc="★暂未使用★RxFFmpeg视频压缩和封面获取">
 
-          override fun onError(message: String?) {
-            call?.invoke(false, message ?: StringUtils.getString(R.string.pic_first_frame_fail))
-          }
-        })
+//  //封面获取命令
+//  @Throws
+//  private fun getCommandFirstFrame(
+//    originPath: String,
+//    outPath: String
+//  ): Array<String> {
+//    //读取图片尺寸和旋转角度
+//    val mMetadataRetriever = MediaMetadataRetriever()
+//    mMetadataRetriever.setDataSource(originPath)
+//    val videoRotation =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION)
+//    val videoHeight =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
+//    val videoWidth =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
+//    mMetadataRetriever.release()
+//    val width: Int
+//    val height: Int
+//    if (Integer.parseInt(videoRotation) == 90 || Integer.parseInt(videoRotation) == 270) {
+//      //角度不对需要宽高调换
+//      width = videoHeight.toInt()
+//      height = videoWidth.toInt()
+//    } else {
+//      width = videoWidth.toInt()
+//      height = videoHeight.toInt()
+//    }
+//    //return String.format("ffmpeg -y -i %1\$s -y -f image2 -t 0.001 -s %2\$sx%3\$s %4\$s",originPath, width, height, outPath)
+//    val list = mutableListOf<String>()
+//    list.add("ffmpeg")
+//    list.add("-y")
+//    list.add("-i")
+//    list.add(originPath)
+//    list.add("-y")
+//    list.add("-f")
+//    list.add("image2")
+//    list.add("-t")
+//    list.add("0.001")
+//    list.add("-s")
+//    list.add("${width}x${height}")
+//    list.add(outPath)
+//    return list.toTypedArray()//采用list转换，防止文件名带空格造成分割错误
+//  }
+//
+//  //文件压缩命令
+//  private fun getCommandCompress(
+//    originPath: String,
+//    outPath: String
+//  ): Array<String>? {
+//    //https://blog.csdn.net/qq_31332467/article/details/79166945
+//    //4K视频可能会闪退，所以需要添加尺寸压缩
+//    val mMetadataRetriever = MediaMetadataRetriever()
+//    mMetadataRetriever.setDataSource(originPath)
+//    val videoRotation =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION)
+//    val videoHeight =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
+//    val videoWidth =
+//      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
+//    val bitrate = mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_BITRATE)
+//    mMetadataRetriever.release()
+//    //码率低于400不进行压缩
+//    if (bitrate.toInt() < 400 * 1000) return null
+//    val newBitrate = if (bitrate.toInt() > 3000 * 1000) {
+//      3000
+//    } else {
+//      (bitrate.toInt() * 0.8f / 1000f).toInt()
+//    }
+//    val width: Int
+//    val height: Int
+//    if (Integer.parseInt(videoRotation) == 90 || Integer.parseInt(videoRotation) == 270) {
+//      //角度不对需要宽高调换
+//      width = videoHeight.toInt()
+//      height = videoWidth.toInt()
+//    } else {
+//      width = videoWidth.toInt()
+//      height = videoHeight.toInt()
+//    }
+//    //需要根据视频大小和视频时长计算得到需要压缩的码率，不然会导致高清视频压缩后变模糊，非高清视频压缩后文件变大
+//    //https://blog.csdn.net/zhezhebie/article/details/79263492
+//    val list = mutableListOf<String>()
+//    list.add("ffmpeg")
+//    list.add("-y")
+//    list.add("-i")
+//    list.add(originPath)
+//    list.add("-b")
+//    list.add("${newBitrate}k")
+//    list.add("-r")
+//    list.add("30")
+//    list.add("-vcodec")
+//    list.add("libx264")
+//    if (min(width, height) > 1080) {//大于1080p
+//      list.add("-vf")
+//      list.add("scale=${if (width > height) "1080:-1" else "-1:1080"}")
+//    }
+//    list.add("-preset")
+//    list.add("superfast")
+//    list.add(outPath)
+//    return list.toTypedArray()//采用list转换，防止文件名带空格造成分割错误
+//  }
+//</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="释放、清理">
+  private var disposableCompress: Disposable? = null
+  private var disposableCover: Disposable? = null
+  private var disposableNet: Disposable? = null
+
+  //释放
+  fun release() {
+    disposableCompress?.dispose()
+    disposableCover?.dispose()
+    disposableNet?.dispose()
+    CompressCall.instance.release()
+  }
+
+  //清理封面
+  fun clearFirstFrame() {
+    FileUtils.deleteAllInDir(outParentImgs)
   }
 
   //清理压缩的视频文件(保留发布失败的视频)
@@ -141,143 +301,15 @@ class VideoUtils private constructor() {
       FileUtils.deleteAllInDir(outParentVideo)
     } else {
       File(outParentVideo).listFiles()
-          ?.let { files ->
-            for (file in files) {
-              if (file == keepFile) {
-                continue
-              }
-              FileUtils.delete(file)
+        ?.let { files ->
+          for (file in files) {
+            if (file == keepFile) {
+              continue
             }
+            FileUtils.delete(file)
           }
+        }
     }
   }
-
-  //清理封面
-  fun clearFirstFrame() {
-    FileUtils.deleteAllInDir(outParentImgs)
-  }
-
-  //封面获取命令
-  @Throws
-  private fun getCommandFirstFrame(
-    originPath: String,
-    outPath: String
-  ): Array<String> {
-    //读取图片尺寸和旋转角度
-    val mMetadataRetriever = MediaMetadataRetriever()
-    mMetadataRetriever.setDataSource(originPath)
-    val videoRotation =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION)
-    val videoHeight =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
-    val videoWidth =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
-    mMetadataRetriever.release()
-    val width: Int
-    val height: Int
-    if (Integer.parseInt(videoRotation) == 90 || Integer.parseInt(videoRotation) == 270) {
-      //角度不对需要宽高调换
-      width = videoHeight.toInt()
-      height = videoWidth.toInt()
-    } else {
-      width = videoWidth.toInt()
-      height = videoHeight.toInt()
-    }
-    //return String.format("ffmpeg -y -i %1\$s -y -f image2 -t 0.001 -s %2\$sx%3\$s %4\$s",originPath, width, height, outPath)
-    val list = mutableListOf<String>()
-    list.add("ffmpeg")
-    list.add("-y")
-    list.add("-i")
-    list.add(originPath)
-    list.add("-y")
-    list.add("-f")
-    list.add("image2")
-    list.add("-t")
-    list.add("0.001")
-    list.add("-s")
-    list.add("${width}x${height}")
-    list.add(outPath)
-    return list.toTypedArray()//采用list转换，防止文件名带空格造成分割错误
-  }
-
-  //文件压缩命令
-  private fun getCommandCompress(
-    originPath: String,
-    outPath: String
-  ): Array<String>? {
-    //https://blog.csdn.net/qq_31332467/article/details/79166945
-    //4K视频可能会闪退，所以需要添加尺寸压缩
-    val mMetadataRetriever = MediaMetadataRetriever()
-    mMetadataRetriever.setDataSource(originPath)
-    val videoRotation =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION)
-    val videoHeight =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
-    val videoWidth =
-      mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
-    val bitrate = mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_BITRATE)
-    mMetadataRetriever.release()
-    //码率低于400不进行压缩
-    if (bitrate.toInt() < 400 * 1000) return null
-    val newBitrate = if (bitrate.toInt() > 3000 * 1000) {
-      3000
-    } else {
-      (bitrate.toInt() * 0.8f / 1000f).toInt()
-    }
-    val width: Int
-    val height: Int
-    if (Integer.parseInt(videoRotation) == 90 || Integer.parseInt(videoRotation) == 270) {
-      //角度不对需要宽高调换
-      width = videoHeight.toInt()
-      height = videoWidth.toInt()
-    } else {
-      width = videoWidth.toInt()
-      height = videoHeight.toInt()
-    }
-    //需要根据视频大小和视频时长计算得到需要压缩的码率，不然会导致高清视频压缩后变模糊，非高清视频压缩后文件变大
-    //https://blog.csdn.net/zhezhebie/article/details/79263492
-    val list = mutableListOf<String>()
-    list.add("ffmpeg")
-    list.add("-y")
-    list.add("-i")
-    list.add(originPath)
-    list.add("-b")
-    list.add("${newBitrate}k")
-    list.add("-r")
-    list.add("30")
-    list.add("-vcodec")
-    list.add("libx264")
-    if (min(width, height) > 1080) {//大于1080p
-      list.add("-vf")
-      list.add("scale=${if (width > height) "1080:-1" else "-1:1080"}")
-    }
-    list.add("-preset")
-    list.add("superfast")
-    list.add(outPath)
-    return list.toTypedArray()//采用list转换，防止文件名带空格造成分割错误
-  }
-
-  //网络视频封面获取
-  fun getNetVideoFistFrame(videoUrl: String, call: (bit: Bitmap?) -> Unit) {
-    val ob = io.reactivex.Observable.just(videoUrl)
-      .flatMap {
-        val retriever = MediaMetadataRetriever()
-        retriever.setDataSource(it, HashMap())
-        val bitmap = retriever.frameAtTime
-        retriever.release()
-        io.reactivex.Observable.just(
-          if (bitmap.width > ScreenUtils.getScreenWidth()) {
-            val width = ScreenUtils.getScreenWidth()
-            val height = ScreenUtils.getScreenWidth() * 1f / bitmap.width * bitmap.height
-            ImageUtils.compressBySampleSize(bitmap, width, height.toInt())
-          } else {
-            bitmap
-          }
-        )
-      }
-      .compose(RxUtils.instance.rx2SchedulerHelperO())
-      .subscribe({ bit ->
-        call.invoke(bit)
-      }, { call.invoke(null) })
-  }
+  //</editor-fold>
 }
