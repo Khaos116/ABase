52278609a69b7c049504381e901e5df28ae4d8fe
CASE <442415631@qq.com>
2020-10-10 16:38:23 +0700

    优化注释

--- ablibrary/src/main/java/cc/ab/base/widget/roundlayout/SizeUtils.kt
+++ ablibrary/src/main/java/cc/ab/base/widget/roundlayout/SizeUtils.kt
@@ -2,13 +2,13 @@ package cc.ab.base.widget.roundlayout
 
 import android.content.Context
 
-/**
- * 密度转换像素
- * @param dipValue dp值
- * @return 像素
- */
 class SizeUtils {
   companion object {
+      /**
+       * 密度转换像素
+       * @param dipValue dp值
+       * @return 像素
+       */
     fun dip2px(context: Context, dipValue: Float): Float {
       val displayMetrics = context.applicationContext.resources.displayMetrics
       return dipValue * displayMetrics.density + 0.5f
