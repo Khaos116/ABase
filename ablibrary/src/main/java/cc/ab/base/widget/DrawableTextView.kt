c3f8ec6193635eab95b2c8baebf4bfd95bec4c4f
CASE <442415631@qq.com>
2020-10-10 16:31:50 +0700

    增加自定义控件

---
+++ ablibrary/src/main/java/cc/ab/base/widget/DrawableTextView.kt
@@ -0,0 +1,90 @@
+package cc.ab.base.widget
+
+/**
+ * Author:CASE
+ * Date:2020-10-10
+ * Time:14:15
+ */
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import cc.ab.base.R
+
+@SuppressLint("PrivateResource", "CustomViewStyleable")
+class DrawableTextView(context: Context, attributeSet: AttributeSet?) :
+    AppCompatTextView(context, attributeSet) {
+  private var leftWidth = -1
+  private var leftHeight = -1
+  private var topWidth = -1
+  private var topHeight = -1
+  private var rightWidth = -1
+  private var rightHeight = -1
+  private var bottomWidth = -1
+  private var bottomHeight = -1
+
+  init {
+    val array = context.obtainStyledAttributes(attributeSet, R.styleable.DrawableTextView)
+
+    leftWidth = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableLeftWidth, -1)
+    leftHeight = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableLeftHeight, -1)
+
+    topWidth = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableTopWidth, -1)
+    topHeight = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableTopHeight, -1)
+
+    rightWidth = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableRightWidth, -1)
+    rightHeight = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableRightHeight, -1)
+
+    bottomWidth = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableBottomWidth, -1)
+    bottomHeight = array.getDimensionPixelSize(R.styleable.DrawableTextView_drawableBottomHeight, -1)
+
+    array.recycle()
+
+    val arrayHelper = context.obtainStyledAttributes(attributeSet, R.styleable.AppCompatTextHelper)
+
+    var drawableLeft: Drawable? = null
+    var drawableTop: Drawable? = null
+    var drawableRight: Drawable? = null
+    var drawableBottom: Drawable? = null
+    var drawableStart: Drawable? = null
+    var drawableEnd: Drawable? = null
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableLeft, -1)
+        .takeIf { it != -1 }?.apply { drawableLeft = ContextCompat.getDrawable(context, this) }
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableStart, -1)
+        .takeIf { it != -1 }?.apply { drawableStart = ContextCompat.getDrawable(context, this) }
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableTop, -1)
+        .takeIf { it != -1 }?.apply { drawableTop = ContextCompat.getDrawable(context, this) }
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableBottom, -1)
+        .takeIf { it != -1 }?.apply { drawableBottom = ContextCompat.getDrawable(context, this) }
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableEnd, -1)
+        .takeIf { it != -1 }?.apply { drawableEnd = ContextCompat.getDrawable(context, this) }
+    arrayHelper.getResourceId(R.styleable.AppCompatTextHelper_android_drawableRight, -1)
+        .takeIf { it != -1 }?.apply { drawableRight = ContextCompat.getDrawable(context, this) }
+
+    arrayHelper.recycle()
+    setCompoundDrawablesWithIntrinsicBounds(drawableStart ?: drawableLeft,
+        drawableTop, drawableEnd ?: drawableRight, drawableBottom)
+  }
+
+  override fun setCompoundDrawablesWithIntrinsicBounds(left: Drawable?, top: Drawable?, right: Drawable?, bottom: Drawable?) {
+    left?.setBounds(0, 0,
+        if (leftWidth != -1) leftWidth else left.intrinsicWidth,
+        if (leftHeight != -1) leftHeight else left.intrinsicHeight
+    )
+    top?.setBounds(0, 0,
+        if (topWidth != -1) topWidth else top.intrinsicWidth,
+        if (topHeight != -1) topHeight else top.intrinsicHeight
+    )
+    right?.setBounds(0, 0,
+        if (rightWidth != -1) rightWidth else right.intrinsicWidth,
+        if (rightHeight != -1) rightHeight else right.intrinsicHeight
+    )
+    bottom?.setBounds(0, 0,
+        if (bottomWidth != -1) bottomWidth else bottom.intrinsicWidth,
+        if (bottomHeight != -1) bottomHeight else bottom.intrinsicHeight
+    )
+    setCompoundDrawables(left, top, right, bottom)
+  }
+}
---
+++ ablibrary/src/main/java/cc/ab/base/widget/NoScrollViewPager.kt
@@ -0,0 +1,68 @@
+package cc.ab.base.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.animation.Interpolator
+import android.widget.Scroller
+import androidx.viewpager.widget.ViewPager
+import java.lang.reflect.Field
+
+/**
+ * Author:CASE
+ * Date:2020-10-10
+ * Time:10:10
+ */
+class NoScrollViewPager(context: Context, attrs: AttributeSet?) : ViewPager(context, attrs) {
+  //<editor-fold defaultstate="collapsed" desc="初始化">
+  init {
+    initSpeedViewPager()
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="禁止ViewPager手动滚动">
+  //是否可以手动滑动,false禁止
+  var canScroll = false
+
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onTouchEvent(ev: MotionEvent?): Boolean = if (!canScroll) false else super.onTouchEvent(ev)
+
+  override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = if (!canScroll) false else super.onInterceptTouchEvent(ev)
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="反射修改滚动速度">
+  private var scroller: SpeedScroller? = null
+  private fun initSpeedViewPager() {
+    try {
+      val scrollerField: Field = ViewPager::class.java.getDeclaredField("mScroller")
+      scrollerField.isAccessible = true
+      val interpolator: Field = ViewPager::class.java.getDeclaredField("sInterpolator")
+      interpolator.isAccessible = true
+      scroller = SpeedScroller(context, interpolator.get(null) as Interpolator)
+      scrollerField.set(this, scroller)
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="外部设置滚动速度">
+  fun setScrollSpeed(scrollSpeed: Float) {
+    scroller?.setScrollSpeed(scrollSpeed)
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="速度控制">
+  class SpeedScroller(context: Context, interpolator: Interpolator) : Scroller(context, interpolator) {
+    private var scrollSpeed = 1f//数值越大滚动越慢
+    fun setScrollSpeed(speed: Float) {
+      scrollSpeed = speed
+    }
+
+    override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+      super.startScroll(startX, startY, dx, dy, (duration * scrollSpeed).toInt())
+    }
+  }
+  //</editor-fold>
+}
