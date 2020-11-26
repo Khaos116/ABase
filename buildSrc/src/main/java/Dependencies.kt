c1896d65f993c2c2040dd2f5db28deb4ea75bb28
CASE <442415631@qq.com>
2020-11-26 16:52:27 +0700

    版本升级

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,9 +1,9 @@
 object Versions {
   internal const val sdkMin = 23
   internal const val sdkTarget = 30
-  internal const val kotlin = "1.4.10"
-  internal const val okHttp = "4.8.1"
-  internal const val rxHttp = "2.4.1"
+  internal const val kotlin = "1.4.20"
+  internal const val okHttp = "4.9.0"
+  internal const val rxHttp = "2.4.4"
   internal const val doraemonkit = "3.2.0"
   internal const val dkplayer = "3.2.6"
   internal const val epoxy = "4.1.0"
