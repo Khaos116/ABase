2cf088ea6e7ee77c0ac5cc805d9dd434f9e81fa5
CASE <442415631@qq.com>
2020-12-11 15:01:57 +0700

    修改插件使用方式

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
   internal const val sdkMin = 23
   internal const val sdkTarget = 30
-  internal const val kotlin = "1.4.20"
+  internal const val kotlin = "1.4.21"
   internal const val okHttp = "4.9.0"
   internal const val rxHttp = "2.4.4"
   internal const val doraemonkit = "3.2.0"
