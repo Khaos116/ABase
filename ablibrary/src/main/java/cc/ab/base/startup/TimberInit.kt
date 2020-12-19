0c58f17ccfcad3551d7f63cf3b219689bad1f9c2
CASE <442415631@qq.com>
2020-12-19 15:21:46 +0700

    增加初始化

---
+++ ablibrary/src/main/java/cc/ab/base/startup/TimberInit.kt
@@ -0,0 +1,24 @@
+package cc.ab.base.startup
+
+import android.content.Context
+import androidx.startup.Initializer
+import cc.ab.base.BuildConfig
+import cc.ab.base.ext.logI
+import timber.log.Timber
+
+/**
+ * Author:CASE
+ * Date:2020/12/19
+ * Time:15:11
+ */
+class TimberInit : Initializer<Int> {
+    override fun create(context: Context): Int {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        "初始化完成".logI()
+        return 0
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
---
+++ ablibrary/src/main/java/cc/ab/base/startup/UtilsInit.kt
@@ -0,0 +1,24 @@
+package cc.ab.base.startup
+
+import android.app.Application
+import android.content.Context
+import androidx.startup.Initializer
+import cc.ab.base.ext.logI
+import com.blankj.utilcode.util.Utils
+
+/**
+ * Author:CASE
+ * Date:2020/12/19
+ * Time:15:08
+ */
+class UtilsInit : Initializer<Int> {
+    override fun create(context: Context): Int {
+        Utils.init(context.applicationContext as Application)
+        "初始化完成".logI()
+        return 0
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf(TimberInit::class.java)
+    }
+}
