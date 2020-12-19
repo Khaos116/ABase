2300553c47bb34e0e6e7e7fc55811be45938cef8
CASE <442415631@qq.com>
2020-12-19 16:25:56 +0700

    删除DKPlayer，准备替换为gysPlayer

---
+++ app/src/main/java/cc/abase/demo/widget/video/MyVideoView.kt
@@ -0,0 +1,13 @@
+package cc.abase.demo.widget.video
+
+import android.content.Context
+import android.util.AttributeSet
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
+
+/**
+ * Author:CASE
+ * Date:2020/12/19
+ * Time:16:07
+ */
+class MyVideoView(context: Context, attrs: AttributeSet? = null) : StandardGSYVideoPlayer(context, attrs) {
+}
