908e0a77f5c5f7001e456c1bb47cfaef9a6c5c95
CASE <442415631@qq.com>
2020-12-21 18:46:31 +0700

    简化

--- app/src/main/java/cc/abase/demo/component/verticalpage/VerticalViewHolderCreator.kt
+++ app/src/main/java/cc/abase/demo/component/verticalpage/VerticalViewHolderCreator.kt
@@ -2,17 +2,11 @@ package cc.abase.demo.component.verticalpage
 
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.TextView
-import cc.ab.base.ext.load
 import cc.ab.base.widget.discretescrollview.holder.DiscreteHolder
 import cc.ab.base.widget.discretescrollview.holder.DiscreteHolderCreator
 import cc.abase.demo.R
 import cc.abase.demo.bean.local.VerticalPageBean
-import kotlinx.android.synthetic.main.item_vertical_page.view.itemVerticalThumb
-import kotlinx.android.synthetic.main.item_vertical_page.view.itemVerticalTitle
 import kotlinx.android.synthetic.main.item_vertical_page_parent.view.itemVerticalContainer
-import kotlinx.android.synthetic.main.item_vertical_page_parent.view.itemVerticalPIV
-import me.panpf.sketch.SketchImageView
 
 /**
  * Description:
@@ -26,23 +20,11 @@ class VerticalPageHolderCreator : DiscreteHolderCreator {
 }
 
 class VerticalPageHolderView(view: View) : DiscreteHolder<VerticalPageBean>(view) {
-
-  lateinit var tvDes: TextView
-  lateinit var ivCover: SketchImageView
-  lateinit var parentVideo: FrameLayout
-  lateinit var piv: VerticalPagerItemView
-
+  var container: FrameLayout? = null
   override fun updateUI(data: VerticalPageBean, position: Int, count: Int) {
-    tvDes.text = data.description
-    ivCover.load(data.cover)
-    ivCover.clearAnimation()
-    if (piv.parent == null) parentVideo.addView(piv)
   }
 
   override fun initView(view: View) {
-    this.tvDes = view.itemVerticalTitle
-    this.ivCover = view.itemVerticalThumb
-    this.parentVideo = view.itemVerticalContainer
-    this.piv = view.itemVerticalPIV
+    container = view.itemVerticalContainer
   }
 }
