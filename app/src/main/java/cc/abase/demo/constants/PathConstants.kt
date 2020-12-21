7c8a573a19cd73b15d8f036c195ee64123a0f10f
CASE <442415631@qq.com>
2020-12-21 16:06:09 +0700

    视频缓存目录

---
+++ app/src/main/java/cc/abase/demo/constants/PathConstants.kt
@@ -0,0 +1,13 @@
+package cc.abase.demo.constants
+
+import com.blankj.utilcode.util.PathUtils
+
+/**
+ * Author:CASE
+ * Date:2020/12/19
+ * Time:16:42
+ */
+object PathConstants {
+  //视频缓存目录
+  val videoCacheDir: String = PathUtils.getExternalAppMoviesPath()
+}
