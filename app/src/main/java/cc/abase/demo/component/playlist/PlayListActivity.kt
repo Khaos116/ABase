e8e6acd5304dbe6999d166b55b29ff9e62d5a995
CASE <442415631@qq.com>
2020-12-22 13:13:31 +0700

    修改播放方式

--- app/src/main/java/cc/abase/demo/component/playlist/PlayListActivity.kt
+++ app/src/main/java/cc/abase/demo/component/playlist/PlayListActivity.kt
@@ -15,7 +15,6 @@ import cc.abase.demo.epoxy.item.videoListItem
 import cc.abase.demo.mvrx.MvRxEpoxyController
 import cc.abase.demo.widget.video.MyVideoView
 import com.blankj.utilcode.util.StringUtils
-import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack
 import kotlinx.android.synthetic.main.activity_play_list.playListRecycler
 import kotlinx.android.synthetic.main.item_list_video.view.itemVideoContainer
 
@@ -62,25 +61,11 @@ class PlayListActivity : CommTitleActivity() {
     mVideoView = MyVideoView(mContext)
     mVideoView?.backButton?.gone()
     mVideoView?.titleTextView?.gone()
-    mVideoView?.setVideoAllCallBack(object : GSYSampleCallBack() {
-      override fun onComplete(url: String?, vararg objects: Any?) {
-        super.onComplete(url, *objects)
-        mVideoView?.removeParent()
-        mLastPos = mCurPos
-        mCurPos = -1
-      }
-    })
     //列表相关
     playListRecycler.setController(epoxyController)
-    playListRecycler.addOnChildAttachStateChangeListener(object :
-        RecyclerView.OnChildAttachStateChangeListener {
+    playListRecycler.addOnChildAttachStateChangeListener(object : RecyclerView.OnChildAttachStateChangeListener {
       override fun onChildViewDetachedFromWindow(view: View) {
-        view.itemVideoContainer?.getChildAt(0)
-            ?.let {
-              if (it == mVideoView && mVideoView?.isIfCurrentIsFullscreen == false) {
-                releaseVideoView()
-              }
-            }
+        view.itemVideoContainer?.getChildAt(0)?.let { if (it == mVideoView) releaseVideoView() }
       }
 
       override fun onChildViewAttachedToWindow(view: View) {
@@ -121,28 +106,16 @@ class PlayListActivity : CommTitleActivity() {
     }
   }
 
-  override fun onPause() {
-    super.onPause()
-    releaseVideoView()
-  }
-
-  override fun onResume() {
-    super.onResume()
-    if (mLastPos == -1) return
-    //恢复上次播放的位置
-    startPlay(mLastPos)
-  }
-
   //开始播放
   private fun startPlay(position: Int) {
     if (mCurPos == position) return
     if (mCurPos != -1) releaseVideoView()
-    val videoBean = mVideoList[position]
-    mVideoView?.setPlayUrl(videoBean.url ?: "", "", videoBean.thumb ?: "")
     playListRecycler.layoutManager?.findViewByPosition(position)
         ?.let {
           mVideoView?.removeParent()
-          it.itemVideoContainer.addView(mVideoView, 0)
+          it.itemVideoContainer.addView(mVideoView)
+          val videoBean = mVideoList[position]
+          mVideoView?.setPlayUrl(videoBean.url ?: "", "", videoBean.thumb ?: "")
           mVideoView?.startPlayLogic()
           mCurPos = position
         }
@@ -151,8 +124,8 @@ class PlayListActivity : CommTitleActivity() {
   //释放播放
   private fun releaseVideoView() {
     mVideoView?.let {
-      it.release()
-      it.onBackPress()
+      it.onVideoReset()
+      it.removeParent()
       mCurPos = -1
     }
   }
