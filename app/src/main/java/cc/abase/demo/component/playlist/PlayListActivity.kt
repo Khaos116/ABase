176400a3f26a964ec850bba99814144aa163e891
CASE <442415631@qq.com>
2020-12-31 10:38:28 +0700

    优化全屏播放和滑出屏幕停止播放

--- app/src/main/java/cc/abase/demo/component/playlist/PlayListActivity.kt
+++ app/src/main/java/cc/abase/demo/component/playlist/PlayListActivity.kt
@@ -13,7 +13,7 @@ import cc.abase.demo.component.playlist.viewmoel.PlayListViewModel
 import cc.abase.demo.epoxy.base.loadMoreItem
 import cc.abase.demo.epoxy.item.videoListItem
 import cc.abase.demo.mvrx.MvRxEpoxyController
-import cc.abase.demo.widget.video.MyVideoView
+import cc.abase.demo.widget.dkplayer.MyVideoView
 import com.blankj.utilcode.util.StringUtils
 import kotlinx.android.synthetic.main.activity_play_list.playListRecycler
 import kotlinx.android.synthetic.main.item_list_video.view.itemVideoContainer
@@ -56,13 +56,12 @@ class PlayListActivity : CommTitleActivity() {
     setTitleText(StringUtils.getString(R.string.title_play_list))
     //播放相关
     mVideoView = MyVideoView(mContext)
-    mVideoView?.backButton?.gone()
-    mVideoView?.titleTextView?.gone()
+    mVideoView?.setInList(true)
     //列表相关
     playListRecycler.setController(epoxyController)
     playListRecycler.addOnChildAttachStateChangeListener(object : RecyclerView.OnChildAttachStateChangeListener {
-      override fun onChildViewDetachedFromWindow(view: View) { //视频全屏的时候view.parent为空，正常滑出屏幕时view.parent不为空
-        view.itemVideoContainer?.getChildAt(0)?.let { if (it == mVideoView) releaseVideoView(view.parent != null) }
+      override fun onChildViewDetachedFromWindow(view: View) { //非全屏滑出去释放掉
+        view.itemVideoContainer?.getChildAt(0)?.let { if (it == mVideoView && mVideoView?.isFullScreen == false) releaseVideoView() }
       }
 
       override fun onChildViewAttachedToWindow(view: View) {
@@ -106,23 +105,22 @@ class PlayListActivity : CommTitleActivity() {
   //开始播放
   private fun startPlay(position: Int) {
     if (mCurPos == position) return
-    if (mCurPos != -1) releaseVideoView(true)
+    if (mCurPos != -1) releaseVideoView()
     playListRecycler.layoutManager?.findViewByPosition(position)
         ?.let {
           mVideoView?.removeParent()
           it.itemVideoContainer.addView(mVideoView)
           val videoBean = mVideoList[position]
-          mVideoView?.setPlayUrl(videoBean.url ?: "", "", videoBean.thumb ?: "")
-          mVideoView?.startPlayLogic()
+          mVideoView?.setPlayUrl(url = videoBean.url ?: "", autoPlay = true, needHolder = false)
           mCurPos = position
         }
   }
 
   //释放播放
-  private fun releaseVideoView(remove: Boolean) {
+  private fun releaseVideoView() {
     mVideoView?.let {
-      it.onVideoReset()
-      if (remove) it.removeParent()
+      it.release()
+      it.removeParent()
       mCurPos = -1
     }
   }
