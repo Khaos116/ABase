ec092c8ea8a9930629458f4a805110a3fc9d1fd3
CASE <442415631@qq.com>
2021-01-02 16:31:43 +0700

    添加viewModel

---
+++ ablibrary/src/main/java/cc/ab/base/ui/viewmodel/BaseViewModel.kt
@@ -0,0 +1,11 @@
+package cc.ab.base.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Author:case
+ * Date:2020/8/13
+ * Time:10:08
+ */
+abstract class BaseViewModel : ViewModel() {
+}
---
+++ ablibrary/src/main/java/cc/ab/base/ui/viewmodel/DataState.kt
@@ -0,0 +1,31 @@
+package cc.ab.base.ui.viewmodel
+
+/**
+ * https://www.cnblogs.com/Jetictors/p/8157969.html
+ * 密封类 用来表示受限的类继承结构
+ * Author:CASE
+ * Date:2020-11-30
+ * Time:15:46
+ */
+sealed class DataState<T>(val data: T? = null) {
+  //请求开始
+  class Start<T>(oldData: T?) : DataState<T>(data = oldData) {}
+
+  //请求结束
+  class Complete<T>(totalData: T?, val hasMore: Boolean) : DataState<T>(data = totalData) {}
+
+  //刷新成功
+  class SuccessRefresh<T>(newData: T?) : DataState<T>(data = newData) {}
+
+  //加载更多成功
+  class SuccessMore<T>(val newData: T?, totalData: T?) : DataState<T>(data = totalData) {}
+
+  //刷新失败
+  class FailRefresh<T>(oldData: T?, val exc: Throwable?) : DataState<T>(data = oldData) {}
+
+  //加载更多失败
+  class FailMore<T>(oldData: T?, val exc: Throwable?) : DataState<T>(data = oldData) {}
+
+  //判断是否数据可能改变
+  fun dataMaybeChange() = (this is Start || this is SuccessRefresh || this is SuccessMore || this is FailRefresh)
+}
