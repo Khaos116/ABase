12c7c0d3903949670c009212c8372950f882f533
CASE <442415631@qq.com>
2021-01-04 13:32:40 +0700

    Sticky适配器

---
+++ app/src/main/java/cc/abase/demo/sticky/StickyAnyAdapter.kt
@@ -0,0 +1,27 @@
+package cc.abase.demo.sticky
+
+import android.graphics.Color
+import android.view.View
+import cc.ab.base.ext.pressEffectBgColor
+import cc.ab.base.ext.pressEffectDisable
+import com.drakeet.multitype.*
+
+/**
+ * Author:case
+ * Date:2020/8/31
+ * Time:16:00
+ */
+abstract class StickyAnyAdapter(val i: List<Any> = emptyList(), val c: Int = 0, val t: Types = MutableTypes(c)
+) : MultiTypeAdapter(i, c, t), StickyHeaderCallbacks {
+  override fun setupStickyHeaderView(stickyHeader: View) {
+    super.setupStickyHeaderView(stickyHeader)
+    stickyHeader.setBackgroundColor(Color.WHITE)
+    stickyHeader.pressEffectBgColor()
+  }
+
+  override fun teardownStickyHeaderView(stickyHeader: View) {
+    super.teardownStickyHeaderView(stickyHeader)
+    stickyHeader.setBackgroundColor(Color.TRANSPARENT)
+    stickyHeader.pressEffectDisable()
+  }
+}
