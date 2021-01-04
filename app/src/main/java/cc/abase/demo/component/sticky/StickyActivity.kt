5afbed4839626bc1f4944a748f049c1307093045
CASE <442415631@qq.com>
2021-01-04 14:02:05 +0700

    改为新的方式

--- app/src/main/java/cc/abase/demo/component/sticky/StickyActivity.kt
+++ app/src/main/java/cc/abase/demo/component/sticky/StickyActivity.kt
@@ -2,14 +2,20 @@ package cc.abase.demo.component.sticky
 
 import android.content.Context
 import android.content.Intent
-import cc.ab.base.ext.mContext
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import cc.ab.base.ext.mActivity
 import cc.ab.base.ext.toast
+import cc.ab.base.ui.viewmodel.DataState
 import cc.abase.demo.R
+import cc.abase.demo.bean.local.DividerBean
+import cc.abase.demo.bean.local.ProvinceBean
 import cc.abase.demo.component.comm.CommTitleActivity
-import cc.abase.demo.component.sticky.adapter.StickyHeaderAdapter
-import cc.abase.demo.component.sticky.viewmodel.StickyViewModel
-import cc.abase.demo.component.sticky.widget.StickyHeaderLinearLayoutManager
+import cc.abase.demo.component.sticky.viewmodel.StickyViewModel2
 import cc.abase.demo.constants.EventKeys
+import cc.abase.demo.item.*
+import cc.abase.demo.sticky.StickyAnyAdapter
+import cc.abase.demo.sticky.StickyHeaderLinearLayoutManager
 import com.blankj.utilcode.util.StringUtils
 import com.jeremyliao.liveeventbus.LiveEventBus
 import kotlinx.android.synthetic.main.activity_sticky.stickyBar
@@ -21,7 +27,7 @@ import kotlinx.android.synthetic.main.activity_sticky.stickyRecycler
  * @date: 2019/11/26 16:54
  */
 class StickyActivity : CommTitleActivity() {
-
+  //<editor-fold defaultstate="collapsed" desc="外部跳转">
   companion object {
     private const val INTENT_KEY_CHOOSE = "INTENT_KEY_CHOOSE"
     private const val INTENT_KEY_TAG = "INTENT_KEY_TAG"
@@ -32,56 +38,61 @@ class StickyActivity : CommTitleActivity() {
       context.startActivity(intent)
     }
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="变量">
   //数据层
-  private val viewModel: StickyViewModel by lazy {
-    StickyViewModel()
-  }
+  private val viewModel: StickyViewModel2 by lazy { StickyViewModel2() }
 
   //是否需要选择
   private var needChoose = false
+
   //那个页面需要选择的标签
   private var mTag = ""
+
   //适配器
-  private var adapter: StickyHeaderAdapter? = null
-
-  override fun layoutResContentId() = R.layout.activity_sticky
-
-  override fun initContentView() {
-    setTitleText(StringUtils.getString(R.string.title_sticky))
-    val manager = StickyHeaderLinearLayoutManager<StickyHeaderAdapter>(this)
-    stickyRecycler.layoutManager = manager
-    showLoadingView()
-    stickyBar.setOnTouchingLetterChangedListener { tag ->
-      adapter?.let { ad ->
-        val position = ad.getTagPosition(tag)
-        if (position >= 0) manager.scrollToPositionWithOffset(position, 0)
-      }
+  private var multiTypeAdapter: StickyAnyAdapter = object : StickyAnyAdapter() {
+    override fun isStickyHeader(position: Int): Boolean {
+      return items[position] is ProvinceBean
     }
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="XML">
+  override fun layoutResContentId() = R.layout.activity_sticky
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
+  override fun initContentView() {
+    setTitleText(StringUtils.getString(R.string.title_sticky))
+    val manager = StickyHeaderLinearLayoutManager<StickyAnyAdapter>(mActivity, LinearLayoutManager.VERTICAL, false)
+    stickyRecycler.layoutManager = manager
+    stickyBar.setOnTouchingLetterChangedListener { tag ->
+      val position = multiTypeAdapter.items.indexOfFirst { c -> c is ProvinceBean && c.pinYinFirst?.substring(0, 1) == tag }
+      if (position >= 0) manager.scrollToPositionWithOffset(position, 0)
+    }
+    //注册多类型
+    multiTypeAdapter.register(StickyTopItem {
+      it.regionName?.toast()
+    })
+    multiTypeAdapter.register(StickyNormalItem {
+      if (needChoose) {
+        it.fromTag = mTag
+        LiveEventBus.get(EventKeys.CHOOSE_STICKY).post(it)
+        finish()
+      } else it.regionFullName?.toast()
+    })
+    multiTypeAdapter.register(DividerItem())
+    stickyRecycler.adapter = multiTypeAdapter
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化Data">
   override fun initData() {
     needChoose = intent.getBooleanExtra(INTENT_KEY_CHOOSE, false)
     mTag = intent.getStringExtra(INTENT_KEY_TAG) ?: ""
-    viewModel.subscribe { state ->
-      if (state.request.complete) {
-        dismissLoadingView()
-        if (adapter == null) {
-          adapter = StickyHeaderAdapter(state.provinces,
-            onProvinceClick = { mContext.toast(it.regionName) },
-            onCityClick = {
-              if (needChoose) {
-                it.fromTag = mTag
-                LiveEventBus.get(EventKeys.CHOOSE_STICKY).post(it)
-                finish()
-              } else {
-                mContext.toast(it.regionFullName)
-              }
-            })
-          stickyRecycler.adapter = adapter
-        }
-      }
-    }
+    viewModel.countryLiveData.observe(this, observer)
+    viewModel.cityLiveData.observe(this, observer)
     //随机加载数据
     if (System.currentTimeMillis() % 2 == 0L) {
       viewModel.loadCountry()
@@ -89,4 +100,33 @@ class StickyActivity : CommTitleActivity() {
       viewModel.loadProvince()
     }
   }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="监听加载结果">
+  private val observer = Observer<DataState<MutableList<ProvinceBean>>?> {
+    when (it) {
+      is DataState.SuccessRefresh -> {
+        val items = mutableListOf<Any>()
+        it.data?.forEach { p ->
+          items.add(p)
+          val cSize = p.cmsRegionDtoList?.size ?: 0
+          p.cmsRegionDtoList?.forEachIndexed { index, cityBean ->
+            items.add(cityBean)
+            if (index < cSize - 1) items.add(DividerBean(heightPx = 1))
+          }
+        }
+        multiTypeAdapter.items = items
+        multiTypeAdapter.notifyDataSetChanged()
+      }
+      is DataState.Start -> {
+        showLoadingView()
+      }
+      is DataState.Complete -> {
+        dismissLoadingView()
+      }
+      else -> {
+      }
+    }
+  }
+  //</editor-fold>
 }
