bb185a067fd6209075f2bc84af60c028d3275626
CASE <442415631@qq.com>
2021-01-04 13:44:05 +0700

    needBg

--- app/src/main/java/cc/abase/demo/sticky/StickyAnyAdapter.kt
+++ app/src/main/java/cc/abase/demo/sticky/StickyAnyAdapter.kt
@@ -11,17 +11,25 @@ import com.drakeet.multitype.*
  * Date:2020/8/31
  * Time:16:00
  */
-abstract class StickyAnyAdapter(val i: List<Any> = emptyList(), val c: Int = 0, val t: Types = MutableTypes(c)
+abstract class StickyAnyAdapter(
+    private val needBg: Boolean = false,
+    val i: List<Any> = emptyList(),
+    val c: Int = 0,
+    val t: Types = MutableTypes(c)
 ) : MultiTypeAdapter(i, c, t), StickyHeaderCallbacks {
   override fun setupStickyHeaderView(stickyHeader: View) {
     super.setupStickyHeaderView(stickyHeader)
-    stickyHeader.setBackgroundColor(Color.WHITE)
-    stickyHeader.pressEffectBgColor()
+    if (needBg) {
+      stickyHeader.setBackgroundColor(Color.WHITE)
+      stickyHeader.pressEffectBgColor()
+    }
   }
 
   override fun teardownStickyHeaderView(stickyHeader: View) {
     super.teardownStickyHeaderView(stickyHeader)
-    stickyHeader.setBackgroundColor(Color.TRANSPARENT)
-    stickyHeader.pressEffectDisable()
+    if (needBg) {
+      stickyHeader.setBackgroundColor(Color.TRANSPARENT)
+      stickyHeader.pressEffectDisable()
+    }
   }
 }
