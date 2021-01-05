f607b559ff8b3e02819ba0296f5038146b566acf
CASE <442415631@qq.com>
2021-01-05 15:31:16 +0700

    新的适配器完善拖拽

--- app/src/main/java/cc/abase/demo/component/drag/DragActivity.kt
+++ app/src/main/java/cc/abase/demo/component/drag/DragActivity.kt
@@ -3,25 +3,22 @@ package cc.abase.demo.component.drag
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.text.TextUtils
-import android.util.Log
-import android.view.View
+import android.content.pm.ActivityInfo
+import android.graphics.Color
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import cc.ab.base.ext.mActivity
-import cc.ab.base.widget.engine.PicSelEngine
+import androidx.recyclerview.widget.ItemTouchHelper
+import cc.ab.base.ext.*
+import cc.ab.base.widget.engine.ImageEngine2
 import cc.abase.demo.R
 import cc.abase.demo.component.comm.CommTitleActivity
-import cc.abase.demo.component.drag.viewmodel.DragState
-import cc.abase.demo.component.drag.viewmodel.DragViewModel
-import cc.abase.demo.epoxy.item.DragImgItem_
-import cc.abase.demo.epoxy.item.dragImgItem
-import cc.abase.demo.mvrx.MvRxEpoxyController
-import cc.abase.demo.utils.BrowserUtils
-import com.airbnb.epoxy.EpoxyTouchHelper
-import com.airbnb.mvrx.withState
-import com.blankj.utilcode.util.*
+import cc.abase.demo.drag.GridItemTouchHelperCallback
+import cc.abase.demo.item.NineImgItem
+import cc.abase.demo.widget.decoration.GridSpaceItemDecoration
+import com.blankj.utilcode.util.SizeUtils
+import com.blankj.utilcode.util.StringUtils
+import com.drakeet.multitype.MultiTypeAdapter
 import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.config.PictureConfig
 import com.luck.picture.lib.config.PictureMimeType
 import com.luck.picture.lib.entity.LocalMedia
 import kotlinx.android.synthetic.main.activity_drag.dragRecycler
@@ -33,174 +30,148 @@ import kotlinx.android.synthetic.main.activity_drag.dragRoot
  * @date: 2019/11/30 20:48
  */
 class DragActivity : CommTitleActivity() {
+  //<editor-fold defaultstate="collapsed" desc="外部跳转">
   companion object {
-    private const val INTENT_SEL_IMGS = 0x0107
     fun startActivity(context: Context) {
       val intent = Intent(context, DragActivity::class.java)
       context.startActivity(intent)
     }
   }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="变量">
 
   //最大图片数量
   private val MAX_IMG_SIZE = 9
-  //360 - (96+12+96+12+96) = 48 /2 = 24
-  //列表左边间距
-  private val paddingStart: Int = SizeUtils.dp2px(24f)
-  //列表右边间距
-  private var paddingRight: Int = SizeUtils.dp2px(24f - 12f)
-  //数据层
-  private val viewModel: DragViewModel by lazy {
-    DragViewModel()
-  }
 
+  //Item间距
+  private val spaceItem = SizeUtils.dp2px(6f)
+
+  //适配器
+  val multiTypeAdapter = MultiTypeAdapter()
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="XML">
   override fun layoutResContentId() = R.layout.activity_drag
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
   override fun initContentView() {
     setTitleText(StringUtils.getString(R.string.title_drag))
-    //使用GridLayoutManager的方法
-    val spanCount = 3
-    val layoutManager = GridLayoutManager(this, spanCount)
-    epoxyController.spanCount = spanCount
-    layoutManager.spanSizeLookup = epoxyController.spanSizeLookup
-    dragRecycler.layoutManager = layoutManager
-    dragRecycler.adapter = epoxyController.adapter
-    dragRecycler.setPadding(
-        paddingStart, dragRecycler.paddingTop, paddingRight, dragRecycler.paddingBottom
-    )
-    initDrag(dragRecycler, epoxyController)
+    dragRecycler.layoutManager = GridLayoutManager(mContext, 3)
+    dragRecycler.setPadding(spaceItem, dragRecycler.paddingTop, spaceItem, dragRecycler.paddingBottom)
+    if (dragRecycler.itemDecorationCount > 0) dragRecycler.removeItemDecorationAt(0)
+    dragRecycler.addItemDecoration(GridSpaceItemDecoration(spaceItem).setDragGridEdge(false))
+    //适配器注册
+    multiTypeAdapter.register(NineImgItem(
+        onItemClick = { url, p, v -> if (url.isBlank()) go2ImgSel() else showPic(url) }, //为空代表点击选择添加
+        onDelClick = { url, p, v -> removeSelect(url) }
+    ))
+    //默认+
+    val items = mutableListOf<Any>()
+    items.add("")
+    multiTypeAdapter.items = items
+    dragRecycler.adapter = multiTypeAdapter
+    val mParent = dragRoot
+    //拖拽
+    ItemTouchHelper(GridItemTouchHelperCallback(
+        mAdapter = multiTypeAdapter,
+        dragBgColor = Color.parseColor("#f7f7f7"),
+        dragStart = { mParent.bringChildToFront(dragRecycler) },
+        dragEnd = {
+          dragRecycler.removeParent()
+          mParent.addView(dragRecycler, 0)
+        },
+        canMove = { p ->
+          val hasAdd = multiTypeAdapter.items.toMutableList().any { a -> a is String && a.isBlank() }
+          if (!hasAdd) true else p < multiTypeAdapter.items.size - 1
+        })).attachToRecyclerView(dragRecycler)
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化Data">
   override fun initData() {
-    viewModel.subscribe { epoxyController.data = it }
   }
+  //</editor-fold>
 
-  private val epoxyController = MvRxEpoxyController<DragState> {
-    withState(viewModel) { state ->
-      //添加选中的图片
-      state.medias.forEachIndexed { _, media ->
-        dragImgItem {
-          id(media.path)
-          localMedia(media)
-          roundRadiusDp(0f)
-          onClickDel { viewModel.removeSelect(media) }
-          onItemClick { it, iv -> showPic(it) }
-          //需要写这个cell占用的span，不然不起作用
-          spanCount = 1
-        }
-      }
-      //图片选择
-      if (state.medias.size < MAX_IMG_SIZE) {
-        dragImgItem {
-          id("no_sel_pic")
-          roundRadiusDp(0f)
-          onItemClick { it, iv ->
-            PictureSelector.create(mActivity)
-                .openGallery(PictureMimeType.ofImage())
-                .maxSelectNum(9)
-                .isCamera(false)
-                .previewImage(true)
-                .loadImageEngine(PicSelEngine())
-                .selectionMedia(viewModel.getSelMediaList())
-                .forResult(INTENT_SEL_IMGS)
-          }
-          spanCount = 1
-        }
-      }
+  //<editor-fold defaultstate="collapsed" desc="去选图">
+  private fun go2ImgSel() {
+    val datas = multiTypeAdapter.items.toMutableList().filter { f -> f is String && f.isNotBlank() }
+    val list = mutableListOf<LocalMedia>()
+    datas.forEach { d -> list.add(LocalMedia().also { lm -> lm.path = d as String }) }
+    //https://github.com/LuckSiege/PictureSelector/wiki/PictureSelector-Api%E8%AF%B4%E6%98%8E
+    PictureSelector.create(this)
+        .openGallery(PictureMimeType.ofImage())
+        .imageEngine(ImageEngine2())
+        .isGif(false)
+        .isPageStrategy(true, PictureConfig.MAX_PAGE_SIZE, true) //过滤掉已损坏的图片
+        .selectionData(list) //过滤掉添加操作的图片
+        .maxSelectNum(MAX_IMG_SIZE)
+        .queryMaxFileSize(5f)
+        .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        .forResult(PictureConfig.CHOOSE_REQUEST)
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="数据处理">
+  //设置选中的图片
+  private fun setSelectMedias(list: MutableList<LocalMedia>?) {
+    list?.let { l ->
+      val items = mutableListOf<Any>()
+      items.addAll(l.flatMap { f -> listOf(f.path) })
+      if (items.size < MAX_IMG_SIZE) items.add("")
+      multiTypeAdapter.items = items
+      multiTypeAdapter.notifyDataSetChanged()
     }
   }
 
-  //初始化拖拽
-  private fun initDrag(
-    recyclerView: RecyclerView,
-    epoxyController: MvRxEpoxyController<DragState>
-  ) {
-    //长按拖动排序
-    EpoxyTouchHelper.initDragging(epoxyController)
-        .withRecyclerView(recyclerView)
-        .forGrid()//可以控制是横向还是纵向或者是grid的拖拽
-        .withTarget(DragImgItem_::class.java)
-        .andCallbacks(object : EpoxyTouchHelper.DragCallbacks<DragImgItem_>() {
-          override fun isDragEnabledForModel(model: DragImgItem_?): Boolean {
-            return model?.localMedia != null
-          }
-
-          override fun onModelMoved(
-            fromPosition: Int,
-            toPosition: Int,
-            modelBeingMoved: DragImgItem_?,
-            itemView: View?
-          ) {
-            modelBeingMoved?.localMedia()
-                ?.let {
-                  viewModel.moveBean(it, fromPosition, toPosition)
-                }
-          }
-
-          override fun onDragStarted(
-            model: DragImgItem_?,
-            itemView: View?,
-            adapterPosition: Int
-          ) {
-            itemView?.apply {
-              animate()
-                  .scaleX(1.05f)
-                  .scaleY(1.05f)
-            }
-            dragRecycler.bringToFront()
-          }
-
-          override fun onDragReleased(
-            model: DragImgItem_?,
-            itemView: View?
-          ) {
-            itemView?.apply {
-              animate()
-                  .scaleX(1.0f)
-                  .scaleY(1.0f)
-            }
-          }
-
-          override fun clearView(
-            model: DragImgItem_?,
-            itemView: View?
-          ) {
-            onDragReleased(model, itemView)
-            dragRoot.removeView(dragRecycler)
-            dragRoot.addView(dragRecycler, 0)
-          }
-        })
+  //删除选中
+  private fun removeSelect(url: String) {
+    val list = multiTypeAdapter.items.toMutableList()
+    val index = list.indexOfFirst { l -> l == url }
+    if (index >= 0) {
+      list.removeAt(index)
+      var hasAdd = false
+      if (list.isEmpty()) {
+        list.add("")
+        hasAdd = true
+      } else if ((list[list.size - 1] as? String)?.isBlank() == false) {
+        list.add("")
+        hasAdd = true
+      }
+      multiTypeAdapter.items = list
+      multiTypeAdapter.notifyItemRemoved(index)
+      if (hasAdd) multiTypeAdapter.notifyItemInserted(list.size - 1)
+    }
   }
+  //</editor-fold>
 
-  override fun onActivityResult(
-    requestCode: Int,
-    resultCode: Int,
-    data: Intent?
-  ) {
+  //<editor-fold defaultstate="collapsed" desc="选择图片的回调">
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
     super.onActivityResult(requestCode, resultCode, data)
     data?.let {
-      if (requestCode == INTENT_SEL_IMGS && resultCode == Activity.RESULT_OK) {
+      if (requestCode == PictureConfig.CHOOSE_REQUEST && resultCode == Activity.RESULT_OK) {
         // 图片、视频、音频选择结果回调
-        PictureSelector.obtainMultipleResult(data)
-            ?.let { medias ->
-              if (medias.isNotEmpty()) {
-                viewModel.setSelectMedias(medias)
-              }
-            }
-      } else {
-        LogUtils.e("CASE:onActivityResult:other")
-      }
+        PictureSelector.obtainMultipleResult(data)?.let { medias ->
+          if (medias.isNotEmpty()) setSelectMedias(medias)
+        }
+      } else "onActivityResult:other".logE()
     }
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="预览图片">
   //预览图片
-  private fun showPic(media: LocalMedia) {
-    var position = 0
-    val temp = viewModel.getSelMediaList()
-    val current = viewModel.getSelMediaList()
-        .filter { m -> TextUtils.equals(m.path, media.path) }
-    val result = mutableListOf<String>()
-    if (current.isNotEmpty()) position = temp.indexOf(current[0])
-    temp.forEach { t -> result.add(t.path) }
-    BrowserUtils.instance.show(result as ArrayList<String>, position)
+  private fun showPic(url: String) {
+    val datas = multiTypeAdapter.items.toMutableList().filter { f -> f is String && f.isNotBlank() }
+    val index = datas.indexOfFirst { f -> f == url }
+    val list = mutableListOf<LocalMedia>()
+    datas.forEach { d -> list.add(LocalMedia().also { lm -> lm.path = d as String }) }
+    PictureSelector.create(this)
+        .themeStyle(R.style.picture_default_style)
+        .isNotPreviewDownload(true)
+        .imageEngine(ImageEngine2())
+        .openExternalPreview2(0.coerceAtLeast(index), list)
   }
+  //</editor-fold>
 }
