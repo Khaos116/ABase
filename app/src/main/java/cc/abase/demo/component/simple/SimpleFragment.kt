a665695b5ea383b2554ac9e8927b1eca245659c6
CASE <442415631@qq.com>
2021-01-05 18:51:12 +0700

    修改适配器和下拉刷新

--- app/src/main/java/cc/abase/demo/component/simple/SimpleFragment.kt
+++ app/src/main/java/cc/abase/demo/component/simple/SimpleFragment.kt
@@ -3,15 +3,18 @@ package cc.abase.demo.component.simple
 import android.graphics.Color
 import android.view.Gravity
 import android.view.View
+import androidx.lifecycle.rxLifeScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import cc.abase.demo.R
+import cc.abase.demo.bean.local.DividerBean
+import cc.abase.demo.bean.local.SimpleTxtBean
 import cc.abase.demo.component.comm.CommFragment
-import cc.abase.demo.epoxy.item.simpleTextItem
-import cc.abase.demo.mvrx.MvRxEpoxyController
-import cc.abase.demo.widget.decoration.SpacesItemDecoration
-import com.billy.android.swipe.SmartSwipeRefresh
-import com.billy.android.swipe.SmartSwipeRefresh.SmartSwipeRefreshDataLoader
-import com.billy.android.swipe.consumer.SlidingConsumer
+import cc.abase.demo.item.DividerItem
+import cc.abase.demo.item.SimpleTxtItem
+import com.drakeet.multitype.MultiTypeAdapter
 import kotlinx.android.synthetic.main.fragment_simple.simpleRecycler
+import kotlinx.android.synthetic.main.fragment_simple.simpleRefresh
+import kotlinx.coroutines.*
 
 /**
  * Description:
@@ -19,81 +22,86 @@ import kotlinx.android.synthetic.main.fragment_simple.simpleRecycler
  * @date: 2019/11/19 14:44
  */
 class SimpleFragment : CommFragment() {
-
+  //<editor-fold defaultstate="collapsed" desc="外部获取实例">
   companion object {
-    //0-无刷新 1-Swipe
+    //0-无刷新 1-Smart
     fun newInstance(type: Int = 0): SimpleFragment {
       val fragment = SimpleFragment()
       fragment.mType = type
       return fragment
     }
   }
+  //</editor-fold>
 
-  //下拉刷新
-  private var mSmartSwipeRefresh: SmartSwipeRefresh? = null
+  //<editor-fold defaultstate="collapsed" desc="变量">
+
+  //类型
   private var mType = 0
 
+  //适配器
+  private val multiTypeAdapter = MultiTypeAdapter()
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="XML">
   override val contentLayout: Int = R.layout.fragment_simple
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
   override fun initView(root: View?) {
-    simpleRecycler.addItemDecoration(SpacesItemDecoration())
-    simpleRecycler.setController(epoxyController)
-    val datas = mutableListOf<String>()
-    for (i in 0 until 20) datas.add(
-      "这是${
-      when (mType) {
-        1 -> "Swipe"
-        else -> "Nomal"
-      }
-      }测试数据${i + 1}"
-    )
-    epoxyController.data = datas
-    if (mType == 1) mSmartSwipeRefresh = SmartSwipeRefresh.translateMode(simpleRecycler, false)
-    mSmartSwipeRefresh?.swipeConsumer?.let {
-      if (it is SlidingConsumer) {
-        it.setOverSwipeFactor(2f)
-        it.relativeMoveFactor = 0.5f
-      }
-    }
-    mSmartSwipeRefresh?.disableLoadMore()
-    //下拉刷新
-    mSmartSwipeRefresh?.dataLoader = object : SmartSwipeRefreshDataLoader {
-      override fun onLoadMore(ssr: SmartSwipeRefresh?) {
-      }
-
-      override fun onRefresh(ssr: SmartSwipeRefresh?) {
-        val list = mutableListOf<String>()
-        val size = epoxyController.data?.size ?: 0
-        for (i in size until size + 10) list.add(
-          "这是${
-          when (mType) {
-            1 -> "Swipe"
-            else -> "Nomal"
+    simpleRecycler.layoutManager = LinearLayoutManager(mContext)
+    multiTypeAdapter.register(SimpleTxtItem())
+    multiTypeAdapter.register(DividerItem())
+    simpleRecycler.adapter = multiTypeAdapter
+    simpleRefresh.setEnableLoadMore(false)
+    simpleRefresh.setEnableRefresh(mType != 0)
+    //simpleRefresh.setRefreshHeader(if (System.currentTimeMillis() % 2 == 0L) BlackLoadingHeader(mContext) else MidaMusicHeader(mContext))
+    simpleRefresh.setOnRefreshListener {
+      rxLifeScope.launch {
+        withContext(Dispatchers.IO) { delay(2000) }.let {
+          val list = mutableListOf<String>()
+          val size = multiTypeAdapter.items.size / 2
+          for (i in size + 10 downTo size + 1) list.add(
+              "这是${
+                when (mType) {
+                  1 -> "Smart"
+                  else -> "Normal"
+                }
+              }测试数据${i}"
+          )
+          val items = mutableListOf<Any>()
+          list.forEach {
+            items.add(SimpleTxtBean(txt = it).also { stb -> stb.gravity = Gravity.CENTER_VERTICAL })
+            items.add(DividerBean(bgColor = Color.parseColor("#f3f3f3")))
           }
-          }测试数据${i + 1}"
-        )
-        list.addAll(epoxyController.data ?: mutableListOf())
-        epoxyController.data = list
-        mSmartSwipeRefresh?.finished(true)
-      }
-    }
-  }
-
-  override fun initData() {
-  }
-
-  //epoxy
-  private val epoxyController =
-    MvRxEpoxyController<List<String>> { list ->
-      list.forEachIndexed { index, str ->
-        //内容
-        simpleTextItem {
-          id("str_$index")
-          msg(str)
-          textColor(Color.BLACK)
-          gravity(Gravity.CENTER_VERTICAL)
-          onItemClick { }
+          items.addAll(multiTypeAdapter.items)
+          multiTypeAdapter.items = items
+          multiTypeAdapter.notifyItemRangeInserted(0, 10)
+          simpleRecycler.smoothScrollToPosition(0)
+          simpleRefresh.finishRefresh()
         }
       }
     }
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化Data">
+  override fun initData() {
+    val datas = mutableListOf<String>()
+    for (i in 20 downTo 1) datas.add(
+        "这是${
+          when (mType) {
+            1 -> "Smart"
+            else -> "Normal"
+          }
+        }测试数据${i}"
+    )
+    val items = mutableListOf<Any>()
+    datas.forEach {
+      items.add(SimpleTxtBean(txt = it).also { stb -> stb.gravity = Gravity.CENTER_VERTICAL })
+      items.add(DividerBean(bgColor = Color.parseColor("#f3f3f3")))
+    }
+    multiTypeAdapter.items = items
+    multiTypeAdapter.notifyDataSetChanged()
+  }
+  //</editor-fold>
 }
