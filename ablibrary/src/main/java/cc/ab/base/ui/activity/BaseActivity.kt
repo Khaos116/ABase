136967e5532c575f489a62c90345c396a4378582
CASE <442415631@qq.com>
2021-01-07 13:42:02 +0700

    删除MvRxView

--- ablibrary/src/main/java/cc/ab/base/ui/activity/BaseActivity.kt
+++ ablibrary/src/main/java/cc/ab/base/ui/activity/BaseActivity.kt
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import cc.ab.base.ext.*
 import cc.ab.base.utils.CleanLeakUtils
-import com.airbnb.mvrx.MvRxView
-import com.airbnb.mvrx.MvRxViewId
 import com.gyf.immersionbar.ktx.immersionBar
 import com.trello.lifecycle2.android.lifecycle.AndroidLifecycle
 import com.trello.rxlifecycle3.LifecycleProvider
@@ -20,10 +18,7 @@ import kotlinx.android.synthetic.main.base_activity.baseStatusView
  * @author: CASE
  * @date: 2019/9/20 16:37
  */
-abstract class BaseActivity : AppCompatActivity(), MvRxView {
-  //MvRxView
-  private val mvrxViewIdProperty = MvRxViewId()
-  final override val mvrxViewId: String by mvrxViewIdProperty
+abstract class BaseActivity : AppCompatActivity() {
   //状态栏填充的view,当设置fillStatus返回为true时才生效
   protected var mStatusView: View? = null
   //防止内存泄漏
@@ -31,7 +26,6 @@ abstract class BaseActivity : AppCompatActivity(), MvRxView {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     lifecycleProvider = AndroidLifecycle.createLifecycleProvider(this)
-    mvrxViewIdProperty.restoreFrom(savedInstanceState)
     this.onCreateBefore()
     this.initStatus()
     super.onCreate(savedInstanceState)
@@ -49,11 +43,6 @@ abstract class BaseActivity : AppCompatActivity(), MvRxView {
     initData()
   }
 
-  override fun onSaveInstanceState(outState: Bundle) {
-    super.onSaveInstanceState(outState)
-    mvrxViewIdProperty.saveTo(outState)
-  }
-
   protected open fun onCreateBefore() {}
 
   //状态栏处理(默认白底，黑字)
@@ -67,9 +56,6 @@ abstract class BaseActivity : AppCompatActivity(), MvRxView {
   //是否需要默认填充状态栏,默认填充为白色view
   protected open fun fillStatus(): Boolean {
     return true
-  }
-
-  override fun invalidate() {
   }
 
   override fun onDestroy() {
