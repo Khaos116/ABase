139570a125b8a93127b109d54732e459651d5ec1
CASE <442415631@qq.com>
2021-01-07 13:47:46 +0700

    简化重写

--- ablibrary/src/main/java/cc/ab/base/widget/discretescrollview/holder/DiscreteHolder.java
+++ ablibrary/src/main/java/cc/ab/base/widget/discretescrollview/holder/DiscreteHolder.java
@@ -2,15 +2,11 @@ package cc.ab.base.widget.discretescrollview.holder;
 
 import android.view.View;
 import androidx.recyclerview.widget.RecyclerView;
-import org.jetbrains.annotations.NotNull;
 
 public abstract class DiscreteHolder<T> extends RecyclerView.ViewHolder {
   public DiscreteHolder(View itemView) {
     super(itemView);
-    initView(itemView);
   }
-
-  protected abstract void initView(@NotNull View itemView);
 
   public abstract void updateUI(T data, int position, int count);
 }
