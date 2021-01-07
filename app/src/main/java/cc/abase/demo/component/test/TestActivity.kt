909b53290aa6f4285e41b0a8c77b3e2e0e9edccc
CASE <442415631@qq.com>
2021-01-07 13:34:40 +0700

    修改测试效果

--- app/src/main/java/cc/abase/demo/component/test/TestActivity.kt
+++ app/src/main/java/cc/abase/demo/component/test/TestActivity.kt
@@ -1,5 +1,6 @@
 package cc.abase.demo.component.test
 
+import cc.ab.base.ui.viewmodel.DataState
 import cc.abase.demo.R
 import cc.abase.demo.component.comm.CommTitleActivity
 import cc.abase.demo.component.test.viewmodel.TestViewModel
@@ -13,25 +14,32 @@ import kotlinx.android.synthetic.main.activity_test.testTv2
  * @date: 2020/5/7 9:41
  */
 class TestActivity : CommTitleActivity() {
-
+  //<editor-fold defaultstate="collapsed" desc="XML">
   override fun layoutResContentId() = R.layout.activity_test
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="变量">
   private val viewModel: TestViewModel by lazy { TestViewModel() }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
   override fun initContentView() {
     setTitleText(StringUtils.getString(R.string.title_test))
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化Data">
   override fun initData() {
-    viewModel.subscribe(this) {
-      testTv1.append("时间1=${it.time1}\n")
-      testTv2.append("时间2=${it.time2}\n")
-    }
+    viewModel.timeLiveData1.observe(this) { if (it is DataState.SuccessRefresh) testTv1.append("时间1=${it.data}\n") }
+    viewModel.timeLiveData2.observe(this) { if (it is DataState.SuccessRefresh) testTv2.append("时间2=${it.data}\n") }
     viewModel.startGetTime()
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="注释">
   override fun finish() {
     super.finish()
     viewModel.release()
   }
+  //</editor-fold>
 }
--- app/src/main/java/cc/abase/demo/component/test/viewmodel/TestViewModel.kt
+++ app/src/main/java/cc/abase/demo/component/test/viewmodel/TestViewModel.kt
@@ -1,10 +1,14 @@
 package cc.abase.demo.component.test.viewmodel
 
-import cc.ab.base.mvrx.MvRxViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.rxLifeScope
+import cc.ab.base.ui.viewmodel.DataState
 import cc.ab.base.utils.RxUtils
-import com.airbnb.mvrx.*
+import cc.abase.demo.component.comm.CommViewModel
 import io.reactivex.Flowable
 import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import java.util.concurrent.TimeUnit
 
 /**
@@ -12,14 +16,10 @@ import java.util.concurrent.TimeUnit
  * @author: CASE
  * @date: 2020/5/7 9:46
  */
-data class TestState(
-    val time1: Long = 0,
-    val time2: Long = 0,
-    val request: Async<Any> = Uninitialized) : MvRxState
-
-class TestViewModel(state: TestState = TestState()) : MvRxViewModel<TestState>(state) {
-
+class TestViewModel : CommViewModel() {
   //<editor-fold defaultstate="collapsed" desc="外部调用">
+  val timeLiveData1 = MutableLiveData<DataState<String>?>()
+  val timeLiveData2 = MutableLiveData<DataState<String>?>()
   fun startGetTime() {
     startTime1()
     startTime2()
@@ -27,37 +27,40 @@ class TestViewModel(state: TestState = TestState()) : MvRxViewModel<TestState>(s
   //</editor-fold>
 
   //<editor-fold defaultstate="collapsed" desc="内部方法">
-
   //运行次数
   private var count = 20L
 
-  //内部调用withState
+  //Rxjava倒计时
   private fun startTime1() {
     disposable1?.dispose()
-    disposable1 = Flowable.intervalRange(0, count, 0, 200, TimeUnit.MILLISECONDS)
-      .compose(RxUtils.instance.rx2SchedulerHelperF())
-      .doOnNext { withState { state -> setState { copy(time1 = state.time1 + 1) } } }
-      .doOnComplete { }
-      .subscribe()
+    disposable1 = Flowable.intervalRange(1, count, 0, 500, TimeUnit.MILLISECONDS)
+        .compose(RxUtils.instance.rx2SchedulerHelperF())
+        .doOnNext { timeLiveData1.value = DataState.SuccessRefresh(newData = it.toString()) }
+        .doOnComplete { }
+        .subscribe()
   }
 
-  //外部调用withState(同时调用会导致time1的数据恢复到第一次获取到的状态)
-  private fun startTime2() = withState { state ->
-    disposable2?.dispose()
-    disposable2 = Flowable.intervalRange(0, count, 0, 300, TimeUnit.MILLISECONDS)
-      .compose(RxUtils.instance.rx2SchedulerHelperF())
-      .doOnNext { setState { copy(time2 = state.time2 + 100) } }
-      .doOnComplete { }
-      .subscribe()
+  //协程倒计时
+  private fun startTime2() {
+    disposable2?.cancel()
+    disposable2 = rxLifeScope.launch {
+      var count = 1
+      timeLiveData2.value = DataState.SuccessRefresh(newData = count.toString())
+      while (count < 20) {
+        delay(500)
+        count++
+        timeLiveData2.value = DataState.SuccessRefresh(newData = count.toString())
+      }
+    }
   }
   //</editor-fold>
 
   //<editor-fold defaultstate="collapsed" desc="释放">
   private var disposable1: Disposable? = null
-  private var disposable2: Disposable? = null
+  private var disposable2: Job? = null
   fun release() {
     disposable1?.dispose()
-    disposable2?.dispose()
+    disposable2?.cancel()
   }
   //</editor-fold>
 }
