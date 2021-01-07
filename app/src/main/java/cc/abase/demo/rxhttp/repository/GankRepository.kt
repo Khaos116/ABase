0f06ff13c61008100a3be3d63b687ae5e34b6e9a
CASE <442415631@qq.com>
2021-01-07 14:17:05 +0700

    修改单利，改为协程方式请求

--- app/src/main/java/cc/abase/demo/rxhttp/repository/GankRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/GankRepository.kt
@@ -1,36 +1,26 @@
 package cc.abase.demo.rxhttp.repository
 
 import androidx.annotation.IntRange
-import cc.ab.base.utils.RxUtils
 import cc.abase.demo.bean.gank.GankAndroidBean
 import cc.abase.demo.constants.GankUrls
 import cc.abase.demo.constants.TimeConstants
-import io.reactivex.Observable
 import rxhttp.wrapper.cahce.CacheMode
 import rxhttp.wrapper.param.RxHttp
+import rxhttp.wrapper.param.toResponseGank
 
 /**
  * Description:
  * @author: CASE
  * @date: 2020/3/5 18:09
  */
-class GankRepository private constructor() {
-  private object SingletonHolder {
-    val holder = GankRepository()
-  }
-
-  companion object {
-    val instance = SingletonHolder.holder
-  }
-
+object GankRepository  {
   //安卓列表
-  fun androidList(@IntRange(from = 1) page: Int, size: Int, readCache: Boolean = true): Observable<MutableList<GankAndroidBean>> {
+  suspend fun androidList(@IntRange(from = 1) page: Int, size: Int, readCache: Boolean = true): MutableList<GankAndroidBean> {
     return RxHttp.get(String.format(GankUrls.ANDROID, page, size))
-      .setDomainToGankIfAbsent()
-      .setCacheValidTime(TimeConstants.DYN_CACHE)//设置缓存时长
-      .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK
-      else CacheMode.ONLY_NETWORK)//先读取缓存，失败再请求数据
-      .asResponseGankList(GankAndroidBean::class.java)
-      .compose(RxUtils.instance.rx2SchedulerHelperODelay())
+        .setDomainToGankIfAbsent()
+        .setCacheValidTime(TimeConstants.DYN_CACHE) //设置缓存时长
+        .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
+        .toResponseGank<MutableList<GankAndroidBean>>()
+        .await()
   }
 }
--- app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
@@ -2,55 +2,38 @@ package cc.abase.demo.rxhttp.repository
 
 import androidx.annotation.IntRange
 import cc.ab.base.net.http.response.BasePageList
-import cc.ab.base.utils.RxUtils
 import cc.abase.demo.bean.wan.ArticleBean
 import cc.abase.demo.bean.wan.BannerBean
 import cc.abase.demo.constants.TimeConstants
 import cc.abase.demo.constants.WanUrls
-import io.reactivex.Observable
 import rxhttp.wrapper.cahce.CacheMode
 import rxhttp.wrapper.param.RxHttp
+import rxhttp.wrapper.param.toResponseWan
 
 /**
  * Description:
  * @author: CASE
  * @date: 2020/3/5 18:10
  */
-class WanRepository private constructor() {
-  private object SingletonHolder {
-    val holder = WanRepository()
-  }
-
-  companion object {
-    val instance = SingletonHolder.holder
-  }
+object WanRepository {
 
   //获取Banner
-  fun banner(readCache: Boolean = true): Observable<MutableList<BannerBean>> {
+  suspend fun banner(readCache: Boolean = true): MutableList<BannerBean> {
     return RxHttp.get(WanUrls.Home.BANNER)
         .setDomainToWanIfAbsent()
-        .setCacheValidTime(TimeConstants.HOME_CACHE)//设置缓存时长
-        .setCacheMode(
-            if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK
-            else CacheMode.ONLY_NETWORK
-        )//先读取缓存，失败再请求数据
-        .asResponseWanList(BannerBean::class.java)
-        .compose(RxUtils.instance.rx2SchedulerHelperODelay())
+        .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
+        .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
+        .toResponseWan<MutableList<BannerBean>>()
+        .await()
   }
 
   //获取文章列表
-  fun article(
-    @IntRange(from = 0) page: Int,
-    readCache: Boolean = true
-  ): Observable<BasePageList<ArticleBean>> {
+  suspend fun article(@IntRange(from = 0) page: Int, readCache: Boolean = true): BasePageList<ArticleBean> {
     return RxHttp.get(String.format(WanUrls.Home.ARTICLE, page))
         .setDomainToWanIfAbsent()
-        .setCacheValidTime(TimeConstants.HOME_CACHE)//设置缓存时长
-        .setCacheMode(
-            if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK
-            else CacheMode.ONLY_NETWORK
-        )//先读取缓存，失败再请求数据
-        .asResponseWanBasePageList(ArticleBean::class.java)
-        .compose(RxUtils.instance.rx2SchedulerHelperODelay())
+        .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
+        .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
+        .toResponseWan<BasePageList<ArticleBean>>()
+        .await()
   }
 }
