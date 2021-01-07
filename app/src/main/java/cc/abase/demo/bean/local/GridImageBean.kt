f89f28e5f642cdf5921ea6fe5bf09f4e3145fee1
CASE <442415631@qq.com>
2021-01-07 14:14:55 +0700

    九宫格图片bean

---
+++ app/src/main/java/cc/abase/demo/bean/local/GridImageBean.kt
@@ -0,0 +1,11 @@
+package cc.abase.demo.bean.local
+
+/**
+ * Author:CASE
+ * Date:2020-11-20
+ * Time:18:10
+ */
+data class GridImageBean(
+    val url: String = "",
+    val list: MutableList<String> = mutableListOf()
+)
