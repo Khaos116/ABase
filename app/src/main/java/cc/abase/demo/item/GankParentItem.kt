cdfb868e1ad9f31e3873dc1ab956eb61fedb5d98
CASE <442415631@qq.com>
2021-01-08 17:38:39 +0700

    整合文字和九宫格

---
+++ app/src/main/java/cc/abase/demo/item/GankParentItem.kt
@@ -0,0 +1,49 @@
+package cc.abase.demo.item
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import cc.ab.base.ext.click
+import cc.ab.base.ext.click2Parent
+import cc.ab.base.ui.item.BaseItemView
+import cc.abase.demo.R
+import cc.abase.demo.bean.gank.GankAndroidBean
+import cc.abase.demo.bean.local.GridImageBean
+import com.drakeet.multitype.MultiTypeAdapter
+import kotlinx.android.synthetic.main.item_gank_parent.view.itemGankParentRecycler
+
+/**
+ * Author:CASE
+ * Date:2021年1月8日
+ * Time:16:23:13
+ */
+class GankParentItem(
+    private val onItemClick: ((item: GankAndroidBean) -> Unit)? = null,
+    private val onImgClick: ((url: String, position: Int, iv: ImageView, list: MutableList<String>) -> Unit)? = null,
+) : BaseItemView<GankAndroidBean>() {
+  //<editor-fold defaultstate="collapsed" desc="XML">
+  override fun layoutResId() = R.layout.item_gank_parent
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="数据填充">
+  @SuppressLint("ClickableViewAccessibility")
+  override fun fillData(holder: ViewHolder, itemView: View, item: GankAndroidBean) {
+    itemView.click { onItemClick?.invoke(item) }
+    val recyclerView = itemView.itemGankParentRecycler
+    recyclerView.click2Parent()
+    recyclerView.adapter = null
+    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+    val multiTypeAdapter = MultiTypeAdapter()
+    multiTypeAdapter.register(GankItem())
+    multiTypeAdapter.register(NineGridItem { url, p, iv, list -> onImgClick?.invoke(url, p, iv, list) })
+    recyclerView.adapter = multiTypeAdapter
+    val items = mutableListOf<Any>()
+    items.add(item)
+    if (item.imagesNoNull().isNotEmpty()) items.add(GridImageBean(url = item.url ?: "", list = item.imagesNoNull()))
+    multiTypeAdapter.items = items
+    multiTypeAdapter.notifyDataSetChanged()
+  }
+  //</editor-fold>
+}
