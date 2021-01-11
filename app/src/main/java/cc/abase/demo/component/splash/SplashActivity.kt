0b7a2f007d90ca8db8d84ae09f9d7a97f971fb20
CASE <442415631@qq.com>
2021-01-11 16:27:38 +0700

    使用PAG动画

--- app/src/main/java/cc/abase/demo/component/splash/SplashActivity.kt
+++ app/src/main/java/cc/abase/demo/component/splash/SplashActivity.kt
@@ -1,29 +1,23 @@
 package cc.abase.demo.component.splash
 
 import android.content.Intent
-import cc.ab.base.ext.*
+import android.graphics.Color
+import android.view.ViewGroup
+import cc.ab.base.ext.mContext
+import cc.ab.base.ext.toast
 import cc.ab.base.utils.PermissionUtils
-import cc.ab.base.utils.RxUtils
 import cc.abase.demo.R
 import cc.abase.demo.component.comm.CommActivity
 import cc.abase.demo.component.login.LoginActivity
 import cc.abase.demo.component.main.MainActivity
-import cc.abase.demo.constants.ImageUrls
 import cc.abase.demo.rxhttp.repository.UserRepository
 import cc.abase.demo.utils.MMkvUtils
-import coil.imageLoader
-import coil.load
-import coil.request.ImageRequest
 import com.blankj.utilcode.util.TimeUtils
-import com.blankj.utilcode.util.Utils
 import com.gyf.immersionbar.ktx.immersionBar
 import com.hjq.permissions.*
-import io.reactivex.Flowable
-import io.reactivex.disposables.Disposable
-import kotlinx.android.synthetic.main.activity_splash.splashCover
-import kotlinx.android.synthetic.main.activity_splash.splashTime
-import java.util.concurrent.TimeUnit
-import kotlin.math.max
+import kotlinx.android.synthetic.main.activity_splash.splashBgView
+import kotlinx.android.synthetic.main.activity_splash.splashRoot
+import org.libpag.*
 
 /**
  * Description:
@@ -31,25 +25,24 @@ import kotlin.math.max
  * @date: 2019/10/8 10:03
  */
 class SplashActivity : CommActivity() {
+  //<editor-fold defaultstate="collapsed" desc="变量">
   //一个小时变一张图
-  private val randomImg = TimeUtils.millis2String(System.currentTimeMillis())
-      .split(" ")[1].split(":")[0].toInt()
-
-  //倒计时3秒
-  private val count = 3L
-
-  //倒计时
-  private var disposable: Disposable? = null
+  private val randomImg = TimeUtils.millis2String(System.currentTimeMillis()).split(" ")[1].split(":")[0].toInt()
 
   //是否有SD卡读写权限
-  private var hasSDPermission: Boolean? = null
+  private var hasSDPermission = false
 
   //倒计时是否结束
-  private var countDownFinish: Boolean? = null
+  private var countDownFinish = false
 
   //是否需要关闭页面
   private var hasFinish = false
 
+  //PAG动画
+  private var mPAGView: PAGView? = null
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="状态栏">
   //不设置状态栏填充，即显示全屏
   override fun fillStatus() = false
 
@@ -60,64 +53,102 @@ class SplashActivity : CommActivity() {
       fullScreen(true)
     }
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="XML">
   override fun layoutResId() = R.layout.activity_splash
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="初始化View">
+  private var isFirstSize = true
   override fun initView() {
     hasFinish = checkReOpenHome()
     if (hasFinish) return
-    disposable?.dispose()
     //页面无缝过渡后重置背景，不然会导致页面显示出现问题。主要解决由于window背景设置后的一些问题
     window.setBackgroundDrawable(null)
-    //有尺寸了才开始计时
-    mContentView.post {
-      disposable = Flowable.intervalRange(0, count + 1, 0, 1, TimeUnit.SECONDS)
-          .compose(RxUtils.rx2SchedulerHelperF(lifecycleProvider))
-          .doOnNext { splashTime.text = String.format("%d", max(1, count - it)) }
-          .doOnComplete {
-            "倒计时结束".logE()
-            countDownFinish = true
-            goNextPage()
-          }
-          .subscribe()
-    }
-  }
-
-  override fun initData() {
-    if (hasFinish) return
-    loadData()
-    //UI显示出来再执行倒计时和权限判断
-    mContentView.post {
-      val temp = PermissionUtils.hasSDPermission()
-      //请求SD卡权限
-      if (!temp) {
-        XXPermissions.with(this)
-            .permission(Permission.MANAGE_EXTERNAL_STORAGE)
-            .request(object : OnPermissionCallback {
-              override fun onGranted(granted: MutableList<String>, all: Boolean) {
-                if (PermissionUtils.hasSDPermission()) {
-                  hasSDPermission = true
-                  goNextPage()
-                } else {
-                  mContext.toast("没有SD卡权限,不能使用APP")
-                  hasSDPermission = false
-                  goNextPage()
-                }
-              }
-
-              override fun onDenied(denied: MutableList<String>, quick: Boolean) {
-                mContext.toast("没有SD卡权限,不能使用APP")
-                hasSDPermission = false
-                goNextPage()
-              }
-            })
-      } else {
-        hasSDPermission = true
-        goNextPage()
+    splashRoot.viewTreeObserver.addOnGlobalLayoutListener {
+      if (splashRoot.height > 0 && isFirstSize) {
+        isFirstSize = false
+        initAfterSize()
       }
     }
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="View尺寸拿到之后获取权限和添加动画">
+  private fun initAfterSize() {
+    if (hasFinish) return
+    //添加PAG动画
+    val pagView = PAGView(mContext)
+    pagView.alpha = 0f
+    mPAGView = pagView
+    pagView.setScaleMode(PAGScaleMode.Zoom)
+    splashRoot.addView(pagView, ViewGroup.LayoutParams(-1, -1))
+    val pagFile = PAGFile.Load(assets, "guide.pag")
+    val textData = pagFile.getTextData(0)
+    textData.fauxItalic = false
+    textData.fauxBold = true
+    textData.strokeWidth = 6f
+    textData.fillColor = Color.MAGENTA
+    textData.strokeColor = Color.CYAN
+    textData.strokeOverFill = false
+    textData.text = "这只是一个\n演示用的基础库"
+    textData.fontSize = 100f
+    pagView.setTextData(0, textData)
+    pagView.setRepeatCount(1)
+    pagView.file = pagFile
+    pagView.addListener(object : PAGView.PAGViewListener {
+      override fun onAnimationStart(view: PAGView?) {}
+
+      override fun onAnimationEnd(view: PAGView?) {
+        countDownFinish = true
+        goNextPage()
+      }
+
+      override fun onAnimationCancel(view: PAGView?) {
+        view?.stop()
+      }
+
+      override fun onAnimationRepeat(view: PAGView?) {}
+
+    })
+    pagView.animate().alpha(1f).setDuration(1000).start()
+    splashBgView.animate().alpha(0f).setDuration(1000).start()
+    pagView.play()
+    hasSDPermission = PermissionUtils.hasSDPermission()
+    //请求SD卡权限
+    if (!hasSDPermission) {
+      XXPermissions.with(this)
+          .permission(Permission.MANAGE_EXTERNAL_STORAGE)
+          .request(object : OnPermissionCallback {
+            override fun onGranted(granted: MutableList<String>, all: Boolean) {
+              if (PermissionUtils.hasSDPermission()) {
+                hasSDPermission = true
+                goNextPage()
+              } else {
+                mContext.toast("没有SD卡权限,不能使用APP")
+                finish()
+              }
+            }
+
+            override fun onDenied(denied: MutableList<String>, quick: Boolean) {
+              mContext.toast("没有SD卡权限,不能使用APP")
+              finish()
+            }
+          })
+    } else {
+      goNextPage()
+    }
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="初始化Data">
+  override fun initData() {
+    if (hasFinish) return
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="如果程序在后台，点击APP图片，不再重新走启动页流程">
   //https://www.cnblogs.com/xqz0618/p/thistaskroot.html
   private fun checkReOpenHome(): Boolean {
     // 避免从桌面启动程序后，会重新实例化入口类的activity
@@ -130,59 +161,37 @@ class SplashActivity : CommActivity() {
     }
     return false
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="进入下个页面">
   //打开下个页面
   private fun goNextPage() {
-    if (hasSDPermission == null) return
-    if (countDownFinish != true) return
-    if (hasSDPermission == true) {
-      when {
-        //是否引导
-        MMkvUtils.getNeedGuide() -> GuideActivity.startActivity(mContext)
-        //是否登录
-        UserRepository.isLogin() -> MainActivity.startActivity(mContext)
-        //没有其他需要，进入主页
-        else -> LoginActivity.startActivity(mContext)
-      }
+    if (!countDownFinish) return
+    if (!hasSDPermission) return
+    when {
+      //是否引导
+      MMkvUtils.getNeedGuide() -> GuideActivity.startActivity(mContext)
+      //是否登录
+      UserRepository.isLogin() -> MainActivity.startActivity(mContext)
+      //没有其他需要，进入主页
+      else -> LoginActivity.startActivity(mContext)
     }
     finish()
   }
+  //</editor-fold>
 
-  /**
-   * 流程：默认显示的window背景
-   * 1.有当前缓存图片，则直接展示
-   * 2.没有缓存图片：
-   *    A.有上次缓存图片，显示缓存并预加载新图
-   *    B.没有任何缓存，显示默认并进行预加载
-   */
-  private fun loadData() {
-    //图片地址
-    val url = ImageUrls.getRandomImgUrl(randomImg)
-    //判断是否存在缓存图片
-    val cacheFile = splashCover.getCacheFile(url)
-    //缓存图片存在
-    if (cacheFile?.exists() == true) {
-      splashCover.load(cacheFile, Utils.getApp().imageLoader)
-    } else { //加载网络图片
-      splashCover.gone()
-      Utils.getApp().imageLoader.enqueue(
-          ImageRequest.Builder(Utils.getApp()).data(url).target(
-              onStart = {
-                "闪屏页图片开始下载".logE()
-              },
-              onSuccess = {
-                "闪屏页图片下载成功".logE()
-              },
-              onError = {
-                "闪屏页图片下载失败:${url}".logE()
-              }
-          ).build()
-      )
-    }
-  }
+  //<editor-fold defaultstate="collapsed" desc="生命周期">
+  //禁止返回
+  override fun onBackPressed() {}
 
   override fun finish() {
-    disposable?.dispose()
+    mPAGView?.stop()
     super.finish()
   }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mPAGView?.stop()
+  }
+  //</editor-fold>
 }
