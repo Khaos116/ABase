1a941681364e9a1afc984799c48207d33401d598
CASE <442415631@qq.com>
2021-01-12 17:09:36 +0700

    修改个人信息保存

--- app/src/main/java/cc/abase/demo/rxhttp/repository/UserRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/UserRepository.kt
@@ -4,16 +4,15 @@ import android.annotation.SuppressLint
 import cc.ab.base.utils.RxUtils
 import cc.abase.demo.bean.wan.IntegralBean
 import cc.abase.demo.bean.wan.UserBean
-import cc.abase.demo.constants.BaseUrl
+import cc.abase.demo.config.UserManager
 import cc.abase.demo.constants.WanUrls
-import cc.abase.demo.rxhttp.config.RxCookie
 import cc.abase.demo.utils.MMkvUtils
 import com.blankj.utilcode.util.EncryptUtils
 import com.blankj.utilcode.util.LogUtils
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import rxhttp.wrapper.cahce.CacheMode
 import rxhttp.RxHttp
+import rxhttp.wrapper.cahce.CacheMode
 
 /**
  * Description:
@@ -22,33 +21,25 @@ import rxhttp.RxHttp
  */
 object UserRepository {
   //注册
-  fun register(
-      username: String,
-      password: String,
-      repassword: String
-  ): Observable<UserBean> {
+  fun register(username: String, password: String, repassword: String): Observable<UserBean> {
     return RxHttp.postForm(WanUrls.User.REGISTER)
-        .setDomainToWanIfAbsent()
         .add("username", username)
         .add("password", EncryptUtils.encryptMD5ToString(password))
         .add("repassword", EncryptUtils.encryptMD5ToString(repassword))
         .setCacheMode(CacheMode.ONLY_NETWORK) //不使用缓存
         .asResponseWan(UserBean::class.java)
         .map {
-          setUid(it.id)
-          this.user = it
+          UserManager.setUid(it.id)
+          MMkvUtils.setAccount(username)
+          MMkvUtils.setPassword(password)
           it
         }
         .compose(RxUtils.rx2SchedulerHelperODelay())
   }
 
   //登录
-  fun login(
-      username: String,
-      password: String
-  ): Observable<UserBean> {
+  fun login(username: String, password: String): Observable<UserBean> {
     return RxHttp.postForm(WanUrls.User.LOGIN)
-        .setDomainToWanIfAbsent()
         .add("username", username)
         .add("password", EncryptUtils.encryptMD5ToString(password))
         .setAssemblyEnabled(true) //添加公共参数/头部
@@ -56,10 +47,9 @@ object UserRepository {
         .asResponseWan(UserBean::class.java)
         .observeOn(AndroidSchedulers.mainThread()) //指定在主线程回调
         .map {
-          setUid(it.id)
+          UserManager.setUid(it.id)
           MMkvUtils.setAccount(username)
           MMkvUtils.setPassword(password)
-          this.user = it
           /**
            * 采用自动管理Cookie的方式可以采用下面的方式保存Token
            * @see cc.abase.demo.rxhttp.config.RxHttpConfig.getDefaultOkHttpClient
@@ -77,7 +67,6 @@ object UserRepository {
   fun logOut() {
     RxHttp.get(WanUrls.User.LOGOUT)
         .setCacheMode(CacheMode.ONLY_NETWORK) //不使用缓存
-        .setDomainToWanIfAbsent()
         .asString()
         .map { LogUtils.e("退出成功:$it") }
         .subscribe({}, { LogUtils.e("退出失败:$it") })
@@ -86,56 +75,7 @@ object UserRepository {
   //我的积分
   fun myIntegral(): Observable<IntegralBean> {
     return RxHttp.get(WanUrls.User.INTEGRAL)
-        .setDomainToWanIfAbsent()
         .asResponseWan(IntegralBean::class.java)
         .compose(RxUtils.rx2SchedulerHelperODelay())
-  }
-
-  //======================用户登录相关信息======================//
-  private var user: UserBean? = null
-  private var uid: Long = 0
-  private var token: String? = null
-  fun isLogin(): Boolean {
-    if (uid == 0L) getUid()
-    if (token.isNullOrBlank()) getToken()
-    return uid > 0 && !token.isNullOrBlank()
-  }
-
-  fun getUid(): Long {
-    if (uid == 0L) uid = MMkvUtils.getUid()
-    return uid
-  }
-
-  fun getToken(): String? {
-    if (token.isNullOrBlank()) token = MMkvUtils.getToken()
-    return token
-  }
-
-  fun getUser(): UserBean? {
-    return user
-  }
-
-  fun clearUserInfo() {
-    uid = 0
-    token = null
-    user = null
-    MMkvUtils.clearUserInfo()
-  }
-
-  private fun setUid(uid: Long) {
-    this.uid = uid
-    MMkvUtils.setUid(uid)
-  }
-
-  fun setToken(
-      token: String,
-      url: String = BaseUrl.gankUrl
-  ) {
-    if (token.isNotBlank() && token != this.token) {
-      LogUtils.e("CASE:更新Token为:${token}")
-      this.token = token
-      MMkvUtils.setToken(token)
-      RxCookie.setCookie(token, url)
-    }
   }
 }
