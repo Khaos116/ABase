b5905fc3e73badbd15f4877caddc465c906ee19b
CASE <442415631@qq.com>
2021-01-12 17:16:46 +0700

    去掉Domain

--- app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
@@ -5,7 +5,7 @@ import cc.ab.base.net.http.response.BasePageList
 import cc.abase.demo.bean.wan.ArticleBean
 import cc.abase.demo.bean.wan.BannerBean
 import cc.abase.demo.constants.TimeConstants
-import cc.abase.demo.constants.WanUrls
+import cc.abase.demo.constants.api.WanUrls
 import rxhttp.RxHttp
 import rxhttp.toResponseWan
 import rxhttp.wrapper.cahce.CacheMode
@@ -20,7 +20,6 @@ object WanRepository {
   //获取Banner
   suspend fun banner(readCache: Boolean = true): MutableList<BannerBean> {
     return RxHttp.get(WanUrls.Home.BANNER)
-        .setDomainToWanIfAbsent()
         .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
         .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
         .toResponseWan<MutableList<BannerBean>>()
@@ -30,7 +29,6 @@ object WanRepository {
   //获取文章列表
   suspend fun article(@IntRange(from = 0) page: Int, readCache: Boolean = true): BasePageList<ArticleBean> {
     return RxHttp.get(String.format(WanUrls.Home.ARTICLE, page))
-        .setDomainToWanIfAbsent()
         .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
         .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
         .toResponseWan<BasePageList<ArticleBean>>()
