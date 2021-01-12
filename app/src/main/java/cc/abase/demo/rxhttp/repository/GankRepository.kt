9cdff46ae440de4aaf88141ad1605d76b39f5dc2
CASE <442415631@qq.com>
2021-01-12 17:16:25 +0700

    去掉Domain

--- app/src/main/java/cc/abase/demo/rxhttp/repository/GankRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/GankRepository.kt
@@ -2,11 +2,11 @@ package cc.abase.demo.rxhttp.repository
 
 import androidx.annotation.IntRange
 import cc.abase.demo.bean.gank.GankAndroidBean
-import cc.abase.demo.constants.GankUrls
 import cc.abase.demo.constants.TimeConstants
+import cc.abase.demo.constants.api.GankUrls
 import rxhttp.RxHttp
-import rxhttp.wrapper.cahce.CacheMode
 import rxhttp.toResponseGank
+import rxhttp.wrapper.cahce.CacheMode
 
 /**
  * Description:
@@ -17,7 +17,6 @@ object GankRepository  {
   //安卓列表
   suspend fun androidList(@IntRange(from = 1) page: Int, size: Int, readCache: Boolean = true): MutableList<GankAndroidBean> {
     return RxHttp.get(String.format(GankUrls.ANDROID, page, size))
-        .setDomainToGankIfAbsent()
         .setCacheValidTime(TimeConstants.DYN_CACHE) //设置缓存时长
         .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
         .toResponseGank<MutableList<GankAndroidBean>>()
