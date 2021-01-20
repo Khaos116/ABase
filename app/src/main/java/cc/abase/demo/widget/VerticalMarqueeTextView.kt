ffd33e3dda8dfff18c389f2e8e525450d46167ce
CASE <442415631@qq.com>
2021-01-20 15:19:29 +0700

    兼容html

--- app/src/main/java/cc/abase/demo/widget/VerticalMarqueeTextView.kt
+++ app/src/main/java/cc/abase/demo/widget/VerticalMarqueeTextView.kt
@@ -5,10 +5,12 @@ import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
+import android.text.Html
 import android.util.AttributeSet
 import android.view.Gravity
 import android.widget.FrameLayout
 import cc.ab.base.ext.dp2Px
+import cc.abase.demo.R
 
 /**
  * @Description
@@ -27,6 +29,7 @@ class VerticalMarqueeTextView @JvmOverloads constructor(c: Context, a: Attribute
 
   //<editor-fold defaultstate="collapsed" desc="初始化">
   init {
+    this.setBackgroundResource(R.drawable.shape_half_orange)
     //抗锯齿
     mTv1.paintFlags = Paint.ANTI_ALIAS_FLAG
     mTv2.paintFlags = Paint.ANTI_ALIAS_FLAG
@@ -42,9 +45,9 @@ class VerticalMarqueeTextView @JvmOverloads constructor(c: Context, a: Attribute
     mTv2.setTextColor(textColor)
     //内部间距
     val paddingStartEnd = 11.dp2Px()
-    val paddingTop = 1.dp2Px()
-    mTv1.setPadding(paddingStartEnd, paddingTop, paddingStartEnd, 0)
-    mTv2.setPadding(paddingStartEnd, paddingTop, paddingStartEnd, 0)
+    val paddingTopTxt = 1.dp2Px()
+    mTv1.setPadding(paddingStartEnd, paddingTopTxt, paddingStartEnd, 0)
+    mTv2.setPadding(paddingStartEnd, paddingTopTxt, paddingStartEnd, 0)
     addView(mTv1, FrameLayout.LayoutParams(-1, -1))
     addView(mTv2, FrameLayout.LayoutParams(-1, -1))
     //第二个先看不见
@@ -68,14 +71,15 @@ class VerticalMarqueeTextView @JvmOverloads constructor(c: Context, a: Attribute
   fun updateNotice(notice: CharSequence) {
     //第一次设置
     if (mTv1.text.toString().isBlank() && mTv2.text.toString().isBlank()) {
-      mTv1.text = notice
+      mTv1.text = if (notice.contains("<font")) Html.fromHtml(notice.toString()) else notice
       return
     }
     if (isAnim) return
     isAnim = true
     val inTv = if (mTv1.translationY == 0f) mTv1 else mTv2
     val outTv = if (inTv == mTv1) mTv2 else mTv1
-    outTv.text = notice
+    outTv.marqueeRepeatLimit = 0
+    outTv.text = if (notice.contains("<font")) Html.fromHtml(notice.toString()) else notice
     inTv.animate().translationY(-mHeight).setDuration(500).start()
     outTv.animate().translationY(0f).setDuration(500)
         .setListener(object : AnimatorListenerAdapter() {
@@ -86,6 +90,8 @@ class VerticalMarqueeTextView @JvmOverloads constructor(c: Context, a: Attribute
             inTv.translationY = mHeight
             outTv.animate().setListener(null)
             outTv.postInvalidate()
+            outTv.marqueeRepeatLimit = -1
+            outTv.onWindowFocusChanged(true)
           }
         })
         .start()
