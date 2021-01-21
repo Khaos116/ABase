445c577abaf74831cefb077aee8cb8a849a8a0b6
CASE <442415631@qq.com>
2021-01-21 12:41:28 +0700

    增加Tag

--- app/src/main/java/cc/abase/demo/constants/StringConstants.kt
+++ app/src/main/java/cc/abase/demo/constants/StringConstants.kt
@@ -5,20 +5,26 @@ package cc.abase.demo.constants
  * @author: CASE
  * @date: 2019/10/30 10:10
  */
-interface StringConstants {
-  interface Update {
-    companion object {
-      //安装APP的intent key
-      const val INTENT_KEY_INSTALL_APP = "INTENT_KEY_INSTALL_APP"
-      //更新的消息id
-      const val INTENT_KEY_UPDATE_ID = "INTENT_KEY_UPDATE_ID"
-      //下载app失败
-      const val INTENT_KEY_APK_DOWNLOAD_ERROR = "INTENT_KEY_APK_DOWNLOAD_ERROR"
-      //需要安装的APK路径
-      const val INTENT_KEY_INSTALL_PATH = "INTENT_KEY_INSTALL_PATH"
-      const val INTENT_KEY_RETRY_PATH = "INTENT_KEY_RETRY_PATH"
-      const val INTENT_KEY_RETRY_NAME = "INTENT_KEY_RETRY_NAME"
-      const val INTENT_KEY_APP_NAME = "INTENT_KEY_APP_NAME"
-    }
+object StringConstants {
+  object Update {
+    //安装APP的intent key
+    const val INTENT_KEY_INSTALL_APP = "INTENT_KEY_INSTALL_APP"
+
+    //更新的消息id
+    const val INTENT_KEY_UPDATE_ID = "INTENT_KEY_UPDATE_ID"
+
+    //下载app失败
+    const val INTENT_KEY_APK_DOWNLOAD_ERROR = "INTENT_KEY_APK_DOWNLOAD_ERROR"
+
+    //需要安装的APK路径
+    const val INTENT_KEY_INSTALL_PATH = "INTENT_KEY_INSTALL_PATH"
+    const val INTENT_KEY_RETRY_PATH = "INTENT_KEY_RETRY_PATH"
+    const val INTENT_KEY_RETRY_NAME = "INTENT_KEY_RETRY_NAME"
+    const val INTENT_KEY_APP_NAME = "INTENT_KEY_APP_NAME"
+  }
+
+  object Tag {
+    //悬浮窗播放Tag
+    const val FLOAT_PLAY = "float_play"
   }
 }
