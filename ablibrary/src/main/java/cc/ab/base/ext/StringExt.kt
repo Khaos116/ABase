01d0cfd091557ba225123d6bd53e5bca7bed6d50
CASE <442415631@qq.com>
2021-02-05 18:54:42 +0700

    保存图片到相册

--- ablibrary/src/main/java/cc/ab/base/ext/StringExt.kt
+++ ablibrary/src/main/java/cc/ab/base/ext/StringExt.kt
@@ -1,13 +1,19 @@
 package cc.ab.base.ext
 
+import android.content.Intent
 import android.net.Uri
 import android.view.Gravity
-import com.blankj.utilcode.util.AppUtils
-import com.blankj.utilcode.util.ToastUtils
-import com.blankj.utilcode.util.UriUtils
+import coil.imageLoader
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import coil.util.CoilUtils
+import com.blankj.utilcode.util.*
+import kotlinx.coroutines.GlobalScope
+import okhttp3.Cache
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import timber.log.Timber
 import java.io.File
-import java.util.*
+import java.util.Locale
 import java.util.regex.Pattern
 
 /**
@@ -88,6 +94,91 @@ fun String?.toFile(): File? {
     return if (this.startsWith("http", true)) null else {
       val f = File(this)
       if (f.exists()) f else UriUtils.uri2File(Uri.parse(this))
+    }
+  }
+  return null
+}
+
+//将图片保存到相册
+@Suppress("DEPRECATION")
+fun String?.downloadImg(call: (start: Boolean, end: Boolean, sucPath: String) -> Unit) {
+  val url = this
+  if (url.isNullOrBlank()) {
+    call.invoke(false, true, "")
+    return
+  }
+  GlobalScope.launchError(handler = { _, e ->
+    e.logE()
+    call.invoke(false, true, "")
+  }) {
+    call.invoke(true, false, "")
+    val cacheFile = url.getCacheFile()
+    if (cacheFile == null && url.startsWith("http")) {
+      val request = ImageRequest.Builder(Utils.getApp())
+          .data(url)
+          .memoryCachePolicy(CachePolicy.DISABLED)
+          .listener(
+              onCancel = { call.invoke(false, true, "") },
+              onError = { _, t ->
+                t.logE()
+                call.invoke(false, true, "")
+              },
+              onSuccess = { _, _ ->
+                url.getCacheFile()?.let { f ->
+                  val dcim = PathUtils.getExternalDcimPath()
+                  val picture = PathUtils.getExternalPicturesPath()
+                  val dest = if (dcim.isNullOrBlank()) picture else dcim
+                  if (dest.isNullOrBlank()) {
+                    call.invoke(false, true, "")
+                  } else {
+                    val destPath = "$dest${File.separator}${EncryptUtils.encryptMD5File2String(f)}.jpg"
+                    val result = if (File(destPath).exists()) true else FileUtils.copy(f.path, destPath)
+                    //发送广播刷新图片
+                    if (result) {
+                      Utils.getApp().sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, UriUtils.file2Uri(f)))
+                      call.invoke(false, true, destPath)
+                    } else call.invoke(false, true, "")
+                  }
+                } ?: call.invoke(false, true, "")
+              }
+          )
+          .build()
+      Utils.getApp().imageLoader.enqueue(request)
+    } else {
+      (cacheFile ?: url.toFile())?.let { f ->
+        val dcim = PathUtils.getExternalDcimPath()
+        val picture = PathUtils.getExternalPicturesPath()
+        val dest = if (dcim.isNullOrBlank()) picture else dcim
+        if (dest.isNullOrBlank()) {
+          call.invoke(false, true, "")
+        } else {
+          val destPath = "$dest${File.separator}${EncryptUtils.encryptMD5File2String(f)}.jpg"
+          val result = if (File(destPath).exists()) true else FileUtils.copy(f.path, destPath)
+          //发送广播刷新图片
+          if (result) {
+            Utils.getApp().sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, UriUtils.file2Uri(f)))
+            call.invoke(false, true, destPath)
+          } else call.invoke(false, true, "")
+        }
+      } ?: call.invoke(false, true, "")
+    }
+  }
+}
+
+//获取缓存文件
+fun String?.getCacheFile(): File? {
+  val url = this
+  url?.let { u ->
+    var f = u.toFile()
+    if (f != null) {
+      return f
+    } else {
+      url.toHttpUrlOrNull()?.let { h ->
+        f = CoilUtils.createDefaultCache(Utils.getApp()).directory.listFiles()?.firstOrNull { v -> v.name.contains(Cache.key(h)) }
+        if (f?.exists() == true) {
+          return f
+        }
+      }
     }
   }
   return null
