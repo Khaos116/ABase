c33ca49373964f3a69d362217cfcdd457aa513eb
CASE <442415631@qq.com>
2021-02-18 19:08:32 +0700

    增加返回是否需要关闭窗口

--- app/src/main/java/cc/abase/demo/widget/dialog/CommAlertDialog.kt
+++ app/src/main/java/cc/abase/demo/widget/dialog/CommAlertDialog.kt
@@ -31,7 +31,7 @@ class CommAlertDialog : BaseFragmentDialog() {
   var confirmText: String? = null
   var confirmTextColor: Int? = null
   var cancelCallback: (() -> Unit)? = null
-  var confirmCallback: (() -> Unit)? = null
+  var confirmCallback: (() -> Boolean)? = null
   var contentHorizontal: Boolean = false
   var titleHorizontal: Boolean = true
 
@@ -74,8 +74,8 @@ class CommAlertDialog : BaseFragmentDialog() {
       dismissAllowingStateLoss()
     }
     commAlertConfirm.click {
-      confirmCallback?.invoke()
-      dismissAllowingStateLoss()
+      val dis = confirmCallback?.invoke() ?: true
+      if (dis) dismissAllowingStateLoss()
     }
     //按钮类型
     when (type) {
