7a99dcb7c13b7af8c319a732ec01b7e49124701a
CASE <442415631@qq.com>
2021-02-20 14:33:28 +0700

    优化基类

--- ablibrary/src/main/java/cc/ab/base/ui/item/BaseItemView.kt
+++ ablibrary/src/main/java/cc/ab/base/ui/item/BaseItemView.kt
@@ -1,7 +1,9 @@
 package cc.ab.base.ui.item
 
-import android.view.*
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import cc.ab.base.ext.click
 import com.drakeet.multitype.ItemViewBinder
 
 /**
@@ -10,7 +12,9 @@ import com.drakeet.multitype.ItemViewBinder
  * @Date：2021/2/20
  * @Time：10:42
  */
-abstract class BaseItemView<T> : ItemViewBinder<T, BaseViewHolder>() {
+abstract class BaseItemView<T>(
+    protected var onItemClick: ((item: T) -> Unit)? = null
+) : ItemViewBinder<T, BaseViewHolder>() {
   //<editor-fold defaultstate="collapsed" desc="创建ViewHolder">
   override fun onCreateViewHolder(inflater: LayoutInflater, parent: ViewGroup) = BaseViewHolder(inflater.inflate(layoutResId(), parent, false))
   //</editor-fold>
@@ -18,7 +22,14 @@ abstract class BaseItemView<T> : ItemViewBinder<T, BaseViewHolder>() {
   //<editor-fold defaultstate="collapsed" desc="绑定数据">
   //holder可以获得holder.layoutPosition
   override fun onBindViewHolder(holder: BaseViewHolder, item: T) {
-    holder.apply(fillData(holder.itemView, item))
+    //写在fillData前，方便fillData修改点击事件
+    if (onItemClick == null) {
+      holder.itemView.setOnClickListener(null)
+    } else {
+      holder.itemView.click { onItemClick?.invoke(item) }
+    }
+    //SDL方式填充数据，解决findViewById的麻烦
+    holder.apply(fillData(item))
   }
   //</editor-fold>
 
@@ -28,6 +39,6 @@ abstract class BaseItemView<T> : ItemViewBinder<T, BaseViewHolder>() {
   protected abstract fun layoutResId(): Int
 
   //加载数据(返回值是为了解决findViewById)
-  abstract fun fillData(itemView: View, item: T): BaseViewHolder.() -> Unit
+  abstract fun fillData(item: T): BaseViewHolder.() -> Unit
   //</editor-fold>
 }
