9db19e1c3d339035cb1d28a37d89fd2f3f0a8f6f
CASE <442415631@qq.com>
2021-02-23 16:51:36 +0700

    格式化

--- ablibrary/src/main/java/cc/ab/base/widget/roundlayout/widget/GeneralRoundConstraintLayout.kt
+++ ablibrary/src/main/java/cc/ab/base/widget/roundlayout/widget/GeneralRoundConstraintLayout.kt
@@ -16,42 +16,37 @@ import cc.ab.base.widget.roundlayout.abs.IRoundView
  * @time Created by 2019/6/8 0:36
  *
  */
-open class GeneralRoundConstraintLayout : ConstraintLayout, IRoundView {
-    private lateinit var generalRoundViewImpl: GeneralRoundViewImpl
+class GeneralRoundConstraintLayout : ConstraintLayout, IRoundView {
+  private lateinit var generalRoundViewImpl: GeneralRoundViewImpl
 
-    constructor(context: Context) : this(context, null)
+  constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init(this, context, attrs)
-    }
+  constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+    init(this, context, attrs)
+  }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(this, context, attrs)
-    }
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    init(this, context, attrs)
+  }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-        generalRoundViewImpl.onLayout(changed, left, top, right, bottom)
-    }
+  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+    super.onLayout(changed, left, top, right, bottom)
+    generalRoundViewImpl.onLayout(changed, left, top, right, bottom)
+  }
 
-    override fun dispatchDraw(canvas: Canvas?) {
-        generalRoundViewImpl.beforeDispatchDraw(canvas)
-        super.dispatchDraw(canvas)
-        generalRoundViewImpl.afterDispatchDraw(canvas)
-    }
+  override fun dispatchDraw(canvas: Canvas?) {
+    generalRoundViewImpl.beforeDispatchDraw(canvas)
+    super.dispatchDraw(canvas)
+    generalRoundViewImpl.afterDispatchDraw(canvas)
+  }
 
-    private fun init(view: View, context: Context, attributeSet: AttributeSet?) {
-        generalRoundViewImpl = GeneralRoundViewImpl(
-            view,
-            context,
-            attributeSet,
-            R.styleable.GeneralRoundConstraintLayout,
-            R.styleable.GeneralRoundRelativeLayout_corner_radius
-        )
-    }
+  private fun init(view: View, context: Context, attributeSet: AttributeSet?) {
+    generalRoundViewImpl = GeneralRoundViewImpl(view, context, attributeSet, R.styleable.GeneralRoundConstraintLayout,
+        R.styleable.GeneralRoundRelativeLayout_corner_radius
+    )
+  }
 
-    override fun setCornerRadius(cornerRadius: Float) {
-        generalRoundViewImpl.setCornerRadius(cornerRadius)
-    }
-
+  override fun setCornerRadius(cornerRadius: Float) {
+    generalRoundViewImpl.setCornerRadius(cornerRadius)
+  }
 }
