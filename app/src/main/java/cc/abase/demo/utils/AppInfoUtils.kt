eca08745f3fecdecd3a5bf0ae80b94a74f3bb12b
CASE <442415631@qq.com>
2021-03-19 15:06:47 +0700

    优化CPU显示

--- app/src/main/java/cc/abase/demo/utils/AppInfoUtils.kt
+++ app/src/main/java/cc/abase/demo/utils/AppInfoUtils.kt
@@ -20,38 +20,34 @@ object AppInfoUtils {
   fun getAppInfo(): CharSequence {
     val builder = SpannableStringBuilder()
     builder.append("APP信息：").append("\n")
-        .append("=====================================\n")
-        .append("Emulator1：").append(EmulatorDetectUtil.isEmulator(Utils.getApp()).toString()).append("\n")
-        .append("Emulator2：").append(DeviceUtils.isEmulator().toString()).append("\n")
-        .append("UUID：").append(DeviceUtils.getUniqueDeviceId()).append("\n")
-        .append("Release：").append(BuildConfig.APP_IS_RELEASE.toString()).append("\n")
-        .append("BuildTime：").append(StringUtils.getString(R.string.build_time)).append("\n")
-        .append("VersionName：").append(AppUtils.getAppVersionName()).append("\n")
-        .append("VersionCode：").append(AppUtils.getAppVersionCode().toString()).append("\n")
-        .append("BaseUrl：").append(ApiUrl.appBaseUrl).append("\n")
-        .append("CPU：").append(getDeviceCPU()).append("\n")
-        .append("渠道号").append(MySpanUtils.getSpanSimple("[测试服永远-1]")).append("：").append(WalleUtils.getChannel()).append("\n")
-        .append("Wifi-BSSID：").append(MacAddressUtils.getConnectedWifiMacAddress(Utils.getApp()) ?: "null").append("\n") //需要地理位置权限
-        .append("=====================================\n")
-        .append("签名SHA1：").append("\n")
-        .append(AppUtils.getAppSignaturesSHA1().firstOrNull()).append("\n")
-        .append("签名SHA256：").append("\n")
-        .append(AppUtils.getAppSignaturesSHA256().firstOrNull()).append("\n")
-        .append("签名MD5：").append("\n")
-        .append(AppUtils.getAppSignaturesMD5().firstOrNull()).append("\n")
+      .append("=====================================\n")
+      .append("Emulator1：").append(EmulatorDetectUtil.isEmulator(Utils.getApp()).toString()).append("\n")
+      .append("Emulator2：").append(DeviceUtils.isEmulator().toString()).append("\n")
+      .append("UUID：").append(DeviceUtils.getUniqueDeviceId()).append("\n")
+      .append("Release：").append(BuildConfig.APP_IS_RELEASE.toString()).append("\n")
+      .append("BuildTime：").append(StringUtils.getString(R.string.build_time)).append("\n")
+      .append("VersionName：").append(AppUtils.getAppVersionName()).append("\n")
+      .append("VersionCode：").append(AppUtils.getAppVersionCode().toString()).append("\n")
+      .append("BaseUrl：").append(ApiUrl.appBaseUrl).append("\n")
+      .append("CPU：").append(getDeviceCPU()).append("\n")
+      .append("渠道号").append(MySpanUtils.getSpanSimple("[测试服永远-1]")).append("：").append(WalleUtils.getChannel()).append("\n")
+      .append("Wifi-BSSID：").append(MacAddressUtils.getConnectedWifiMacAddress(Utils.getApp()) ?: "null").append("\n") //需要地理位置权限
+      .append("=====================================\n")
+      .append("签名SHA1：").append("\n")
+      .append(AppUtils.getAppSignaturesSHA1().firstOrNull()).append("\n")
+      .append("签名SHA256：").append("\n")
+      .append(AppUtils.getAppSignaturesSHA256().firstOrNull()).append("\n")
+      .append("签名MD5：").append("\n")
+      .append(AppUtils.getAppSignaturesMD5().firstOrNull()).append("\n")
     return builder
   }
 
   //获取CPU架构
   private fun getDeviceCPU(): String {
     return try {
-      BufferedReader(
-          InputStreamReader(
-              Runtime.getRuntime().exec("getprop ro.product.cpu.abi").inputStream
-          )
-      ).readLine()
+      BufferedReader(InputStreamReader(Runtime.getRuntime().exec("getprop ro.product.cpu.abi").inputStream)).readLine()
     } catch (e: Exception) {
-      ""
+      DeviceUtils.getABIs().toString()
     }
   }
 }
