19fa5edf465c62f478c65370bac201bb8ea9cd7b
CASE <442415631@qq.com>
2021-04-05 22:47:11 +0700

    新增时区转换工具

--- app/src/main/java/cc/abase/demo/utils/TimeUtils.kt
+++ app/src/main/java/cc/abase/demo/utils/TimeUtils.kt
@@ -1,9 +1,10 @@
 package cc.abase.demo.utils
 
+import com.google.gson.internal.bind.util.ISO8601Utils
+import java.text.ParsePosition
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.*
 
 /**
  * Description:时区转换工具
@@ -37,8 +38,8 @@ object TimeUtils {
    * @param utcTime  UTC时间
    */
   fun utc2Local(
-      utcTime: String,
-      offSet: Int? = null //当前时区偏移，如:北京+8，纽约-5，默认为手机自带时区
+    utcTime: String,
+    offSet: Int? = null //当前时区偏移，如:北京+8，纽约-5，默认为手机自带时区
   ): String {
     val utcFormatter = SimpleDateFormat(UTC_FORMAT, Locale.getDefault()) //UTC时间格式
     utcFormatter.timeZone = TimeZone.getTimeZone("UTC")
@@ -58,4 +59,79 @@ object TimeUtils {
     }
     return ""
   }
+
+  //<editor-fold defaultstate="collapsed" desc="2021年4月5日22:46:47新增转换">
+  /**
+   * 带时区的时间转换为本地时间显示
+   * @author CASE
+   * @param time GTM带时区的时间格式(格式为[yyyy-MM-dd|yyyyMMdd][T(hh:mm[:ss[.sss]]|hhmm[ss[.sss]])]?[Z|[+-]hh[:mm]]])
+   * @return 返回yyyy-MM-dd HH:mm:ss格式的时间
+   */
+  fun iso8601ToLocal(time: String/*2021-04-03T00:00:00-04:00*/): String {
+    val localFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+    return localFormatter.format(ISO8601Utils.parse(time, ParsePosition(0)))
+  }
+
+  /**
+   * GMT时间转本地时间
+   * @author CASE
+   * @param time GMT时间(格式为格式为yyyy-MM-dd HH:mm:ss)
+   * @param offSet GMT的时区(默认为0时区)
+   * @return 返回yyyy-MM-dd HH:mm:ss格式的时间
+   */
+  fun gmt2Local(time: String/*2021-04-05 21:34:46*/, offSet: Int = 0): String {
+    val split = time.split(" ")
+    if (split.size != 2) return time
+    val offsetStr = if (offSet < 0) {
+      when {
+        offSet > -10 -> "-0${abs(offSet)}:00"
+        offSet > -12 -> "-${abs(offSet)}:00"
+        else -> "-11:00"
+      }
+    } else {
+      when {
+        offSet < 10 -> "+0${offSet}:00"
+        offSet < 12 -> "+${offSet}:00"
+        else -> "+11:00"
+      }
+    }
+    return iso8601ToLocal("${split[0]}T${split[1]}$offsetStr")
+  }
+
+  /**
+   * 本地时间转指定GMT时区时间
+   * @author CASE
+   * @param time 本地时间(格式为yyyy-MM-dd HH:mm:ss)
+   * @param offSet GMT时区(如:北京+8，纽约-5，默认为0时区)
+   * @return 返回yyyy-MM-dd HH:mm:ss格式的时间
+   */
+  fun local2Gmt(time: String/*2021-04-05 21:34:46*/, offSet: Int = 0): String {
+    val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+    val date = try {
+      format.parse(time)
+    } catch (e: Exception) {
+      return time
+    }
+    return local2Gmt(date, offSet)
+  }
+
+  /**
+   * 本地时间转指定GMT时区时间
+   * @author CASE
+   * @param date 本地时间
+   * @param offSet GMT时区(如:北京+8，纽约-5，默认为0时区)
+   * @param offSet GMT时区(如:北京+8，纽约-5，默认为0时区)
+   * @param endSSS 是否返回带毫秒的时间
+   * @return 返回yyyy-MM-dd HH:mm:ss或者yyyy-MM-dd HH:mm:ss.SSS格式的时间
+   */
+  fun local2Gmt(date: Date = Date(), offSet: Int = 0, endSSS: Boolean = false): String {
+    val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss${if (endSSS) ".SSS" else ""}", Locale.getDefault())
+    if (offSet < 0) {
+      format.timeZone = TimeZone.getTimeZone("GMT${min(offSet, 11)}")
+    } else {
+      format.timeZone = TimeZone.getTimeZone("GMT+${min(offSet, 11)}")
+    }
+    return format.format(date)
+  }
+  //</editor-fold>
 }
