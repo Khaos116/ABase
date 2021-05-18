3079fed593f59c5d3ec5c8a268395db20203a5ec
CASE <442415631@qq.com>
2021-05-18 13:16:52 +0700

    重命名

--- app/src/main/java/cc/abase/demo/app/TestApplication.kt
+++ app/src/main/java/cc/abase/demo/app/TestApplication.kt
@@ -1,14 +1,14 @@
 package cc.abase.demo.app
 
+import android.app.Application
 import android.content.pm.PackageManager
-import cc.ab.base.app.BaseApplication
 
 /**
  * Description:
  * @author: CASE
  * @date: 2019/10/8 10:02
  */
-open class MyApplication : BaseApplication() {
+open class TestApplication : Application() {
   //是否需要走修改后的包信息(只能修改APP运行后的读取)
   var needChangePackageManager = false
   override fun getPackageManager(): PackageManager {
