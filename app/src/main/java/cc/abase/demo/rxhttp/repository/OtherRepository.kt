e89015386ba90376a31aa8110e2f784e5903cd64
CASE <442415631@qq.com>
2021-06-02 21:31:38 +0700

    只获取外网IP的地址

--- app/src/main/java/cc/abase/demo/rxhttp/repository/OtherRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/OtherRepository.kt
@@ -12,6 +12,19 @@ import rxhttp.wrapper.cahce.CacheMode
  * @Time：20:58
  */
 object OtherRepository {
+  //只获取外网IP的地址
+  private var netIpUrls = mutableListOf(
+    "http://bot.whatismyipaddress.com/",
+    "http://checkip.amazonaws.com",
+    "https://ipv4.icanhazip.com",
+    "http://www.icanhazip.com",
+    "http://icanhazip.com",
+    "http://ipinfo.io/ip",
+    "https://api.ip.sb/ip",
+    "https://ifconfig.co/ip",
+    "http://www.trackip.net/ip",
+  )
+
   suspend fun getNetIp(): IpBean {
     //备用地址 https://www.cloudflare.com/cdn-cgi/trace
     return RxHttp.get("http://ip-api.com/json/?lang=zh-CN")
