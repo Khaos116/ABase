360df732ede3ec5698c4e6892d4baa4ab0d3d542
CASE <442415631@qq.com>
2021-06-10 00:03:48 +0700

    正式版只能在主线程初始化，所以统一改为true

--- ablibrary/src/main/java/cc/ab/base/startup/UtilsInit.kt
+++ ablibrary/src/main/java/cc/ab/base/startup/UtilsInit.kt
@@ -17,7 +17,7 @@ import com.rousetime.android_startup.Startup
 class UtilsInit : AndroidStartup<Int>() {
   //<editor-fold defaultstate="collapsed" desc="初始化线程问题">
   //create()方法调时所在的线程：如果callCreateOnMainThread返回true，则表示在主线程中初始化，会导致waitOnMainThread返回值失效；当返回false时，才会判断waitOnMainThread的返回值
-  override fun callCreateOnMainThread(): Boolean = false
+  override fun callCreateOnMainThread(): Boolean = true //正式版只能在主线程初始化，所以统一改为true
 
   //是否需要在主线程进行等待其完成:如果返回true，将在主线程等待，并且阻塞主线程
   override fun waitOnMainThread(): Boolean = false
