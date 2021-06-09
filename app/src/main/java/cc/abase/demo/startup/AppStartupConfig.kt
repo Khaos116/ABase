1ea0a6baa02b4485de23b049f5732801eb53058d
CASE <442415631@qq.com>
2021-06-09 23:09:40 +0700

    打印初始化时长

---
+++ app/src/main/java/cc/abase/demo/startup/AppStartupConfig.kt
@@ -0,0 +1,27 @@
+package cc.abase.demo.startup
+
+import cc.ab.base.ext.logE
+import com.rousetime.android_startup.StartupListener
+import com.rousetime.android_startup.model.*
+import com.rousetime.android_startup.provider.StartupProviderConfig
+
+/**
+ * @Description
+ * @Author：CASE
+ * @Date：2021-06-09
+ * @Time：22:25
+ */
+class AppStartupConfig : StartupProviderConfig {
+  override fun getConfig(): StartupConfig {
+    return StartupConfig.Builder()
+      .setLoggerLevel(LoggerLevel.ERROR)
+      .setAwaitTimeout(3000L)
+      .setListener(object : StartupListener {
+        override fun onCompleted(totalMainThreadCostTime: Long, costTimesModels: List<CostTimesModel>) {
+          costTimesModels.forEach { "${it.name}->初始化耗时=${it.endTime - it.startTime}ms".logE() }
+          "初始化总耗时=${totalMainThreadCostTime / (1000 * 1000L)}ms".logE() //1纳秒=0.000001 毫秒
+        }
+      })
+      .build()
+  }
+}
