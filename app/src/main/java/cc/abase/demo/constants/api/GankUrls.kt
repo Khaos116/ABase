f9e259540f755f6f84b4bf46890b1468b0a3e00b
CASE <442415631@qq.com>
2021-06-28 14:36:03 +0700

    baseUrl

--- app/src/main/java/cc/abase/demo/constants/api/GankUrls.kt
+++ app/src/main/java/cc/abase/demo/constants/api/GankUrls.kt
@@ -6,8 +6,11 @@ package cc.abase.demo.constants.api
  * @date: 2019/10/8 18:48
  */
 object GankUrls {
-  const val ANDROID = "https://gank.io/api/v2/data/category/GanHuo/type/Android/page/%s/count/%s" //{page}/{pageSize}
+  const val HOST = "gank.io" //Gank的Host
+  const val baseUrl = "https://gank.io/" //Gank的Host
+
+  const val ANDROID = "${baseUrl}api/v2/data/category/GanHuo/type/Android/page/%s/count/%s" //{page}/{pageSize}
 
   //https://gank.io/api/v2/data/category/Girl/type/Girl/page/1/count/10
-  const val GIRL = "https://gank.io/api/v2/data/category/Girl/type/Girl/page/%s/count/%s" //{page}/{pageSize}
+  const val GIRL = "${baseUrl}api/v2/data/category/Girl/type/Girl/page/%s/count/%s" //{page}/{pageSize}
 }
