db78c7d4a1cf51c33163a0bb67d8baa21e66591e
khaos116 <khaos116@outlook.com>
2021-07-30 16:54:15 +0700

    完善防抖动搜索

--- ablibrary/src/main/java/cc/ab/base/ext/EditExt.kt
+++ ablibrary/src/main/java/cc/ab/base/ext/EditExt.kt
@@ -17,8 +17,10 @@ import kotlinx.coroutines.flow.*
  */
 @FlowPreview
 @ExperimentalCoroutinesApi
-fun EditText.onDebounceTextChanges(lifecycle: Lifecycle, debounceTime: Long = 600, afterChange: (String) -> Unit) {
-  callbackFlow {
+fun EditText.onDebounceTextChanges(life: Lifecycle, time: Long = 600, onStart: Boolean = false, afterChange: (String) -> Unit) {
+  //防止搜索一样的内容
+  var lastSearchStr: String? = null
+  val call: Flow<CharSequence?> = callbackFlow {
     val listener = object : TextWatcher {
       override fun afterTextChanged(s: Editable?) = Unit
       override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
@@ -28,10 +30,14 @@ fun EditText.onDebounceTextChanges(lifecycle: Lifecycle, debounceTime: Long = 60
     }
     addTextChangedListener(listener)
     awaitClose { removeTextChangedListener(listener) }
-  }.onStart { emit(text) }
-    .distinctUntilChanged()
-    .filterNot { it.isNullOrBlank() }
-    .debounce(debounceTime)
-    .mapLatest { afterChange.invoke(it?.toString() ?: "") }
-    .launchIn(lifecycle.coroutineScope)
+  }
+  if (onStart) call.onStart { emit(text) }//添加监听时发送控件的文本信息(否则只有变化时才会回调)
+  call.debounce(time).onEach {
+    val result = it?.toString() ?: ""
+    if (lastSearchStr != result) {
+      lastSearchStr = result
+      "搜索内容:$result".logI()
+      afterChange.invoke(result)
+    }
+  }.launchIn(life.coroutineScope)
 }
