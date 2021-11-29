d692b8c0a48d13ae71236fe441edcd690993dae5
Khaos116 <khaos116@outlook.com>
2021-11-29 20:22:47 +0700

    优化dp2px

--- ablibrary/src/main/java/cc/ab/base/utils/FixResources.kt
+++ ablibrary/src/main/java/cc/ab/base/utils/FixResources.kt
@@ -1,9 +1,11 @@
 package cc.ab.base.utils
 
+import android.app.Activity
 import android.app.Application
 import android.content.res.Resources
 
 /**
+ * ☆☆☆☆由于在第一个页面无法获取正确的值，所以最好不要直接在第一页面的变量直接使用SizeUtils.dp2px为变量赋值☆☆☆☆
  * 部分手机在AutoSizeConfig适配后下面2个值会获取的不一样(第一个页面onCreate时在Application拿到正确的，Resources拿到是错误的)，
  * 导致SizeUtils.dp2px转换的值不一样，所以SizeUtils.dp2px不能直接使用，需要纠正使用
  * Resources.getSystem().displayMetrics.density
@@ -13,14 +15,39 @@ import android.content.res.Resources
  * Time:14:43
  */
 object FixResources {
-  //需要在第一个页面onCreate时调用(Application中测试还没获取到正常的值)
-  fun fixResources(application: Application) {
-    Resources.getSystem().displayMetrics.density = application.resources.displayMetrics.density
-    Resources.getSystem().displayMetrics.densityDpi = application.resources.displayMetrics.densityDpi
-    Resources.getSystem().displayMetrics.scaledDensity = application.resources.displayMetrics.scaledDensity
-    Resources.getSystem().displayMetrics.xdpi = application.resources.displayMetrics.xdpi
-    Resources.getSystem().displayMetrics.ydpi = application.resources.displayMetrics.ydpi
-    Resources.getSystem().displayMetrics.heightPixels = application.resources.displayMetrics.heightPixels
-    Resources.getSystem().displayMetrics.widthPixels = application.resources.displayMetrics.widthPixels
+  //测试发现在第一页面的OnCreate方法中都无法获取正常的值，所以为了保证拿到正确的值，在每个页面都执行以下
+  @Synchronized fun fixInActivityOnCreate(activity: Activity) {
+    //获取参数
+    val displayMetrics = activity.applicationContext.resources.displayMetrics
+    val targetDensity: Float = displayMetrics.widthPixels / 360f
+    val appScaleDensity = displayMetrics.scaledDensity
+    val appDensity = displayMetrics.density
+    //计算目标值 density，scaleDensity，densityDpi
+    val targetScaleDensity: Float = targetDensity * (appScaleDensity / appDensity)
+    val targetDensityDpi = (targetDensity * 160).toInt()
+    //替换Resources的density scaleDensity，densityDpi
+    mutableListOf(displayMetrics, Resources.getSystem().displayMetrics, activity.resources.displayMetrics).forEach { dm ->
+      dm.density = targetDensity
+      dm.scaledDensity = targetScaleDensity
+      dm.densityDpi = targetDensityDpi
+    }
+  }
+
+  //尽量保证在application中也初始化一次
+  @Synchronized fun fixInApplicationOnCreate(application: Application) {
+    //获取参数
+    val displayMetrics = application.resources.displayMetrics
+    val targetDensity: Float = displayMetrics.widthPixels / 360f
+    val appScaleDensity = displayMetrics.scaledDensity
+    val appDensity = displayMetrics.density
+    //计算目标值 density，scaleDensity，densityDpi
+    val targetScaleDensity: Float = targetDensity * (appScaleDensity / appDensity)
+    val targetDensityDpi = (targetDensity * 160).toInt()
+    //替换Resources的density scaleDensity，densityDpi
+    mutableListOf(displayMetrics, Resources.getSystem().displayMetrics).forEach { dm ->
+      dm.density = targetDensity
+      dm.scaledDensity = targetScaleDensity
+      dm.densityDpi = targetDensityDpi
+    }
   }
 }
