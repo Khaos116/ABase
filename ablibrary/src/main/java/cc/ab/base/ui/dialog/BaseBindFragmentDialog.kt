5c8d8da2e3cefb6fcb697c1b3e86d7300d9731dc
Khaos116 <khaos116@outlook.com>
2021-12-03 19:12:29 +0700

    弹窗不显示状态栏和导航栏

--- ablibrary/src/main/java/cc/ab/base/ui/dialog/BaseBindFragmentDialog.kt
+++ ablibrary/src/main/java/cc/ab/base/ui/dialog/BaseBindFragmentDialog.kt
@@ -61,6 +61,7 @@ abstract class BaseBindFragmentDialog<T : ViewBinding> : DialogFragment() {
     return viewBinding.root
   }
 
+  @Suppress("DEPRECATION")
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     //监听返回按钮
@@ -72,6 +73,23 @@ abstract class BaseBindFragmentDialog<T : ViewBinding> : DialogFragment() {
         false
       }
     }
+    //-------------------弹窗不显示状态栏和导航栏-------------------//
+    //dialog?.window?.let { w ->
+    //  w.statusBarColor = Color.TRANSPARENT
+    //  w.navigationBarColor = Color.TRANSPARENT
+    //  w.addFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+    //}
+    //dialog?.setOnShowListener {
+    //  dialog?.window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
+    //  val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    //      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    //      or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    //      or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    //      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    //      or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    //  dialog?.window?.decorView?.systemUiVisibility = uiOptions
+    //}
+    //-------------------弹窗不显示状态栏和导航栏-------------------//
     initView()
   }
 
