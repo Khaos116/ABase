28d9572360f91155c10afc930efa594589a73188
Khaos116 <khaos116@outlook.com>
2022-03-02 20:42:55 +0700

    图片选择3.0的裁切处理

---
+++ ablibrary/src/main/java/cc/ab/base/widget/engine/MyCropEngine.kt
@@ -0,0 +1,54 @@
+package cc.ab.base.widget.engine
+
+import android.net.Uri
+import androidx.fragment.app.Fragment
+import cc.ab.base.config.PathConfig
+import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.engine.CropEngine
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.utils.DateUtils
+import com.yalantis.ucrop.UCrop
+import com.yalantis.ucrop.model.AspectRatio
+import java.io.File
+
+/**
+ * https://github.com/LuckSiege/PictureSelector/wiki/PictureSelector-3.0-%E5%A6%82%E4%BD%95%E8%A3%81%E5%89%AA%EF%BC%9F
+ * Author:BoLuo
+ * Date:2022/3/2
+ * Time:19:06
+ */
+class MyCropEngine : CropEngine {
+  override fun onStartCrop(fragment: Fragment, currentLocalMedia: LocalMedia, dataSource: ArrayList<LocalMedia>, requestCode: Int) {
+    // 注意* 如果你实现自己的裁剪库，需要在Activity的.setResult();
+    // Intent中需要给MediaStore.EXTRA_OUTPUT，塞入裁剪后的路径；如果有额外数据也可以通过CustomIntentKey.EXTRA_CUSTOM_EXTRA_DATA字段存入；
+    // 1、构造可用的裁剪数据源
+    val currentCropPath = currentLocalMedia.availablePath
+    val inputUri: Uri = if (PictureMimeType.isContent(currentCropPath) || PictureMimeType.isHasHttp(currentCropPath)) {
+      Uri.parse(currentCropPath)
+    } else {
+      Uri.fromFile(File(currentCropPath))
+    }
+    val fileName = DateUtils.getCreateFileName("CROP_") + ".jpg"
+    val destinationUri = Uri.fromFile(File(PathConfig.TEMP_IMG_DIR, fileName))
+    val dataCropSource: ArrayList<String> = ArrayList()
+    for (i in 0 until dataSource.size) {
+      val media = dataSource[i]
+      dataCropSource.add(media.availablePath)
+    }
+    val uCrop = UCrop.of(inputUri, destinationUri, dataCropSource)
+    uCrop.setImageEngine(MyUCropImageEngine())
+    val options = UCrop.Options()
+    //设置裁切比例为1:1
+    val aspectRatios = mutableListOf<AspectRatio>()
+    for (localMedia in dataSource) {
+      aspectRatios.add(AspectRatio("1:1", 1.0f, 1.0f))
+    }
+    options.setMultipleCropAspectRatio(*(aspectRatios.toTypedArray()))
+    //不显示裁切内部网格
+    options.setShowCropGrid(false)
+    //不显示底部操作按钮
+    options.setHideBottomControls(true)
+    uCrop.withOptions(options)
+    uCrop.start(fragment.requireContext(), fragment, requestCode)
+  }
+}
---
+++ ablibrary/src/main/java/cc/ab/base/widget/engine/MyUCropImageEngine.kt
@@ -0,0 +1,105 @@
+package cc.ab.base.widget.engine
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
+import cc.ab.base.ext.PlaceHolderUtils
+import cc.ab.base.ext.loadImgVertical
+import cc.ab.base.ext.toFile
+import cc.ab.base.utils.MediaUtils
+import coil.imageLoader
+import coil.request.ImageRequest
+import com.luck.picture.lib.photoview.PhotoView
+import com.yalantis.ucrop.UCropImageEngine
+import java.lang.ref.WeakReference
+
+/**
+ * Author:BoLuo
+ * Date:2022/3/2
+ * Time:18:59
+ */
+class MyUCropImageEngine : UCropImageEngine {
+  override fun loadImage(context: Context, url: String, imageView: ImageView) {
+    val weak = WeakReference(imageView)
+    val isPhotoView = imageView is PhotoView
+    val isImageFile = MediaUtils.isImageFile(url)
+    val isVideoFile = MediaUtils.isVideoFile(url)
+    if (imageView.width == 0 && isPhotoView && (isImageFile || isVideoFile)) {
+      imageView.post { weak.get()?.let { iv -> delayLoad(url, iv) } }
+    } else delayLoad(url, imageView)
+  }
+
+  private fun delayLoad(url: String, imageView: ImageView) {
+    val width = imageView.width
+    val height = imageView.height
+    if (imageView is PhotoView) {
+      if (MediaUtils.isImageFile(url)) {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(url.toFile()?.path, options)
+        val widthImg = options.outWidth * 1f
+        val heightImg = options.outHeight * 1f
+        if (widthImg > 0) {
+          val isHorizontalVideo = (widthImg * 1f / heightImg) > (width * 1f / height)
+          val defaultRatio = if (isHorizontalVideo) height * 1f / heightImg else width * 1f / widthImg
+          val ratio = if (isHorizontalVideo) (width * 1f / widthImg / defaultRatio)
+          else (height * 1f / heightImg) / defaultRatio
+          imageView.setScaleLevels(ratio, ratio * 1.5f, ratio * 2f)
+          imageView.scale = ratio
+        } else imageView.setScaleLevels(0.5f, 1f, 2f)
+      } else if (MediaUtils.isVideoFile(url)) {
+        //读取视频尺寸和旋转角度
+        val mMetadataRetriever = MediaMetadataRetriever()
+        try {
+          mMetadataRetriever.setDataSource(url)
+          val videoRotation = mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION) ?: "0"
+          val videoHeight = mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT) ?: "0"
+          val videoWidth = mMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH) ?: "0"
+          mMetadataRetriever.release()
+          var heightVideo = 0
+          val widthVideo = if (Integer.parseInt(videoRotation) == 90 || Integer.parseInt(videoRotation) == 270) {
+            //角度不对需要宽高调换
+            heightVideo = videoWidth.toInt()
+            videoHeight.toInt()
+          } else {
+            heightVideo = videoHeight.toInt()
+            videoWidth.toInt()
+          }
+          if (widthVideo > 0) {
+            val isHorizontalVideo = (widthVideo * 1f / heightVideo) > (width * 1f / height)
+            val defaultRatio = if (isHorizontalVideo) height * 1f / heightVideo else width * 1f / widthVideo
+            val ratio = if (isHorizontalVideo) (width * 1f / widthVideo / defaultRatio)
+            else (height * 1f / heightVideo) / defaultRatio
+            imageView.setScaleLevels(ratio, ratio * 1.5f, ratio * 2f)
+          }
+        } catch (e: Exception) {
+          e.printStackTrace()
+        }
+      }
+    }
+    imageView.loadImgVertical(url, 720f / 1280)
+  }
+
+  override fun loadImage(context: Context, url: Uri, maxWidth: Int, maxHeight: Int, call: UCropImageEngine.OnCallbackListener<Bitmap>?) {
+    context.imageLoader.enqueue(
+      ImageRequest.Builder(context)
+        .size(720, 1280)
+        .data(url)
+        .target(
+          onStart = {
+            call?.onCall(PlaceHolderUtils.getLoadingHolder(720f / 1280).toBitmap())
+          },
+          onSuccess = {
+            call?.onCall(it.toBitmap())
+          },
+          onError = {
+            call?.onCall(PlaceHolderUtils.getErrorHolder(720f / 1280).toBitmap())
+          }
+        )
+        .build())
+  }
+}
