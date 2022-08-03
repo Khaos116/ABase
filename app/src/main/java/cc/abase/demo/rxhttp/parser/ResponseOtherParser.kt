5a36391fa5b62794427745faf2cd21bcd8e908f0
BoLuo <123@qq.com>
2022-08-03 12:40:14 +0700

    强制进行统一异常处理

--- app/src/main/java/cc/abase/demo/rxhttp/parser/ResponseOtherParser.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/parser/ResponseOtherParser.kt
@@ -1,8 +1,13 @@
 package cc.abase.demo.rxhttp.parser
 
+import cc.abase.demo.config.GlobalErrorHandle
+import com.blankj.utilcode.util.GsonUtils
+import org.json.JSONObject
 import rxhttp.wrapper.annotation.Parser
+import rxhttp.wrapper.exception.ParseException
 import rxhttp.wrapper.parse.TypeParser
 import rxhttp.wrapper.utils.convert
+import timber.log.Timber
 import java.io.IOException
 import java.lang.reflect.Type
 
@@ -37,6 +42,31 @@ open class ResponseOtherParser<T> : TypeParser<T> {
     val responseOther: T = response.convert(types.first())
     //---------------------------------交给框架处理End---------------------------------//
 
+    //---------------------------------强制进行统一异常处理(不同项目可能code和msg需要替换)Start---------------------------------//
+    val codeKey = "code"//接口正常与否的code码key
+    val msgKey = "msg"//接口正常和异常的提示信息key
+    val successMsg = "success"//接口正常返回的提示消息
+    val successCode = 200//接口正常的code值
+    val noLoginCode = -154//未登录对应的code值
+    try {
+      val jsonStr = GsonUtils.toJson(responseOther)
+      if (jsonStr.startsWith("{") && jsonStr.endsWith("}")) {
+        val jsonObject = JSONObject(jsonStr)
+        if (jsonObject.has(codeKey)) {
+          val code = jsonObject.optInt(codeKey, 0)
+          if (code != successCode) {
+            val msg = jsonObject.optString(msgKey, "")
+            val codeNew = if (msg.contains("未登录")) noLoginCode else code
+            val dealCode = GlobalErrorHandle.dealGlobalErrorCode(codeNew)
+            if (successMsg == msg) Timber.e("ResponseLive解析异常:${response.request.url}")
+            throw ParseException(dealCode.toString(), msg, response)
+          }
+        }
+      }
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+    //---------------------------------强制进行统一异常处理(不同项目可能code和msg需要替换)End---------------------------------//
     return responseOther
   }
 }
