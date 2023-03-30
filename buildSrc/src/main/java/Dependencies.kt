0937454496d0d9bdc8c34a3441b64f644393a036
Khaos116 <Khaos116@qq.com>
2023-03-30 09:59:25 +0700

    3.0.8

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -72,7 +72,7 @@ object Deps {
   const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
 
   //日志json打印 https://github.com/Ayvytr/OKHttpLogInterceptor
-  const val okhttp_log = "io.github.ayvytr:okhttploginterceptor:3.0.6"
+  const val okhttp_log = "io.github.ayvytr:okhttploginterceptor:3.0.8"
 
   //RxHttp https://github.com/liujingxing/okhttp-RxHttp
   const val rxhttp = "com.github.liujingxing.rxhttp:rxhttp:${Versions.rxHttp}"
