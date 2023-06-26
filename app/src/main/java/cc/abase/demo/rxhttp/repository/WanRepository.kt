b3e479dbfecb0f1d95ce562c931d1bba88c42adc
Khaos116 <Khaso116@qq.com>
2023-06-26 18:56:16 +0700

    修改返回格式

--- app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/WanRepository.kt
@@ -8,6 +8,7 @@ import cc.abase.demo.constants.TimeConstants
 import cc.abase.demo.constants.api.WanUrls
 import rxhttp.cc.RxHttp
 import rxhttp.cc.toAwaitResponseWan
+import rxhttp.onErrorReturnItem
 import rxhttp.wrapper.cache.CacheMode
 import rxhttp.wrapper.coroutines.Await
 
@@ -23,7 +24,8 @@ object WanRepository {
     return RxHttp.get(WanUrls.Home.BANNER)
       .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
       .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
-      .toAwaitResponseWan()
+      .toAwaitResponseWan<MutableList<BannerBean>>()
+      .onErrorReturnItem(mutableListOf())
   }
 
   //获取文章列表
@@ -31,6 +33,7 @@ object WanRepository {
     return RxHttp.get(String.format(WanUrls.Home.ARTICLE, page))
       .setCacheValidTime(TimeConstants.HOME_CACHE) //设置缓存时长
       .setCacheMode(if (readCache) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先读取缓存，失败再请求数据
-      .toAwaitResponseWan()
+      .toAwaitResponseWan<BasePageList<ArticleBean>>()
+      .onErrorReturnItem(BasePageList())
   }
 }
