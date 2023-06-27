f393fc2348a55eba1b0b4de8e9903a3b94268464
Khaos116 <Khaso116@qq.com>
2023-06-27 14:57:00 +0700

    完善数据

--- app/src/main/java/cc/abase/demo/bean/readhub/TopicBean.kt
+++ app/src/main/java/cc/abase/demo/bean/readhub/TopicBean.kt
@@ -5,24 +5,29 @@ package cc.abase.demo.bean.readhub
  * Date:2022/3/8
  * Time:16:19
  */
+class TopicBeanParent(
+  var totalItems: Int = 0,
+  var startIndex: Int = 0,
+  var pageIndex: Int = 0,
+  var itemsPerPage: Int = 0,
+  var currentItemCount: Int = 0,
+  var totalPages: Int = 0,
+  val items: MutableList<TopicBean>? = null
+)
+
 class TopicBean(
-  val id: String? = null,
-  val summary: String? = null,
-  val updatedAt: String? = null,
-  val order: Long = 0,
-  val newsArray: MutableList<TopicTitleBean> = mutableListOf(),
+  val uid: String? = null,//8qX87kQF3fP
+  val title: String? = null,//左旋星获数千万元PreA+轮融资
+  val summary: String? = null,//细胞基因治疗和核酸药物CDMO公司左旋星生物已获得数千万元PreA+轮融资，由恒旭资本独家投资 ... 此前，公司曾获得经纬创投的Pre-A轮融资，以及济峰资本和薄荷天使基金的天使轮投资 ... 左旋星成立于2022年3月，专注于细胞及基因治疗（CGT）和核酸药物临床产品及疗法检测与生产服务，开发针对mRNA、病毒载体、CAR-T等治疗产品的临床检测服务，并开发质粒及mRNA的GMP级别生产工艺及过程产品质控方法，以加快基因细胞治疗药物和疗法的进程。
+  val createdAt: String? = null,//2023-06-27T06:46:11.706Z
+  val newsAggList: MutableList<TopicTitleBean>? = null,
 )
 
 class TopicTitleBean(
-  val id: String? = null,// 3816550,
-  val url: String? = null,// "https://www.tmtpost.com/nictation/6030778.html",
-  val title: String? = null,// "比亚迪回应王传福卸任杭州比亚迪董事长 ：简化流程，业务无影响",
-  val siteName: String? = null,// "钛媒体",
-  val mobileUrl: String? = null,// "https://www.tmtpost.com/nictation/6030778.html",
-  val autherName: String? = null,// "",
-  val duplicateId: String? = null,// 2,
-  val publishDate: String? = null,// "2022-03-07T23:40:12.070Z",
-  val language: String? = null,// "zh-cn",
-  val hasInstantView: String? = null,// true,
+  val uid: String? = null,// 8qXuCMWpJcc
+  val url: String? = null,// https://www.chinaventure.com.cn/news/111-20230627-375881.html
+  val title: String? = null,// 左旋星获数千万元PreA+轮融资
+  val siteNameDisplay: String? = null,// 投中网
   val statementType: String? = null,// 1
+  val statementTypeDisplay: String? = null,// 事实
 )
