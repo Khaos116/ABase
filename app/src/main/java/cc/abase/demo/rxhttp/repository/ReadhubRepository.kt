b20fc18558ccd576c2508f20079bb6bcbf1c9209
Khaos116 <Khaso116@qq.com>
2023-06-27 12:02:33 +0700

    读取缓存参数

--- app/src/main/java/cc/abase/demo/rxhttp/repository/ReadhubRepository.kt
+++ app/src/main/java/cc/abase/demo/rxhttp/repository/ReadhubRepository.kt
@@ -14,10 +14,10 @@ import rxhttp.wrapper.coroutines.Await
  */
 object ReadhubRepository {
   //获得热门话题
-  suspend fun getTopicList(lastOrder: Long = 0): Await<MutableList<TopicBean>> {
+  suspend fun getTopicList(lastOrder: Long = 0, readCache: Boolean = true): Await<MutableList<TopicBean>> {
     return RxHttp.get("${ReadhubUrl.Home.TOPIC}?pageSize=20${if (lastOrder <= 0) "" else "&lastCursor=$lastOrder"}")
       .setCacheValidTime(TimeConstants.DYN_CACHE) //设置缓存时长
-      .setCacheMode(CacheMode.REQUEST_NETWORK_FAILED_READ_CACHE) //先请求数据，失败再读取缓存
+      .setCacheMode(if (readCache && lastOrder == 0L) CacheMode.READ_CACHE_FAILED_REQUEST_NETWORK else CacheMode.ONLY_NETWORK) //先请求数据，失败再读取缓存
       .toAwaitResponseReadhub()
   }
 
