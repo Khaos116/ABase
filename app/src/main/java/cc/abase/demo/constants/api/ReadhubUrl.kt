14753f5589ac7c177cb63dbda78e0701cd24aea8
Khaos116 <Khaso116@qq.com>
2023-06-27 12:17:15 +0700

    修改注释

--- app/src/main/java/cc/abase/demo/constants/api/ReadhubUrl.kt
+++ app/src/main/java/cc/abase/demo/constants/api/ReadhubUrl.kt
@@ -1,12 +1,13 @@
 package cc.abase.demo.constants.api
 
 /**
+ * https://readhub.cn/
  * Author:Khaos116
  * Date:2022/3/8
  * Time:15:47
  */
 object ReadhubUrl {
-  const val baseUrl = "https://api.readhub.cn/" //WanAndroid的Host
+  const val baseUrl = "https://api.readhub.cn/" //Readhub的Host
 
   object Home {
     //热门话题
