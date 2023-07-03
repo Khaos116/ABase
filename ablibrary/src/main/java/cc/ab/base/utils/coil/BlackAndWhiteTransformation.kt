6990878e8a7e4710d499884444f0c17b10848452
Khaos116 <Khaso116@qq.com>
2023-07-03 13:24:53 +0700

    修复黑白化图片显示不全的bug

--- ablibrary/src/main/java/cc/ab/base/utils/coil/BlackAndWhiteTransformation.kt
+++ ablibrary/src/main/java/cc/ab/base/utils/coil/BlackAndWhiteTransformation.kt
@@ -1,10 +1,9 @@
 package cc.ab.base.utils.coil
 
 import android.annotation.SuppressLint
-import android.graphics.Bitmap
+import android.graphics.*
 import coil.size.Size
 import coil.transform.Transformation
-import com.blankj.utilcode.util.ImageUtils
 
 /**
  * 转化为黑白图
@@ -16,7 +15,15 @@ class BlackAndWhiteTransformation : Transformation {
   override val cacheKey: String = "${BlackAndWhiteTransformation::class.java.name}-BlackAndWhite"
 
   @SuppressLint("Range")
-  override suspend fun transform(input: Bitmap, size: Size): Bitmap {
-    return ImageUtils.toGray(input)
+  override suspend fun transform(input: Bitmap, size: Size): Bitmap {//不能使用Bitmap.createBitmap，否则会导致图片显示不全
+    val output = input.copy(input.config, true)
+    val canvas = Canvas(output)
+    val paint = Paint().apply {
+      val colorMatrix = ColorMatrix()
+      colorMatrix.setSaturation(0f) // 设置饱和度为0，即将图像转为灰度图
+      colorFilter = ColorMatrixColorFilter(colorMatrix)
+    }
+    canvas.drawBitmap(input, 0f, 0f, paint)
+    return output
   }
 }
