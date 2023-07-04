89528a1ddc907643e5a6aefb110b621eecedc3a1
Khaos116 <Khaso116@qq.com>
2023-07-04 12:47:15 +0700

    可为空

--- app/src/main/java/cc/abase/demo/bean/wan/IntegralBean.kt
+++ app/src/main/java/cc/abase/demo/bean/wan/IntegralBean.kt
@@ -6,9 +6,9 @@ package cc.abase.demo.bean.wan
  * @date: 2019/10/12 20:06
  */
 data class IntegralBean(
-    var coinCount: Int = 0,
-    var rank: Int = 0,
-    var userId: Long = 0,
-    var username: String?,
-    var level: String?
+  var coinCount: Int = 0,
+  var rank: Int = 0,
+  var userId: Long = 0,
+  var username: String? = null,
+  var level: String? = null,
 )
