17de80bf7ea7a095c3cd8dc837e888ff5e65a567
Khaos116 <Khaso116@qq.com>
2023-07-04 10:58:25 +0700

    还是回归LayerDrawable实现占位图，使用Bitmap.createBitmap会在列表中卡顿

--- ablibrary/src/main/java/cc/ab/base/utils/PlaceHolderUtils.kt
+++ ablibrary/src/main/java/cc/ab/base/utils/PlaceHolderUtils.kt
@@ -1,15 +1,15 @@
 package cc.ab.base.utils
 
-import android.content.res.Resources
 import android.graphics.*
-import android.graphics.drawable.Drawable
+import android.graphics.drawable.*
+import android.view.Gravity
 import androidx.annotation.ColorInt
-import androidx.core.graphics.drawable.toDrawable
 import cc.ab.base.R
 import com.blankj.utilcode.util.ResourceUtils
 import com.blankj.utilcode.util.ScreenUtils
 
 /**
+ * 不能使用Bitmap.createBitmap创建占位图，不然在列表中会卡顿很严重
  * 动态生成loading和error占位图,默认占位图资源需要1:1
  * Author:Khaos
  * Date:2020-12-8
@@ -24,8 +24,8 @@ object PlaceHolderUtils {
    * @param bgColor 占位图背景色
    * @param corner 圆角大小px
    */
-  fun getLoadingHolder(ratio: Float = 1f, width: Int = ScreenUtils.getScreenWidth(), @ColorInt bgColor: Int = Color.WHITE, corner: Float = 0f): Drawable {
-    return generatePlaceholder(newRatio = ratio, newWidth = width, bgColor = bgColor, corner = corner, loading = true)
+  fun getLoadingHolder(ratio: Float = 1f, width: Int = ScreenUtils.getScreenWidth(), @ColorInt bgColor: Int = Color.WHITE): Drawable {
+    return generatePlaceholder(newRatio = ratio, newWidth = width, bgColor = bgColor, loading = true)
   }
   //</editor-fold>
 
@@ -37,8 +37,8 @@ object PlaceHolderUtils {
    * @param bgColor 占位图背景色
    * @param corner 圆角大小px
    */
-  fun getErrorHolder(ratio: Float = 1f, width: Int = ScreenUtils.getScreenWidth(), @ColorInt bgColor: Int = Color.WHITE, corner: Float = 0f): Drawable {
-    return generatePlaceholder(newRatio = ratio, newWidth = width, bgColor = bgColor, corner = corner, loading = false)
+  fun getErrorHolder(ratio: Float = 1f, width: Int = ScreenUtils.getScreenWidth(), @ColorInt bgColor: Int = Color.WHITE): Drawable {
+    return generatePlaceholder(newRatio = ratio, newWidth = width, bgColor = bgColor, loading = false)
   }
   //</editor-fold>
 
@@ -51,47 +51,25 @@ object PlaceHolderUtils {
    * @param corner 圆角大小px
    * @param loading 是否是加载中的占位图
    */
-  private fun generatePlaceholder(newRatio: Float = 1f, newWidth: Int, bgColor: Int, corner: Float, loading: Boolean): Drawable {
+  private fun generatePlaceholder(newRatio: Float = 1f, newWidth: Int, bgColor: Int, loading: Boolean): Drawable {
     //计算背景图高度
     val newHeight = (newWidth * 1f / newRatio).toInt()
     //计算中间图标的尺寸
     val size = newWidth.coerceAtMost(newHeight)
-    // 计算居中偏移量
-    val offsetX = (newWidth - size) / 2
-    val offsetY = (newHeight - size) / 2
-    // 创建缩放和居中的占位图
-    val bitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.RGB_565)
-    bitmap.eraseColor(bgColor)//指定背景色
-    val canvas = Canvas(bitmap)
-    val drawable = ResourceUtils.getDrawable(if (loading) R.drawable.loading_square else R.drawable.error_square)
-    drawable.setBounds(offsetX, offsetY, offsetX + size, offsetY + size)
-    drawable.draw(canvas)
-    return (if (corner > 0) getRoundedCornerBitmap(bitmap, corner) else bitmap).toDrawable(Resources.getSystem())
-  }
-  //</editor-fold>
-
-  //<editor-fold defaultstate="collapsed" desc="图片圆角处理(但是图片会缩放，可能填充到UI上不是理想效果)">
-  private fun getRoundedCornerBitmap(bitmap: Bitmap, radius: Float): Bitmap {
-    val output = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-    //output.eraseColor(Color.BLUE)
-    val canvas = Canvas(output)
-    val paint = Paint()
-
-    val rect = Rect(0, 0, bitmap.width, bitmap.height)
-    val rectF = RectF(rect)
-    val path = Path()
-    path.addRoundRect(rectF, radius, radius, Path.Direction.CW)
-
-    paint.isAntiAlias = true
-    canvas.drawARGB(0, 0, 0, 0)
-    paint.color = Color.BLACK
-    canvas.drawPath(path, paint)
-
-    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-    canvas.drawBitmap(bitmap, rect, rect, paint)
-
-    return output
-    //</editor-fold>
+    //背景图
+    val d1 = ColorDrawable()
+    d1.setBounds(0, 0, newWidth, newHeight)
+    d1.color = bgColor
+    //占位图
+    val d2 = ResourceUtils.getDrawable(if (loading) R.drawable.loading_square else R.drawable.error_square)
+    d2.setBounds(0, 0, size, size)
+    //生成LayerDrawable
+    return LayerDrawable(arrayOf(d1, d2)).also {
+      it.setLayerSize(0, newWidth, newHeight)
+      it.setLayerSize(1, size, size)
+      it.setLayerGravity(0, Gravity.CENTER)
+      it.setLayerGravity(1, Gravity.CENTER)
+    }
   }
   //</editor-fold>
 }
