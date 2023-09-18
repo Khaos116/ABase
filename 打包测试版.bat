646da06d00400a14b301e82e8b82bd5b206b8e0c
Khaos116 <XXX@qq.com>
2023-09-18 12:31:07 +0700

    bat打包命令

---
+++ 打包测试版.bat
@@ -0,0 +1,15 @@
+::设置编码方式(65001 UTF-8;936 GBK;437 英语)
+chcp 65001
+
+::双冒号表示注释(setlocal enabledelayedexpansion是延迟变量赋值使用)
+@echo off&setlocal enabledelayedexpansion
+echo ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆==Start==☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+::先杀掉java程序，释放内存
+taskkill /f /t /im java.exe
+::切换到当前目录(.bat文件需要放到项目gradlew文件同目录)
+cd /d %~dp0
+::执行清理并打包
+gradlew clean assembles1Debug
+echo ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆==End==☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+::使用pause可以查看过程
+pause
