4beb8660c0bf51af0b20e336c6db6abd6346adeb
Khaos116 <XXX@qq.com>
2023-09-18 14:30:04 +0700

    bat关闭java，释放内存

---
+++ killJava.bat
@@ -0,0 +1,11 @@
+::设置编码方式(65001 UTF-8;936 GBK;437 英语)
+chcp 65001
+
+::双冒号表示注释(setlocal enabledelayedexpansion是延迟变量赋值使用)
+@echo off&setlocal enabledelayedexpansion
+echo ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆==Start==☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+::杀掉java程序，释放内存
+taskkill /f /t /im java.exe
+echo ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆==End==☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+::使用pause可以查看过程
+pause
