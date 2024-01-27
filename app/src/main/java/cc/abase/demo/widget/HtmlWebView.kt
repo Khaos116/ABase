4226c2571784cb77feffddaf6164b6130accde4d
Khaos116 <XXX@qq.com>
2024-01-27 18:02:00 +0700

    测试能不能提交

--- app/src/main/java/cc/abase/demo/widget/HtmlWebView.kt
+++ app/src/main/java/cc/abase/demo/widget/HtmlWebView.kt
@@ -14,7 +14,6 @@ import org.jsoup.Jsoup
 /**
  * 替换解析抱歉，可以使用Jsoup; https://www.jianshu.com/p/d2acd79c3d32
  * WebView全面解析：https://blog.csdn.net/vicwudi/article/details/81990467
- *
  * http://t.zoukankan.com/renhui-p-11401033.html
  * Author:Khaos
  * Date:2022/1/12
